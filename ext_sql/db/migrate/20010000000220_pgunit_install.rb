161c2853f360d6f7adcb9ccaa356fd3c3125e296
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-13 11:34:41 -0400

    correction for PgunitInstall migration (wrong engine)

--- ext_sql/db/migrate/20010000000220_pgunit_install.rb
+++ ext_sql/db/migrate/20010000000220_pgunit_install.rb
@@ -1,9 +1,9 @@
 class PgunitInstall < ActiveRecord::Migration[5.2]
   def up
-    execute ExtMinitest::Engine.root.join('db/PGUnit.sql').read.strip_sql[1..-1]
+    execute ExtSql::Engine.root.join('db/PGUnit.sql').read.strip_sql[1..-1]
   end
 
   def down
-    execute ExtMinitest::Engine.root.join('db/PGUnitDrop.sql').read.strip_sql
+    execute ExtSql::Engine.root.join('db/PGUnitDrop.sql').read.strip_sql
   end
 end
