440a4b57c4ecd41a1a7f1d9333e81414c45a49a9
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-17 14:35:19 -0400

    centos support for bootstrap/nofile recipe

--- config/provision/recipes/bootstrap/nofile.sh
+++ config/provision/recipes/bootstrap/nofile.sh
@@ -1,6 +1,15 @@
-# https://github.com/serverless/serverless/issues/3249
-
+# /etc/systemd/system/nginx.service.d/nofile.conf
+#
+# [Service]
+# LimitNOFILE=65536
+#
+sun.backup_compare '/etc/security/limits.conf'
+sun.backup_compare '/etc/pam.d/sshd'
+echo "session    required     pam_limits.so" >> /etc/pam.d/sshd
+echo "*               soft    nproc           65536" >> /etc/security/limits.conf
+echo "*               hard    nproc           65536" >> /etc/security/limits.conf
+echo "*               soft    nofile          65536" >> /etc/security/limits.conf
+echo "*               hard    nofile          65536" >> /etc/security/limits.conf
+echo "fs.file-max = 524288" >> /etc/sysctl.conf
+sysctl -p
 ulimit -Sn 65536
-echo "session required pam_limits.so" >> /etc/pam.d/common-session
-echo "root soft  nofile 40000" >> /etc/security/limits.conf
-echo "root hard  nofile 100000" >> /etc/security/limits.conf
