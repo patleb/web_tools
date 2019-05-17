645cddff1de60ac1507bff4ae262048cb4811f4c
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-17 17:24:12 -0400

    swap recipe clean-up

--- config/provision/recipes/bootstrap/swap.sh
+++ config/provision/recipes/bootstrap/swap.sh
@@ -1,16 +1,19 @@
-<% if @sun.os.ubuntu? -%>
+case "$OS" in
+ubuntu)
   fallocate -l <%= @sun.swap_size %> /swap
-<% else %>
-  dd if=/dev/zero of=/swap bs=1<%= @sun.swap_size[-1] %> count=<%= @sun.swap_size[0..-2] %>
-<% end %>
-chmod 600 /swap
-mkswap /swap
-swapon /swap
-
-<% if @sun.os.ubuntu? -%>
+  chmod 600 /swap
+  mkswap /swap
+  swapon /swap
   echo '/swap   none    swap    sw    0   0' >> /etc/fstab
-<% else -%>
+;;
+centos)
+  dd if=/dev/zero of=/swap bs=1<%= @sun.swap_size[-1] %> count=<%= @sun.swap_size[0..-2] %>
+  chmod 600 /swap
+  mkswap /swap
+  swapon /swap
   echo '/swap   swap    swap    defaults    0   0' >> /etc/fstab
-<% end %>
+;;
+esac
+
 echo 'vm.swappiness=10' >> /etc/sysctl.conf
 echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
