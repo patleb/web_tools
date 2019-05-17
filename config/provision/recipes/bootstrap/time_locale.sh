443d2a5bc1dc2a5bd46e6f8bdb179093246728c0
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-17 11:13:01 -0400

    centos support for bootstrap/time_locale recipe

--- config/provision/recipes/bootstrap/time_locale.sh
+++ config/provision/recipes/bootstrap/time_locale.sh
@@ -1,5 +1,21 @@
-sun.mute "timedatectl set-timezone <%= @sun.timezone || 'Etc/UTC' %>"
-sun.mute "locale-gen <%= @sun.locales || 'en_US en_US.UTF-8' %>"
-sun.mute "dpkg-reconfigure locales"
+TIMEZONE=<%= @sun.timezone || 'Etc/UTC' %>
+LOCALE=<%= @sun.locale || 'en_US' %>
+
+sun.mute "timedatectl set-timezone $TIMEZONE"
+sun.mute "timedatectl set-local-rtc 0"
+
+case "$OS" in
+ubuntu)
+  sun.mute "locale-gen $LOCALE $LOCALE.UTF-8"
+  sun.mute "dpkg-reconfigure locales"
+;;
+centos)
+  sun.mute "localectl set-locale LANG=$LOCALE.UTF-8"
+;;
+esac
+
 sun.install "curl"
 sun.install "ntp"
+
+systemctl enable ntpd
+systemctl start ntpd
