873936812480f400c916ee1eed15d7b89aa3e770
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-20 12:49:12 -0400

    centos support added to user/deployer recipe

--- config/provision/recipes/user/deployer.sh
+++ config/provision/recipes/user/deployer.sh
@@ -2,9 +2,18 @@ export DEPLOYER_NAME=<%= @sun.deployer_name %>
 DEPLOYER_PWD=<%= @sun.deployer_password %>
 DEPLOYER_PATH=/home/$DEPLOYER_NAME
 
-adduser $DEPLOYER_NAME --gecos '' --disabled-password
-echo "$DEPLOYER_NAME:$DEPLOYER_PWD" | sudo chpasswd
-adduser $DEPLOYER_NAME sudo
+case "$OS" in
+ubuntu)
+  adduser $DEPLOYER_NAME --gecos '' --disabled-password
+  echo "$DEPLOYER_NAME:$DEPLOYER_PWD" | sudo chpasswd
+  adduser $DEPLOYER_NAME sudo
+;;
+centos)
+  adduser $DEPLOYER_NAME
+  echo -e "$DEPLOYER_PWD\n$DEPLOYER_PWD" | sudo passwd $DEPLOYER_NAME
+  gpasswd -a $DEPLOYER_NAME wheel
+;;
+esac
 
 mkdir $DEPLOYER_PATH/.ssh
 chmod 700 $DEPLOYER_PATH/.ssh
@@ -13,3 +22,5 @@ chmod 600 $DEPLOYER_PATH/.ssh/authorized_keys
 chown -R $DEPLOYER_NAME:$DEPLOYER_NAME $DEPLOYER_PATH
 
 sun.compile "/etc/sudoers.d/deployer" 0440 root:root
+echo -e '<%= @sun.admin_private_key || `cat #{@sun.pkey}`.strip %>' > "$DEPLOYER_PATH/.ssh/id_rsa"
+chmod 600 $DEPLOYER_PATH/.ssh/id_rsa
