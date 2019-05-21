961db00cf0cd03fe719894aea7bcd42953b594c9
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-21 16:21:17 -0400

    epel repo url correction

--- config/provision/recipes/bootstrap/upgrade__UPGRADE__.sh
+++ config/provision/recipes/bootstrap/upgrade__UPGRADE__.sh
@@ -4,7 +4,7 @@ ubuntu)
 ;;
 centos)
   yes | yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
-  yes | yum localinstall --nogpgcheck http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-latest-7.noarch.rpm
+  yes | yum localinstall --nogpgcheck http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
   yes | yum localinstall --nogpgcheck http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
   if [[ ! $(grep -Fx "enabled=1" "/etc/yum.repos.d/remi.repo") ]]; then
     sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/remi.repo
