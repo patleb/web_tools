e75bb023785445e9e72b31e02b7d4e2e26ffb434
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-21 13:17:02 -0400

    db/postgres/madlib recipe updated

--- config/provision/recipes/db/postgres/madlib__POSTGRES__.sh
+++ config/provision/recipes/db/postgres/madlib__POSTGRES__.sh
@@ -1,16 +1,15 @@
 # TODO install from binaries instead, too long to build 13 minutes 14 seconds
 # https://gist.github.com/tqyq/955fcf2e505894d59db6b0e771cfe3e8
 
+PG_MAJOR="<%= @sun.postgres %>"
 PG_VERSION=$(sun.current_version 'postgresql')
-PG_MAJOR=$(sun.pg_major_version "$PG_VERSION")
-PG_HOLD="postgresql-server-dev-$PG_MAJOR postgresql-plpython-$PG_MAJOR"
+PG_PACKAGES="postgresql-server-dev-$PG_MAJOR postgresql-plpython-$PG_MAJOR"
 
 sun.install "m4"
 sun.install "patch"
 sun.install "cmake"
 sun.install "pgxnclient"
-sun.install "postgresql-server-dev-$PG_MAJOR"
-sun.install "postgresql-plpython-$PG_MAJOR"
-apt-mark hold $PG_HOLD
+sun.install "$PG_PACKAGES"
+sun.lock "$PG_PACKAGES"
 
 pgxn install madlib
