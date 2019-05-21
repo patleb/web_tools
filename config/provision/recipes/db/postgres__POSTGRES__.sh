7aa27fc8eaffb32251d1920811238ef5003f9146
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-21 11:30:16 -0400

    centos support for db/postgres recipe (wip)

--- config/provision/recipes/db/postgres__POSTGRES__.sh
+++ config/provision/recipes/db/postgres__POSTGRES__.sh
@@ -3,35 +3,57 @@
 # https://github.com/reorg/pg_repack
 # https://www.modio.se/scaling-past-the-single-machine.html
 # https://askubuntu.com/questions/732431/how-to-uninstall-specific-versions-of-postgres
-
-# '10+191.pgdg16.04+1' --> TODO force MAJOR, do not infer
-PG_VERSION="<%= @sun.postgres || "$(sun.current_version 'postgresql')" %>"
-PG_MAJOR=$(sun.pg_major_version "$PG_VERSION")
+PG_MAJOR="<%= @sun.postgres %>"
 PG_MANIFEST=$(sun.manifest_path 'postgresql')
-PG_HOLD="postgresql-$PG_MAJOR libpq-dev postgresql postgresql-contrib postgresql-common"
 
 case "$OS" in
 ubuntu)
+  PG_PACKAGES="postgresql-$PG_MAJOR postgresql postgresql-contrib postgresql-common libpq-dev"
+  PG_CONF_DIR="/etc/postgresql/$PG_MAJOR/main"
+
   sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' >> /etc/apt/sources.list.d/pgdg.list"
   wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
   sun.update
+
+  PG_VERSION="$(sun.current_version postgresql-$PG_MAJOR)"
 ;;
 centos)
-  yes | yum localinstall --nogpgcheck https://yum.postgresql.org/11/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-  sun.install "postgresql$PG_MAJOR-server"
+  PG_PACKAGES="postgresql$PG_MAJOR-server postgresql$PG_MAJOR postgresql$PG_MAJOR-contrib postgresql$PG_MAJOR-devel"
+  PG_CONF_DIR="/var/lib/pgsql/$PG_MAJOR/data"
+
+  yes | yum localinstall --nogpgcheck "https://yum.postgresql.org/$PG_MAJOR/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
+
+  PG_VERSION="$(sun.current_version postgresql$PG_MAJOR)"
 ;;
 esac
 
 if [[ ! -s "$PG_MANIFEST" ]]; then
-  sun.install "postgresql-$PG_MAJOR"
-  sun.install "postgresql-contrib-$PG_MAJOR"
-  sun.install "libpq-dev"
-  apt-mark hold $PG_HOLD
+  sun.install "$PG_PACKAGES"
+  sun.lock "$PG_PACKAGES"
 
-  sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/postgresql.conf"
-  sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/pg_hba.conf"
-  sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/start.conf"
+  case "$OS" in
+  ubuntu)
+    sun.backup_compare "$PG_CONF_DIR/postgresql.conf"
+    sun.backup_compare "$PG_CONF_DIR/pg_hba.conf"
+  ;;
+  centos)
+    /usr/pgsql-$PG_MAJOR/bin/postgresql-$PG_MAJOR-setup initdb
+    sun.backup_compare "$PG_CONF_DIR/postgresql.conf"
+    sun.backup_compare "$PG_CONF_DIR/pg_hba.conf"
+    echo 'Alias=postgresql.service' >> /usr/lib/systemd/system/postgresql-$PG_MAJOR.service
+    systemctl enable postgresql-$PG_MAJOR
+    systemctl start postgresql
+  ;;
+  esac
 else
+  case "$OS" in
+  centos)
+    # TODO centos
+    echo "postgres upgrade not supported on CentOS"
+    exit 1
+  ;;
+  esac
+
   PG_OLD_VERSION=$(tac "$PG_MANIFEST" | grep -m 1 '.')
   PG_OLD_MAJOR=$(sun.pg_major_version "$PG_OLD_VERSION")
 
@@ -48,18 +70,15 @@ else
 
   systemctl stop postgresql
 
-  apt-mark unhold $PG_HOLD
-  sun.install "postgresql-$PG_MAJOR"
-  sun.install "postgresql-contrib-$PG_MAJOR"
-  sun.install "libpq-dev"
-  apt-mark hold $PG_HOLD
+  sun.unlock "$PG_PACKAGES"
+  sun.install "$PG_PACKAGES"
+  sun.lock "$PG_PACKAGES"
 
   if [[ "$PG_OLD_MAJOR" != "$PG_MAJOR" ]]; then
     # pg_lsclusters
     # ALTER USER "postgres" WITH PASSWORD 'postgres';
-    sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/postgresql.conf"
-    sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/pg_hba.conf"
-    sun.backup_compare "/etc/postgresql/$PG_MAJOR/main/start.conf"
+    sun.backup_compare "$PG_CONF_DIR/postgresql.conf"
+    sun.backup_compare "$PG_CONF_DIR/pg_hba.conf"
     sudo su - postgres << EOF
       pg_dropcluster --stop "$PG_MAJOR" main
       pg_upgradecluster -v "$PG_MAJOR" "$PG_OLD_MAJOR" main
