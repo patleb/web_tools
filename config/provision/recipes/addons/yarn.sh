f3536090009a04f2e0c13a981e6cb69b5dc10825
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 10:12:52 -0400

    centos support for nodejs/system recipe

--- config/provision/recipes/addons/yarn.sh
+++
@@ -1,5 +0,0 @@
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sun.update
-
-sun.install "yarn"
--- config/provision/recipes/nodejs/system__OS_NODEJS__.sh
+++ config/provision/recipes/nodejs/system__OS_NODEJS__.sh
@@ -1,5 +1,17 @@
-NODE_VERSION=<%= @sun.apt_nodejs || '10' %>
+NODE_VERSION=<%= @sun.os_nodejs || '10' %>
 
-curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
+case "$OS" in
+ubuntu)
+  curl -sL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash -
+  curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+;;
+centos)
+  curl -sL https://rpm.nodesource.com/setup_$NODE_VERSION.x | sudo bash -
+  curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+;;
+esac
 
+sun.update
 sun.install "nodejs"
+sun.install "yarn"
