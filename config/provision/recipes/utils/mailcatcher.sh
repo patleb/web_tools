809c880bd2af63e91aa53729e2167713bca11f0c
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 17:26:54 -0400

    mailcatcher recipe cleaned-up

--- config/provision/recipes/utils/mailcatcher.sh
+++ config/provision/recipes/utils/mailcatcher.sh
@@ -1,8 +1,9 @@
 gem install mailcatcher
-/usr/local/bin/mailcatcher --http-ip=0.0.0.0
 
 sun.move '/etc/systemd/system/mailcatcher.service'
-systemctl enable mailcatcher
 
 ufw allow 1080/tcp
 ufw reload
+
+systemctl enable mailcatcher
+systemctl start mailcatcher
