dd3124cae1391bde7fc0389660d9d236620d7e97
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 13:48:19 -0400

    centos support for utils/sysstat recipe

--- config/provision/recipes/utils/sysstat.sh
+++ config/provision/recipes/utils/sysstat.sh
@@ -1,6 +1,13 @@
 sun.install "sysstat"
 
-sun.backup_compile '/etc/default/sysstat'
+case "$OS" in
+ubuntu)
+  sun.backup_compile '/etc/default/sysstat'
+;;
+centos)
+  sun.backup_compare '/etc/sysconfig/sysstat'
+;;
+esac
 
 systemctl enable sysstat
 systemctl start sysstat
