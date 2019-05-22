2f58af249c93749c48f2b9563b675d9519fc7e23
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 15:20:03 -0400

    java recipe added

---
+++ config/provision/recipes/lang/java/system__JAVA__.sh
@@ -0,0 +1,11 @@
+JAVA_VERSION=<%= @sun.java || '8' %>
+
+case "$OS" in
+ubuntu)
+  sun.install "openjdk-$JAVA_VERSION-jdk-headless"
+;;
+centos)
+  sun.install "java-1.$JAVA_VERSION.0-openjdk"
+  sun.install "java-1.$JAVA_VERSION.0-openjdk-devel"
+;;
+esac
