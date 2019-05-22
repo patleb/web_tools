84a1c4963fe14a97ae6a7ff3ff82a066226886ce
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 13:48:00 -0400

    centos support for utils/goaccess recipe

--- config/provision/recipes/utils/goaccess.sh
+++ config/provision/recipes/utils/goaccess.sh
@@ -1,7 +1,18 @@
-echo "deb http://deb.goaccess.io/ $UBUNTU_CODENAME main" | tee -a /etc/apt/sources.list.d/goaccess.list
-wget -O - http://deb.goaccess.io/gnugpg.key | apt-key add -
-sun.update
+case "$OS" in
+ubuntu)
+  echo "deb http://deb.goaccess.io/ $UBUNTU_CODENAME main" | tee -a /etc/apt/sources.list.d/goaccess.list
+  wget -O - http://deb.goaccess.io/gnugpg.key | apt-key add -
+  sun.update
 
-sun.install "goaccess"
+  sun.install "libgeoip-dev"
+  sun.install "libtokyocabinet-dev"
+  sun.install "goaccess"
+;;
+centos)
+  sun.install "geoip-devel"
+  sun.install "tokyocabinet-devel"
+  sun.install "goaccess"
+;;
+esac
 
 sun.backup_compile '/etc/goaccess.conf'
