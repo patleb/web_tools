a903f5b9f347f0b9ffb2a8dc520f945e64a1d9f5
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 16:42:24 -0400

    addons/geoserver recipe added (wip)

---
+++ config/provision/recipes/addons/geoserver__GEOSERVER__.sh
@@ -0,0 +1,7 @@
+GEOSERVER_VERSION=<%= @sun.geoserver || '2.15.1' %>
+
+wget -q "https://netcologne.dl.sourceforge.net/project/geoserver/GeoServer/$GEOSERVER_VERSION/geoserver-$GEOSERVER_VERSION-bin.zip"
+unzip "geoserver-$GEOSERVER_VERSION-bin.zip"
+mv "geoserver-$GEOSERVER_VERSION" /opt/geoserver
+
+echo 'export GEOSERVER_HOME=/opt/geoserver' >> $HOME/.bashrc
