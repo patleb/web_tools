d1459cd5f6b28abe567fd95b9e6a8f62b3598aaf
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-23 10:20:59 -0400

    addons/geoserver recipe completed

--- config/provision/recipes/addons/geoserver__GEOSERVER__.sh
+++ config/provision/recipes/addons/geoserver__GEOSERVER__.sh
@@ -1,14 +1,20 @@
 DEPLOYER_NAME=<%= @sun.deployer_name %>
 GEOSERVER_VERSION=<%= @sun.geoserver || '2.15.1' %>
 
-wget -q "https://netcologne.dl.sourceforge.net/project/geoserver/GeoServer/$GEOSERVER_VERSION/geoserver-$GEOSERVER_VERSION-bin.zip"
+wget -q "http://sourceforge.net/projects/geoserver/files/GeoServer/$GEOSERVER_VERSION/geoserver-$GEOSERVER_VERSION-bin.zip"
 unzip -q "geoserver-$GEOSERVER_VERSION-bin.zip"
 mv "geoserver-$GEOSERVER_VERSION" /opt/geoserver
 
-echo 'export GEOSERVER_HOME=/opt/geoserver' >> /home/$DEPLOYER_NAME/.bashrc
-chown -R $DEPLOYER_NAME:$DEPLOYER_NAME /opt/geoserver
+sun.move '/etc/systemd/system/geoserver.service'
 
 # https://www.google.ca/search?q=geoserver+behind+nginx&oq=geoserver+behind+nginx
 # proxy through Nginx
-# ufw allow 8080/tcp
-# ufw reload
+<% if @sun.env.vagrant? %>
+  ufw allow 8080/tcp
+  ufw reload
+<% end %>
+
+systemctl enable geoserver
+systemctl start geoserver
+
+# sudo journalctl -u geoserver.service -f
