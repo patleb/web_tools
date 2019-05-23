00b89196437fca1cfe7f28c611dad44a4c3e25c7
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-23 10:46:28 -0400

    more stable geoserver.service + docs

--- config/provision/recipes/addons/geoserver__GEOSERVER__.sh
+++ config/provision/recipes/addons/geoserver__GEOSERVER__.sh
@@ -1,10 +1,10 @@
-DEPLOYER_NAME=<%= @sun.deployer_name %>
 GEOSERVER_VERSION=<%= @sun.geoserver || '2.15.0' %>
 
 wget -q "http://sourceforge.net/projects/geoserver/files/GeoServer/$GEOSERVER_VERSION/geoserver-$GEOSERVER_VERSION-bin.zip"
 unzip -q "geoserver-$GEOSERVER_VERSION-bin.zip"
 mv "geoserver-$GEOSERVER_VERSION" /opt/geoserver
 
+# https://www.shellhacks.com/systemd-service-file-example/
 sun.move '/etc/systemd/system/geoserver.service'
 
 # https://www.google.ca/search?q=geoserver+behind+nginx&oq=geoserver+behind+nginx
@@ -17,4 +17,6 @@ sun.move '/etc/systemd/system/geoserver.service'
 systemctl enable geoserver
 systemctl start geoserver
 
+# https://www.digitalocean.com/community/tutorials/how-to-use-journalctl-to-view-and-manipulate-systemd-logs
+# https://www.loggly.com/ultimate-guide/using-journalctl/
 # sudo journalctl -u geoserver.service -f
