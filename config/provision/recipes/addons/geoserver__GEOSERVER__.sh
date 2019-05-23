86ac9724f2e30c3b37e4adf5b785663383ac8f08
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-22 20:16:16 -0400

    geoserver recipe (wip)

--- config/provision/recipes/addons/geoserver__GEOSERVER__.sh
+++ config/provision/recipes/addons/geoserver__GEOSERVER__.sh
@@ -1,7 +1,14 @@
+DEPLOYER_NAME=<%= @sun.deployer_name %>
 GEOSERVER_VERSION=<%= @sun.geoserver || '2.15.1' %>
 
 wget -q "https://netcologne.dl.sourceforge.net/project/geoserver/GeoServer/$GEOSERVER_VERSION/geoserver-$GEOSERVER_VERSION-bin.zip"
 unzip -q "geoserver-$GEOSERVER_VERSION-bin.zip"
 mv "geoserver-$GEOSERVER_VERSION" /opt/geoserver
 
-echo 'export GEOSERVER_HOME=/opt/geoserver' >> $HOME/.bashrc
+echo 'export GEOSERVER_HOME=/opt/geoserver' >> /home/$DEPLOYER_NAME/.bashrc
+chown -R $DEPLOYER_NAME:$DEPLOYER_NAME /opt/geoserver
+
+# https://www.google.ca/search?q=geoserver+behind+nginx&oq=geoserver+behind+nginx
+# proxy through Nginx
+# ufw allow 8080/tcp
+# ufw reload
