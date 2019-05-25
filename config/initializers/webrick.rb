262e57fbdd4aa1986cb66b65d1010414f8836541
Patrice Lebel <patleb@users.noreply.github.com>
2019-05-25 14:45:24 -0400

    WEBrick corrections

---
+++ config/initializers/webrick.rb
@@ -0,0 +1,17 @@
+if defined?(WEBrick) && (Rails.env.development? || Rails.env.test?) 
+  require 'webrick/httpserver'
+  require 'rack/handler/webrick'
+
+  WEBrick::HTTPServer.class_eval do
+    def access_log(config, req, res)
+      # so assets don't log
+    end
+  end
+
+  Rack::Handler::WEBrick.class_eval do
+    def self.shutdown
+      @server&.shutdown
+      @server = nil
+    end
+  end
+end
