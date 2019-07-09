8053cce2846a8383e9d07621c04cbc148473380c
Patrice Lebel <patleb@users.noreply.github.com>
2019-07-09 18:40:57 -0400

    pgrest nginx config added

--- config/initializers/mr_setting.rb
+++ config/initializers/mr_setting.rb
@@ -17,6 +17,29 @@ Setting.class_eval do
     database_url(self[:pgrest_db_username], self[:pgrest_db_password])
   end
 
+  def self.pgrest_nginx_upstream
+    {
+      pgrest_app: <<-UPSTREAM,
+        server #{self[:pgrest_server]};
+        keepalive 64;
+      UPSTREAM
+    }
+  end
+
+  def self.pgrest_nginx_location
+    {
+      "/#{self[:pgrest_path]}/" => <<-LOCATION,
+        proxy_pass http://pgrest_app/;
+  
+        default_type application/json;
+        proxy_hide_header Content-Location;
+        add_header Content-Location /#{self[:pgrest_path]}/$upstream_http_content_location;
+        proxy_set_header Connection "";
+        proxy_http_version 1.1;
+      LOCATION
+    }
+  end
+
   def self.database_url(user = self[:db_username], pwd = self[:db_password])
     "postgresql://#{user}:#{pwd}@#{self[:db_host] || '127.0.0.1'}:#{self[:db_port] || 5432}/#{self[:db_database]}"
   end
