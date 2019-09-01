ac1a015e05a80eaa79db265c726903fe40784181
Patrice Lebel <patleb@users.noreply.github.com>
2019-09-01 12:25:32 -0400

    Rails 6 new app skips

--- lib/mr_backend.rb
+++ lib/mr_backend.rb
@@ -2,6 +2,5 @@ require 'mr_backend/engine'
 
 module MrBackend
   # --skip-webpack-install --skip-yarn --skip-coffee
-  # --skip-active-storage --skip-action-text --skip-action-mailbox
-  # rails new example_name --database=postgresql --webpack=vue --skip-sprockets --skip-turbolinks --skip-action-cable --skip-puma --skip-spring --skip-listen --skip-system-test
+  # rails new example_name --database=postgresql --webpack=vue --skip-sprockets --skip-turbolinks --skip-action-cable --skip-active-storage --skip-action-text --skip-action-mailbox --skip-puma --skip-spring --skip-listen --skip-system-test
 end
