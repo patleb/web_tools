63a357f5e0dc5756b0c69f7437575f08a3fee773
Patrice Lebel <patleb@users.noreply.github.com>
2019-09-10 14:49:59 -0400

    added :flags option to Sh::Process.(kill|pid)

--- lib/mr_backend/sh/helpers/process.rb
+++ lib/mr_backend/sh/helpers/process.rb
@@ -1,9 +1,9 @@
 module Sh::Process
-  def kill(name, sudo: false, signal: nil)
-    %{#{'sudo' if sudo} bash -c 'process_pid=$(pgrep #{name} -o); if [[ ! -z "$process_pid" ]]; then kill #{signal} $process_pid; fi'}
+  def kill(name, sudo: false, signal: nil, flags: '-o')
+    %{#{'sudo' if sudo} bash -c 'process_pid=$(pgrep #{name} #{flags}); if [[ ! -z "$process_pid" ]]; then kill #{signal} $process_pid; fi'}
   end
 
-  def pid(name)
-    "pgrep #{name} -o"
+  def pid(name, flags: '-o')
+    "pgrep #{name} #{flags}"
   end
 end
