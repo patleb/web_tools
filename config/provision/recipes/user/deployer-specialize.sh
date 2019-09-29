d2e0cb563e441101910123a254eedefbbff08798
Patrice Lebel <patleb@users.noreply.github.com>
2019-09-28 23:07:47 -0400

    add specialize to user recipe

--- config/provision/recipes/user/deployer-specialize.sh
+++ config/provision/recipes/user/deployer-specialize.sh
@@ -2,23 +2,15 @@ DEPLOYER_PATH=/home/$__DEPLOYER_NAME__
 
 case "$OS" in
 ubuntu)
-  adduser $__DEPLOYER_NAME__ --gecos '' --disabled-password
   echo "$__DEPLOYER_NAME__:$__DEPLOYER_PASSWORD__" | sudo chpasswd
-  adduser $__DEPLOYER_NAME__ sudo
 ;;
 centos)
-  adduser $__DEPLOYER_NAME__
   echo -e "$__DEPLOYER_PASSWORD__\n$__DEPLOYER_PASSWORD__" | sudo passwd $__DEPLOYER_NAME__
-  gpasswd -a $__DEPLOYER_NAME__ wheel
 ;;
 esac
 
-mkdir $DEPLOYER_PATH/.ssh
-chmod 700 $DEPLOYER_PATH/.ssh
 <%= Sh.build_authorized_keys(@sun.deployer_name) %>
 chmod 600 $DEPLOYER_PATH/.ssh/authorized_keys
-chown -R $__DEPLOYER_NAME__:$__DEPLOYER_NAME__ $DEPLOYER_PATH
 
-sun.compile "/etc/sudoers.d/deployer" 0440 root:root
 echo -e "$__ADMIN_PRIVATE_KEY__" > "$DEPLOYER_PATH/.ssh/id_rsa"
 chmod 600 $DEPLOYER_PATH/.ssh/id_rsa
