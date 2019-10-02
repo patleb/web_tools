9923464fac52b0c9229790184b8beb2ff4b337a0
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-02 19:08:38 -0400

    ACTIVE filter added

--- sun_cap/lib/sun_cap/server_cluster.rb
+++ sun_cap/lib/sun_cap/server_cluster.rb
@@ -9,7 +9,8 @@ module SunCap
       list.map!{ |name| `vagrant ssh #{name} -c "hostname -I | cut -d' ' -f2" 2>/dev/null`.strip }
     when 'openstack'
       os_vars = Setting.select{ |k, _| k.start_with? 'os_' }.map{ |k, v| "#{k.upcase}='#{v}'" }.join(' ')
-      list = `#{os_vars} openstack --quiet server list | grep '#{Setting[:os_project_name]}' | grep '#{Setting[:server_cluster_name]}' | cut -d'|' -f5 | cut -d'=' -f2`
+      grep = [Setting[:os_project_name], Setting[:server_cluster_name], 'ACTIVE'].map{ |filter| "grep '#{filter}'" }
+      list = `#{os_vars} openstack --quiet server list | #{grep.join(' | ')} | cut -d'|' -f5 | cut -d'=' -f2`
       list.lines.map(&:strip)
     else
       raise UnsupportedClusterProvider
