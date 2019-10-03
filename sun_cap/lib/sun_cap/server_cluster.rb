01d38e023f1b32099b2c1ddaaf45d001247f603f
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-03 10:18:01 -0400

    cleaned-up SunCap#server_cluster

--- sun_cap/lib/sun_cap/server_cluster.rb
+++ sun_cap/lib/sun_cap/server_cluster.rb
@@ -4,13 +4,13 @@ module SunCap
   def self.server_cluster
     case Setting[:server_cluster_provider]
     when 'vagrant'
-      list = `vagrant global-status | grep virtualbox | awk '{ print $2; }'`.lines.map(&:strip)
+      list = `vagrant global-status | grep virtualbox | tr -s [:blank:] | cut -d' ' -f2`.lines.map(&:strip)
       list.select!(&:include?.with(Setting[:server_cluster_name]))
       list.map!{ |name| `vagrant ssh #{name} -c "hostname -I | cut -d' ' -f2" 2>/dev/null`.strip }
     when 'openstack'
       os_vars = Setting.select{ |k, _| k.start_with? 'os_' }.map{ |k, v| "#{k.upcase}='#{v}'" }.join(' ')
-      grep = [Setting[:os_project_name], Setting[:server_cluster_name], 'ACTIVE'].map{ |filter| "grep '#{filter}'" }
-      list = `#{os_vars} openstack --quiet server list | #{grep.join(' | ')} | cut -d'|' -f5 | cut -d'=' -f2`
+      grep = [Setting[:server_cluster_name], 'ACTIVE'].map{ |filter| "grep '#{filter}'" }
+      list = `#{os_vars} openstack -q server list | #{grep.join(' | ')} | cut -d'|' -f5 | cut -d'=' -f2`
       list.lines.map(&:strip)
     else
       raise UnsupportedClusterProvider
