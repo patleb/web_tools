1ca3ea904c28361ad0f9138346cb7d0883bc0298
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-04 21:52:36 -0400

    clean-up

--- lib/mr_backend.rb
+++ lib/mr_backend.rb
@@ -1,6 +1,4 @@
 require 'mr_backend/engine'
 
 module MrBackend
-  # --skip-webpack-install --skip-yarn --skip-coffee
-  # rails new example_name --database=postgresql --webpack=vue --skip-sprockets --skip-turbolinks --skip-action-cable --skip-active-storage --skip-action-text --skip-action-mailbox --skip-puma --skip-spring --skip-listen --skip-system-test
 end
