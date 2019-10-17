7316342e78589793c886bc1535519f0b29aa32a3
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-17 10:24:08 -0400

    ftp server recipe added

---
+++ mr_core/config/provision/recipes/addons/ftp/server.sh
@@ -0,0 +1,33 @@
+### References
+# https://linuxize.com/post/how-to-setup-ftp-server-with-vsftpd-on-centos-7/
+# https://www.digitalocean.com/community/tutorials/how-to-set-up-vsftpd-for-a-user-s-directory-on-ubuntu-16-04
+sun.install "ftp"
+sun.install "vsftpd"
+
+case "$OS" in
+ubuntu)
+  FTP_CONF=/etc/vsftpd.conf
+  export FTP_LIST=/etc/vsftpd.userlist
+;;
+centos)
+  FTP_CONF=/etc/vsftpd/vsftpd.conf
+  export FTP_LIST=/etc/vsftpd/user_list
+;;
+esac
+
+sun.backup_compile $FTP_CONF
+sun.backup_compare $FTP_LIST
+echo $__DEPLOYER_NAME__ >> $FTP_LIST
+
+mkdir -p /home/$__DEPLOYER_NAME__/ftp/${__APP__}/${__ENV__}
+sudo chmod 550 /home/$__DEPLOYER_NAME__/ftp
+sudo chmod 750 /home/$__DEPLOYER_NAME__/ftp/${__APP__}/${__ENV__}
+sudo chown -R $__DEPLOYER_NAME__:$__DEPLOYER_NAME__ /home/$__DEPLOYER_NAME__/ftp
+
+ufw allow 20/tcp
+ufw allow 21/tcp
+ufw allow 30000:31000/tcp
+ufw reload
+
+systemctl enable vsftpd
+systemctl start vsftpd
