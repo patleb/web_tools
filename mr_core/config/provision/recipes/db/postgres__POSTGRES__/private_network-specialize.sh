69797f4c0ff2eddd934b85cef3d6ad0998a74075
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-20 19:09:26 -0400

    ufw delete must accept yes as input

--- mr_core/config/provision/recipes/db/postgres__POSTGRES__/private_network-specialize.sh
+++ mr_core/config/provision/recipes/db/postgres__POSTGRES__/private_network-specialize.sh
@@ -6,7 +6,7 @@ PRIVATE_NETWORK=$(sun.network $INTERNAL_IP $__POSTGRES_PRIVATE_MASK__)/$__POSTGR
 
 RULE=$(ufw status numbered | grep $PRIVATE_NETWORK | awk '{ print $1; }' | tr '[]' ' ')
 if [[ "$RULE" ]]; then
-  ufw delete $RULE
+  yes | ufw delete $RULE
 fi
 
 source 'recipes/db/postgres__POSTGRES__/private_network.sh'
