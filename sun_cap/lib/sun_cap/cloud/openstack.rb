58ab32c3bd780969e6d31f7d1346f1c827ae2d50
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-20 17:19:05 -0400

    ... wasn't useful without volume

--- sun_cap/lib/sun_cap/cloud/openstack.rb
+++ sun_cap/lib/sun_cap/cloud/openstack.rb
@@ -3,15 +3,14 @@ module Cloud::Openstack
   class AlreadyDestroyed < ::StandardError; end
   class DoesNotExist < ::StandardError; end
 
-  def openstack_server_create(flavor, network, project, env, app: nil, version: nil, image: nil, count: 1)
+  def openstack_server_create(flavor, network, project, env, app: nil, version: nil, count: 1)
     tag = [project, env, app].compact.join('_')
     keypair = "ssh-#{[project, env].join('-').dasherize}"
-    snapshot = [project, version].compact.join('-') unless image
+    snapshot = [project, version].compact.join('-')
     raise AlreadyCreated if openstack_server_list(tag).present?
     raise DoesNotExist if openstack_flavor_list(flavor).empty?
     raise DoesNotExist if (network = openstack_network_list(network).first).nil?
-    raise DoesNotExist if !image && (snapshot = openstack_snapshot_list(snapshot).first).nil?
-    raise DoesNotExist if image && (image = openstack_image_list(image).first).nil?
+    raise DoesNotExist if (snapshot = openstack_snapshot_list(snapshot).first).nil?
     raise DoesNotExist if (security_group = openstack_security_group_list(project).first).nil?
     raise DoesNotExist if openstack_keypair_list(keypair).empty?
 
@@ -21,8 +20,7 @@ module Cloud::Openstack
         --nic net-id=#{network[:id]}
         --security-groups #{security_group[:name]}
         --key-name #{keypair}
-        #{"--snapshot #{snapshot[:id]}" if snapshot}
-        #{"--image #{image[:id]}" if image}
+        --snapshot #{snapshot[:id]}
         #{"--min-count #{count}" if count && count > 1}
         #{tag}
     CMD
