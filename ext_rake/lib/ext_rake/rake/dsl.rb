f6c81045cf32136d087a7917dc52007cf626b52a
Patrice Lebel <patleb@users.noreply.github.com>
2019-10-28 14:44:58 -0400

    simpler to use cap_task

--- ext_rake/lib/ext_rake/rake/dsl.rb
+++ ext_rake/lib/ext_rake/rake/dsl.rb
@@ -1,19 +1,21 @@
 module Rake
   module DSL
-    def run_task(name, *args)
-      Rake::Task[name].invoke(*args)
+    def run_task(task_name, *args)
+      Rake::Task[task_name].invoke(*args)
     end
 
-    def run_task!(name, *args)
-      Rake::Task[name].invoke!(*args)
+    def run_task!(task_name, *args)
+      Rake::Task[task_name].invoke!(*args)
     end
 
-    def cap_task(stage, task, environment = {})
+    def cap_task(task_name, environment = {})
+      environment = environment.with_indifferent_access
+      stage = [environment.delete(:env).presence || Rails.env, environment.delete(:app)].compact.join(':')
       environment = environment.each_with_object('RAILS_ENV=development') do |(name, value), string|
         string << " #{name.to_s.upcase}=#{value}"
       end
       cap = File.file?('bin/cap') ? 'bin/cap' : 'bundle exec cap'
-      sh "#{environment} #{cap} #{stage} #{task}"
+      sh "#{environment} #{cap} #{stage} #{task_name}"
     end
   end
 end
