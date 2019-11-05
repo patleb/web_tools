6d03da35b68113df648123407814c0eeaf6767df
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-05 17:01:56 -0500

    Sh#psql correction

--- ext_sql/lib/ext_sql/sh/psql.rb
+++ ext_sql/lib/ext_sql/sh/psql.rb
@@ -1,9 +1,8 @@
 module Sh::Psql
   def psql(command, url = nil, pg_options = nil)
-    sudo = "sudo -u postgres psql -d postgres" if url.nil?
+    sudo = "sudo -u postgres" if url.nil?
     pg_options = "--quiet --no-align --tuples-only --echo-errors #{pg_options}"
     cmd_end = ';' unless command.strip.end_with? ';'
-    url = "'#{url}'" if url
-    %{#{sudo} psql #{pg_options} -c "#{command}#{cmd_end}" #{url}}
+    %{#{sudo} psql #{'-d postgres' if sudo} #{pg_options} -c "#{command}#{cmd_end}" #{"'#{url}'" unless sudo}}
   end
 end
