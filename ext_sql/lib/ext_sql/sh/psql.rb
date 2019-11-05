35ab85b7618b592eda895d70efde3a1bddde3bf9
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-05 17:23:04 -0500

    make sure sudo -u postgres doesn't raise an directory permission error

--- ext_sql/lib/ext_sql/sh/psql.rb
+++ ext_sql/lib/ext_sql/sh/psql.rb
@@ -1,8 +1,9 @@
 module Sh::Psql
   def psql(command, url = nil, pg_options = nil)
     sudo = "sudo -u postgres" if url.nil?
-    pg_options = "--quiet --no-align --tuples-only --echo-errors #{pg_options}"
+    pg_options = "--quiet --tuples-only --no-align --echo-errors #{pg_options}" # "-qtAb"
     cmd_end = ';' unless command.strip.end_with? ';'
-    %{#{sudo} psql #{'-d postgres' if sudo} #{pg_options} -c "#{command}#{cmd_end}" #{"'#{url}'" unless sudo}}
+    url = "'#{url}'" unless sudo
+    %{cd /tmp && #{sudo} psql #{'-d postgres' if sudo} #{pg_options} -c "#{command}#{cmd_end}" #{url} && cd - || cd -}
   end
 end
