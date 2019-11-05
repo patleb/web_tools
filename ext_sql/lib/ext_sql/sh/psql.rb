57287c0eeda22802b977b2b0f525e7ddc11ed48a
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-05 18:00:18 -0500

    Sh.psql usage correction

--- ext_sql/lib/ext_sql/sh/psql.rb
+++ ext_sql/lib/ext_sql/sh/psql.rb
@@ -1,9 +1,8 @@
 module Sh::Psql
   def psql(command, url = nil, pg_options = nil)
-    sudo = "sudo -u postgres" if url.nil?
+    sudo = "cd /tmp && sudo -u postgres" if url.nil?
     pg_options = "--quiet --tuples-only --no-align --echo-errors #{pg_options}" # "-qtAb"
     cmd_end = ';' unless command.strip.end_with? ';'
-    url = "'#{url}'" unless sudo
-    %{cd /tmp && #{sudo} psql #{'-d postgres' if sudo} #{pg_options} -c "#{command}#{cmd_end}" #{url} && cd - || cd -}
+    [sudo, 'psql', ('-d postgres' if url.nil?), pg_options, '-c', '"', command, cmd_end, '"', url].join(' ')
   end
 end
--- ext_sql/lib/tasks/ext_sql/db/pg/base.rb
+++ ext_sql/lib/tasks/ext_sql/db/pg/base.rb
@@ -27,7 +27,7 @@ module Db
 
       def psql(command, *sh_rest, raise_on_exception: false, sudo: false)
         cmd = Sh.psql command, (ExtRake.config.db_url unless sudo)
-        cmd = [cmd].concat(sh_rest).compact.join(' ')
+        cmd = [cmd, *sh_rest].join(' ')
         stdout, stderr, _status = Open3.capture3(cmd)
         notify!(cmd, stderr) if raise_on_exception && notify?(stderr)
         stdout
