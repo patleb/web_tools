e2daa6eed52895ab2a7e12a99c19a45256fae717
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-05 18:39:57 -0500

    clearer

--- ext_sql/lib/ext_sql/sh/psql.rb
+++ ext_sql/lib/ext_sql/sh/psql.rb
@@ -1,8 +1,8 @@
 module Sh::Psql
-  def psql(command, url = nil, pg_options = nil)
-    sudo = "cd /tmp && sudo -u postgres" if url.nil?
-    pg_options = "--quiet --tuples-only --no-align --echo-errors #{pg_options}" # "-qtAb"
-    cmd_end = ';' unless command.strip.end_with? ';'
-    [sudo, 'psql', ('-d postgres' if url.nil?), pg_options, '-c', '"', command, cmd_end, '"', url].join(' ')
+  def psql(command, url = nil, options = nil)
+    psql_cmd = url ? 'psql' : 'cd /tmp && sudo -u postgres psql -d postgres'
+    psql_options = "--quiet --tuples-only --no-align --echo-errors #{options}" # "-qtAb"
+    command_end = ';' unless command.strip.end_with? ';'
+    %{#{psql_cmd} #{psql_options} -c "#{command}#{command_end}" #{url}}
   end
 end
