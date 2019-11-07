5a6123fcb121befb8ce865fa4f6094d5220b8271
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-07 15:14:58 -0500

    md5 added to dump

--- ext_sql/lib/tasks/ext_sql/db/pg/dump.rb
+++ ext_sql/lib/tasks/ext_sql/db/pg/dump.rb
@@ -1,6 +1,3 @@
-# TODO wal backup
-# https://www.cybertec-postgresql.com/en/tech-preview-improving-copy-and-bulkloading-in-postgresql-12/
-# https://www.scalingpostgres.com/tutorials/postgresql-backup-point-in-time-recovery/
 module Db
   module Pg
     class Dump < Base
@@ -8,15 +5,16 @@ module Db
 
       def self.args
         {
-          name:      ['--name=NAME',                'Dump file name (default to dump)'],
-          base_dir:  ['--base-dir=BASE_DIR',        'Dump file(s) base directory (default to ENV["RAILS_ROOT"]/db)'],
-          includes:  ['--includes=INCLUDES', Array, 'Included tables (only for pg_dump and COPY command)'],
-          excludes:  ['--excludes=EXCLUDES', Array, 'Excluded tables (only for pg_dump and COPY command)'],
-          compress:  ['--[no-]compress',            'Compress the dump (default to true)'],
-          split:     ['--[no-]split',               'Compress and split the dump'],
-          physical:  ['--[no-]physical',            'Use pg_basebackup instead of pg_dump'],
-          csv:       ['--[no-]csv',                 'Use COPY command instead of pg_dump'],
-          where:     ['--where=WHERE',              'WHERE condition for the COPY command'],
+          name:     ['--name=NAME',                'Dump file name (default to dump)'],
+          base_dir: ['--base-dir=BASE_DIR',        'Dump file(s) base directory (default to ENV["RAILS_ROOT"]/db)'],
+          includes: ['--includes=INCLUDES', Array, 'Included tables (only for pg_dump and COPY command)'],
+          excludes: ['--excludes=EXCLUDES', Array, 'Excluded tables (only for pg_dump and COPY command)'],
+          compress: ['--[no-]compress',            'Compress the dump (default to true)'],
+          split:    ['--[no-]split',               'Compress and split the dump'],
+          md5:      ['--[no-]md5',                 'Generate md5 file after successful dump'],
+          physical: ['--[no-]physical',            'Use pg_basebackup instead of pg_dump'],
+          csv:      ['--[no-]csv',                 'Use COPY command instead of pg_dump'],
+          where:    ['--where=WHERE',              'WHERE condition for the COPY command'],
         }
       end
 
@@ -30,17 +28,28 @@ module Db
         }
       end
 
-      # TODO https://www.tecmint.com/generate-verify-check-files-md5-checksum-linux/
-      # TODO check resulting dump size if bigger than minimal size
       def dump
         case
         when options.physical then pg_basebackup
         when options.csv      then copy_to
         else pg_dump
         end
+        generate_md5 if options.md5
       end
 
       private
+
+      def generate_md5
+        input = case
+          when options.physical then tar_file
+          when options.csv      then "#{dump_path}~*.csv"
+          else pg_file
+          end.to_s
+        input << '.gz' if !options.physical && compress?
+        input << '-*' if options.split
+        sh "md5sum #{input} | sudo tee #{dump_path}.md5 > /dev/null"
+        sh "sudo md5sum -c #{dump_path}.md5"
+      end
 
       def pg_basebackup
         sh "sudo mkdir -p #{dump_path.dirname}"
@@ -48,7 +57,7 @@ module Db
         cmd_options = <<-CMD.squish
           -P -v -Xstream -cfast -Ft
           #{self.class.pg_options}
-          #{'-z' if options.compress || options.split}
+          #{'-z' if compress?}
         CMD
         output = <<-CMD.squish
           -D #{dump_path}
@@ -121,6 +130,10 @@ module Db
 
       def pg_file
         dump_path.sub_ext('.pg')
+      end
+
+      def compress?
+        options.compress || options.split
       end
 
       def dump_path
