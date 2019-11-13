b780b60715cb04262198850e56f0574f29d575f8
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-13 15:30:09 -0500

    TODO added for geoserver

--- mr_core/config/provision/recipes/addons/geoserver__GEOSERVER__-specialize.sh
+++ mr_core/config/provision/recipes/addons/geoserver__GEOSERVER__-specialize.sh
@@ -1,2 +1,3 @@
 # geoserver doesn't reboot after losing connection on 'sun specialize'
+# TODO set '/etc/systemd/system/geoserver.service' Xmx2048M with __GEOSERVER_MAX_SIZE__
 systemctl start geoserver || :
