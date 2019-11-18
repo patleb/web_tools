5417f48dca65414f1291a30421ec373cc8b53188
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-18 17:48:01 -0500

    geoserver specialize corrections

--- mr_core/config/provision/recipes/addons/geoserver__GEOSERVER__-specialize.sh
+++ mr_core/config/provision/recipes/addons/geoserver__GEOSERVER__-specialize.sh
@@ -1,6 +1,7 @@
 export __GEOSERVER_MAX_SIZE__=${__GEOSERVER_MAX_SIZE__:-2048M}
 
 sun.compile '/etc/systemd/system/geoserver.service'
+systemctl daemon-reload
 
 # geoserver doesn't reboot after losing connection on 'sun specialize'
-systemctl start geoserver || systemctl restart geoserver
+systemctl restart geoserver || systemctl start geoserver || :
