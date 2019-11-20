a86e294585730ec274def729f866ae5474bdc4e3
Patrice Lebel <patleb@users.noreply.github.com>
2019-11-20 09:50:14 -0500

    cleaned-up truncate task

--- ext_sql/lib/tasks/ext_sql/db/pg/truncate.rb
+++ ext_sql/lib/tasks/ext_sql/db/pg/truncate.rb
@@ -3,23 +3,17 @@ module Db
     class Truncate < Base
       def self.args
         super.merge!(
-          includes: ['--includes=INCLUDES', Array, 'Included tables'],
+          includes: ['--includes=INCLUDES', Array, 'Included tables', :required],
         )
       end
 
       def truncate
-        if options.includes.blank?
-          raise "comma separated tables must be specified through --includes option"
-        end
-
-        truncate_tables = options.includes.map do |table|
-          <<~SQL
+        options.includes.each do |table|
+          psql! <<~SQL.squish
             TRUNCATE TABLE #{table};
             SELECT setval(pg_get_serial_sequence('#{table}', 'id'), COALESCE((SELECT MAX(id) + 1 FROM #{table}), 1), false);
           SQL
-        end.join(' ').squish
-
-        psql! truncate_tables
+        end
       end
     end
   end
