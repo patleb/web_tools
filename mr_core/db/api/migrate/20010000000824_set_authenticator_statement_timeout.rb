79ef2580cc08064d1f0f344498b44d56f7c23854
Patrice Lebel <patleb@users.noreply.github.com>
2019-12-18 23:28:44 -0500

    better pgrest db timeout

--- mr_core/db/api/migrate/20010000000824_set_authenticator_statement_timeout.rb
+++ mr_core/db/api/migrate/20010000000824_set_authenticator_statement_timeout.rb
@@ -1,7 +1,16 @@
 class SetAuthenticatorStatementTimeout < ActiveRecord::Migration[5.2]
   def up
+    pgrest_timeout = Setting[:pgrest_timeout]
+    pgrest_timeout = case
+      when pgrest_timeout >= 60_000
+        pgrest_timeout - 10_000
+      when pgrest_timeout >= 30_000
+        pgrest_timeout - 5_000
+      else
+        pgrest_timeout - 1_000
+      end
     execute <<-SQL.strip_sql
-      ALTER ROLE authenticator SET statement_timeout = #{Setting[:pgrest_timeout]};
+      ALTER ROLE authenticator SET statement_timeout = #{pgrest_timeout};
     SQL
   end
 
