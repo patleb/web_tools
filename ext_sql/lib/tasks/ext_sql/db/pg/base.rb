cf52c6dab660236a39cda447e2a2efa222312b12
Patrice Lebel <patleb@users.noreply.github.com>
2020-01-16 12:12:05 -0500

    moved rake psql helpers into ExtRake::Pg::Psql module

--- ext_sql/lib/tasks/ext_sql/db/pg/base.rb
+++ ext_sql/lib/tasks/ext_sql/db/pg/base.rb
@@ -1,6 +1,7 @@
 module Db
   module Pg
     class Base < ActiveTask::Base
+      include ExtRake::Pg::Psql
       include ExtRake::Pg::Rescuable
 
       def self.pg_options
@@ -19,18 +20,6 @@ module Db
 
       def pg_conf_dir
         @pg_conf_dir ||= Pathname.new(psql! 'SHOW data_directory', sudo: true)
-      end
-
-      def psql!(command, *sh_rest, **options)
-        psql(command, *sh_rest, raise_on_exception: true, **options)
-      end
-
-      def psql(command, *sh_rest, raise_on_exception: false, sudo: false)
-        cmd = Sh.psql command, (ExtRake.config.db_url unless sudo)
-        cmd = [cmd, *sh_rest].join(' ')
-        stdout, stderr, _status = Open3.capture3(cmd)
-        notify!(cmd, stderr) if raise_on_exception && notify?(stderr)
-        stdout.strip
       end
     end
   end
