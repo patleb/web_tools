94edb10ead21848a2d59b6d5418c410cee5ffb2e
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-03 15:41:20 -0500

    postgres recipe update for ubuntu 18.04

--- mix_core/config/provision/recipes/db/postgres__POSTGRES__/config.sh
+++ mix_core/config/provision/recipes/db/postgres__POSTGRES__/config.sh
@@ -1,23 +1,27 @@
 __PG_SYNCHRONOUS_COMMIT__=${__PG_SYNCHRONOUS_COMMIT__:-on}
 __PG_FSYNC__=${__PG_FSYNC__:-on}
 __PG_HUGE_PAGES__=${__PG_HUGE_PAGES__:-try}
+# __PG_PORT__=${__PG_PORT__:-5432}
 __PG_MAX_CONNECTIONS__=${__PG_MAX_CONNECTIONS__:-100}
 __PG_MAX_LOCKS_PER_TRANSACTION__=${__PG_MAX_LOCKS_PER_TRANSACTION__:-64}
 PG_CONF_DIR=$(sun.pg_conf_dir)
 
-<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'synchronous_commit' %>
+<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'synchronous_commit =' %>
 echo "synchronous_commit = $__PG_SYNCHRONOUS_COMMIT__" >> "$PG_CONF_DIR/postgresql.conf"
 
-<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'fsync' %>
+<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'fsync =' %>
 echo "fsync = $__PG_FSYNC__" >> "$PG_CONF_DIR/postgresql.conf"
 
-<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'huge_pages' %>
+<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'huge_pages =' %>
 echo "huge_pages = $__PG_HUGE_PAGES__" >> "$PG_CONF_DIR/postgresql.conf"
 
-<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'max_connections' %>
+<%# Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'port =' %>
+# echo "port = $__PG_PORT__" >> "$PG_CONF_DIR/postgresql.conf"
+
+<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'max_connections =' %>
 echo "max_connections = $__PG_MAX_CONNECTIONS__" >> "$PG_CONF_DIR/postgresql.conf"
 
-<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'max_locks_per_transaction' %>
+<%= Sh.delete_lines! '$PG_CONF_DIR/postgresql.conf', 'max_locks_per_transaction =' %>
 echo "max_locks_per_transaction = $__PG_MAX_LOCKS_PER_TRANSACTION__" >> "$PG_CONF_DIR/postgresql.conf"
 
 sun.pg_restart_force
