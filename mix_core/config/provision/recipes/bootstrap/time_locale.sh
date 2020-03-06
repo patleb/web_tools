678d292163276948494c4be765da7817b4a2eec3
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-06 17:09:33 -0500

    LOCALE correction

--- mix_core/config/provision/recipes/bootstrap/time_locale.sh
+++ mix_core/config/provision/recipes/bootstrap/time_locale.sh
@@ -1,5 +1,6 @@
 __TIMEZONE__=${__TIMEZONE__:-Etc/UTC}
 __LOCALE__=${__LOCALE__:-en_US}
+LC="$__LOCALE__.UTF-8"
 
 sun.mute "timedatectl set-timezone $__TIMEZONE__"
 sun.mute "timedatectl set-local-rtc 0"
@@ -12,14 +13,17 @@ fi
 
 case "$OS" in
 ubuntu)
-  sun.mute "locale-gen $__LOCALE__ $__LOCALE__.UTF-8"
+  export LANGUAGE=$LC
+  export LANG=$LC
+  export LC_ALL=$LC
+  sun.mute "locale-gen $__LOCALE__ $LC"
   sun.mute "dpkg-reconfigure locales"
 
   systemctl enable ntp
   systemctl start ntp
 ;;
 centos)
-  sun.mute "localectl set-locale LANG=$__LOCALE__.UTF-8"
+  sun.mute "localectl set-locale LANG=$LC"
 
   systemctl enable ntpd
   systemctl start ntpd
