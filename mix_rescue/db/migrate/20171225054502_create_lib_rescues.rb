1bab50e31a756805269f58470aeca4ce35135923
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-05 19:01:29 -0500

    better searchable columns/index for lib_rescues table

--- mix_rescue/db/migrate/20171225054502_create_lib_rescues.rb
+++ mix_rescue/db/migrate/20171225054502_create_lib_rescues.rb
@@ -4,17 +4,16 @@ class CreateLibRescues < ActiveRecord::Migration[5.1]
 
     unless table_exists? :lib_rescues
       create_table :lib_rescues do |t|
-        t.string   :type,       null: false
-        t.string   :exception,  null: false
-        t.text     :message,    null: false
+        t.citext   :type,       null: false
+        t.citext   :exception,  null: false
+        t.citext   :message,    null: false
         t.jsonb    :data,       null: false, default: {}
 
         t.timestamps
       end
 
-      add_index :lib_rescues, [:type, :exception]
       add_index :lib_rescues, :created_at
-      add_index :lib_rescues, [:message, :data], using: :gist
+      add_index :lib_rescues, [:type, :exception, :message], using: :gin, opclass: { title: :gin_trgm_ops }
     end
   end
 
