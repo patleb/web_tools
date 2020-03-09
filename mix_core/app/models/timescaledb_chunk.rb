115a4dd38bf55ec4c48f0684fe1369e388fc9c34
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-09 17:56:10 -0400

    TimescaleDB model relations added

--- mix_core/app/models/timescaledb_chunk.rb
+++ mix_core/app/models/timescaledb_chunk.rb
@@ -1,7 +1,9 @@
 class TimescaledbChunk < ActiveRecord::Base
   self.primary_key = :id
 
+  belongs_to :table, foreign_key: :table_name, class_name: 'TimescaledbTable'
+
   def readonly?
     true
   end
-end
+end if Setting[:timescaledb_enabled]
--- mix_core/app/models/timescaledb_table.rb
+++ mix_core/app/models/timescaledb_table.rb
@@ -1,7 +1,15 @@
 class TimescaledbTable < ActiveRecord::Base
-  self.primary_key = :id
+  self.primary_key = :name
+
+  has_many :chunks, foreign_key: :table_name, class_name: 'TimescaledbChunk'
 
   def readonly?
     true
   end
-end
+
+  def set_chunk_time_interval(value)
+    self.class.connection.exec_query <<-SQL.strip_sql
+      SELECT set_chunk_time_interval('#{name}', #{value});
+    SQL
+  end
+end if Setting[:timescaledb_enabled]
