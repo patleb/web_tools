efab4185ab385896648fd654ee630a7aad1cc8a7
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-11 17:45:49 -0400

    better :timescaledb_max_bytes defaults

--- mix_core/app/models/timescaledb_chunk.rb
+++ mix_core/app/models/timescaledb_chunk.rb
@@ -1,7 +1,7 @@
 class TimescaledbChunk < ActiveRecord::Base
   include AsTimescaledbView
 
-  MAX_CHUNK_BYTES = Setting[:timescaledb_max_bytes]
+  MAX_BYTES = Setting[:timescaledb_max_bytes]
 
   belongs_to :table, foreign_key: :table_id, class_name: 'TimescaledbTable'
 end if Setting[:timescaledb_enabled]
