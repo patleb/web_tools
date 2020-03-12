9d4bbfbbcb74280ffeace6a122a2c8cfd5831aa3
Patrice Lebel <patleb@users.noreply.github.com>
2020-03-12 13:23:01 -0400

    removed app specific concept of :timescaledb_max_bytes

--- mix_core/app/models/timescaledb_chunk.rb
+++ mix_core/app/models/timescaledb_chunk.rb
@@ -1,8 +1,6 @@
 class TimescaledbChunk < ActiveRecord::Base
   include AsTimescaledbView
 
-  MAX_BYTES = Setting[:timescaledb_max_bytes]
-
   belongs_to :table, foreign_key: :table_id, class_name: 'TimescaledbTable'
 
   delegate :set_next_chunk_size, to: :table
