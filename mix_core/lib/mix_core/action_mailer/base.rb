310f4b36564cd3541ce3181a3df3f43fb0952aa7
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-01 12:04:13 -0400

    correction

--- mix_core/lib/mix_core/action_mailer/base.rb
+++ mix_core/lib/mix_core/action_mailer/base.rb
@@ -1,5 +1,5 @@
 require 'mix_core/action_mailer/base/with_attachment_fix'
-require 'ext_rails/action_mailer/base/with_mail_interceptor' if defined? MailInterceptor
+require 'mix_core/action_mailer/base/with_mail_interceptor' if defined? MailInterceptor
 
 ActionMailer::Base.class_eval do
   include ActiveSupport::LazyLoadHooks::Autorun
