c1ce8dc58b8736a3bc2f4a1699250ba2f1679866
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-21 16:48:49 -0400

    support directory in run_ftp_list method

--- mix_core/lib/mix_core/rake/dsl.rb
+++ mix_core/lib/mix_core/rake/dsl.rb
@@ -2,7 +2,11 @@ module Rake
   module DSL
     def run_ftp_list(match)
       `#{Sh.ftp_list(match)}`.lines.map(&:strip).map(&:split).map do |columns|
-        { size: columns[0].to_i, time: DateTime.parse(columns[1]), name: columns[2] }.with_indifferent_access
+        if columns.size == 3
+          { size: columns[0].to_i, time: DateTime.parse(columns[1]), name: columns[2] }.with_indifferent_access
+        else
+          { time: DateTime.parse(columns[0]), name: columns[1].delete_suffix('/') }.with_indifferent_access
+        end
       end
     end
 
