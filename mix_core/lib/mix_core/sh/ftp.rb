e2dc9eeba00e48c1c63c0bea622b5af90a62d132
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-21 12:29:00 -0400

    allow Sh.ftp settings override

--- mix_core/lib/mix_core/sh/ftp.rb
+++ mix_core/lib/mix_core/sh/ftp.rb
@@ -1,10 +1,10 @@
 module Sh::Ftp
-  def ftp_list(match)
-    ftp "cls #{match} --sort=name --size --date --time-style=%Y-%m-%dT%H:%M:%S%z --sortnocase"
+  def ftp_list(match, **options)
+    ftp "cls #{match} --sort=name --size --date --time-style=%Y-%m-%dT%H:%M:%S%z --sortnocase", **options
   end
 
-  def ftp_cat(match)
-    ftp "cat #{match}"
+  def ftp_cat(match, **options)
+    ftp "cat #{match}", **options
   end
 
   def ftp_download(match, client_dir, **options)
@@ -15,17 +15,17 @@ module Sh::Ftp
     ftp "lcd #{client_dir}; mput -c -d #{match}", **options
   end
 
-  def ftp_remove(match)
-    ftp "mrm -r #{match}"
+  def ftp_remove(match, **options)
+    ftp "mrm -r #{match}", **options
   end
 
-  def ftp_rename(old_name, new_name)
-    ftp "mv #{old_name} #{new_name}"
+  def ftp_rename(old_name, new_name, **options)
+    ftp "mv #{old_name} #{new_name}", **options
   end
 
-  def ftp(command, sudo: false)
+  def ftp(command, sudo: false, username: Setting[:ftp_username], password: Setting[:ftp_password], host: Setting[:ftp_host], host_path: Setting[:ftp_host_path])
     <<~SH
-      #{'sudo' if sudo} lftp -u '#{Setting[:ftp_username]},#{Setting[:ftp_password]}' #{Setting[:ftp_host]}:#{Setting[:ftp_host_path]} <<-FTP
+      #{'sudo' if sudo} lftp -u '#{username},#{password}' #{host}:#{host_path} <<-FTP
         #{command}
       FTP
     SH
