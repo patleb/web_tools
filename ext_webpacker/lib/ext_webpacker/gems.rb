40bfd3b5b4e4d344c3be990f6d8fafa1bdb7ea54
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-27 19:03:11 -0400

    - make sure that webpacker watches symlinks; - use lib/javascript as gem js source path;

--- ext_webpacker/lib/ext_webpacker/gems.rb
+++ ext_webpacker/lib/ext_webpacker/gems.rb
@@ -3,36 +3,26 @@ module ExtWebpacker
     extend self
 
     def install
-      prune
-      (expected_gems - current_gems).each do |name|
-        gem_source_path = Gem.root(name).join(source_path)
-        File.symlink(gem_source_path, source_gems_path.join(name)) if gem_source_path.exist?
+      source_gems_path.mkdir unless source_gems_path.exist?
+      source_gems_path.children.select(&:symlink?).each(&:delete)
+      watched_symlinks = gems.map do |(name, path)|
+        symlink = source_gems_path.join(name)
+        File.symlink(path, symlink)
+        symlink.join("**/*{#{watched_extensions}}").to_s
+      end
+      Webpacker::Compiler.watched_paths.concat(watched_symlinks)
+    end
+
+    def gems
+      @gems ||= begin
+        paths = (default_config['gems'] || []).map{ |name| [name, Gem.root(name).join(gems_source_path)] }
+        paths.select!{ |(_name, path)| path.exist? }
+        paths
       end
     end
 
-    def prune
-      discarded_gems = current_gems - expected_gems
-      symlinks.select{ |symlink| symlink.basename.to_s.in? discarded_gems }.each(&:delete)
-    end
-
-    def current_gems
-      symlinks.map(&:basename).map(&:to_s)
-    end
-
-    def expected_gems
-      default_config['gems'] || []
-    end
-
-    def symlinks
-      root_path.children.select(&:symlink?)
-    end
-
-    def root_path
-      @root_path ||= begin
-        gems_path = Pathname.new(Dir.pwd).join(source_gems_path)
-        gems_path.mkdir unless gems_path.exist?
-        gems_path
-      end
+    def gems_source_path
+      @gems_source_path ||= default_config['gems_source_path'] || 'lib/javascript'
     end
 
     def source_gems_path
@@ -41,6 +31,10 @@ module ExtWebpacker
 
     def source_path
       @source_path ||= Pathname.new(default_config['source_path'])
+    end
+
+    def watched_extensions
+      @watched_extensions ||= default_config['extensions'].join(',')
     end
 
     def default_config
