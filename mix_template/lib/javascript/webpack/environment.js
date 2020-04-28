69eaa0c711bdba3ce15de31da22693722c29984e
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-28 09:17:54 -0400

    make environment.js and package.yml more robust

--- mix_template/lib/javascript/webpack/environment.js
+++ mix_template/lib/javascript/webpack/environment.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const require_module = (name) => require(path.resolve('node_modules', name))
 
@@ -8,6 +9,7 @@ const vue = require('./loaders/vue')
 const coffee =  require('./loaders/coffee')
 const source_path = path.resolve(config.source_path)
 const source_gems_path = path.join(source_path, config.source_gems_path)
+const global_scss_path = path.join(source_path, 'stylesheets/_globals.scss')
 
 switch (process.env.NODE_ENV) {
 case 'development':
@@ -25,12 +27,16 @@ environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('coffee', coffee)
 environment.loaders.get('sass').use.splice(-1, 0, { loader: 'resolve-url-loader' })
-environment.loaders.get('sass').use.push({
-  loader: 'sass-resources-loader',
-  options: { resources: [
-    path.join(source_path, 'stylesheets/_globals.scss')
-  ] }
-})
+try {
+  if (fs.existsSync(global_scss_path)) {
+    environment.loaders.get('sass').use.push({
+      loader: 'sass-resources-loader',
+      options: { resources: [ global_scss_path ] }
+    })
+  }
+} finally {
+  // do nothing
+}
 environment.config.merge({
   resolve: {
     alias: {
