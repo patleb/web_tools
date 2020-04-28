fc3ff5dc5fdf96aab99609b9eaf68fb740be122c
Patrice Lebel <patleb@users.noreply.github.com>
2020-04-28 08:56:17 -0400

    cleaner

--- ext_webpacker/lib/tasks/templates/config/webpack/environment.js
+++ ext_webpacker/lib/tasks/templates/config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { config } = require('@rails/webpacker')
-const path = require('path')
-const source_path = path.resolve(config.source_path)
-const source_gems_path = path.join(source_path, config.source_gems_path)
-const environment = require(path.join(source_gems_path, 'mix_template/webpack/environment'))
+const { resolve } = require('path')
+const environment = require(resolve(config.source_path, config.source_gems_path, 'mix_template/webpack/environment'))
 
 module.exports = environment
