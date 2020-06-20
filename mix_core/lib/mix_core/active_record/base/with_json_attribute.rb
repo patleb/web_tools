5dea93b51ddd37ff2756ea67e56f4755d33f4e31
Patrice Lebel <patleb@users.noreply.github.com>
2020-06-20 18:43:40 -0400

    - make sure that json_attribute default works; - json_column and json_key helpers added;

--- mix_core/lib/mix_core/active_record/base/with_json_attribute.rb
+++ mix_core/lib/mix_core/active_record/base/with_json_attribute.rb
@@ -2,8 +2,37 @@
 module ActiveRecord::Base::WithJsonAttribute
   extend ActiveSupport::Concern
 
+  POSTGRESQL_TYPES = {
+    big_integer: 'BIGINT',
+    boolean: 'BOOLEAN',
+    date: 'DATE',
+    datetime: 'TIMESTAMP',
+    decimal: 'NUMERIC',
+    float: 'DOUBLE PRECISION',
+    integer: 'INTEGER',
+    json: 'jsonb',
+    string: 'TEXT',
+    text: 'TEXT',
+    time: 'TIME',
+  }.with_indifferent_access
+
   prepended do
     class_attribute :jsonb_accessors
+    class_attribute :jsonb_accessors_defaults
+
+    after_initialize :initialize_jsonb_accessors_defaults
+
+    delegate :json_column, :json_key, to: :class
+  end
+
+  def initialize_jsonb_accessors_defaults
+    if jsonb_accessors_defaults&.key? :json_data
+      jsonb_accessors_defaults[:json_data].each do |field, default|
+        next unless send(field).nil?
+        value = default.is_a?(Proc) ? default.call : default
+        send("#{field}=", value)
+      end
+    end
   end
 
   class_methods do
@@ -11,10 +40,24 @@ module ActiveRecord::Base::WithJsonAttribute
       jsonb_accessor(:json_data, field_types)
     end
 
+    def json_column(name)
+      "(#{json_key(name)})::#{POSTGRESQL_TYPES[Array.wrap(jsonb_accessors[:json_data][name]).first]}"
+    end
+
+    def json_key(name)
+      "json_data->'#{name}'"
+    end
+
     def jsonb_accessor(jsonb_attribute, field_types)
       self.jsonb_accessors ||= {}.with_indifferent_access
-      self.jsonb_accessors[jsonb_attribute] ||= []
-      self.jsonb_accessors[jsonb_attribute].concat field_types.keys
+      self.jsonb_accessors[jsonb_attribute] ||= {}.with_indifferent_access
+      field_types.each do |field, type|
+        next unless type.is_a?(Array) && (options = type.last).is_a?(Hash) && options.key?(:default)
+        self.jsonb_accessors_defaults ||= {}.with_indifferent_access
+        self.jsonb_accessors_defaults[jsonb_attribute] ||= {}.with_indifferent_access
+        self.jsonb_accessors_defaults[jsonb_attribute][field] = options.delete(:default)
+      end
+      self.jsonb_accessors[jsonb_attribute].merge! field_types
       super
     end
   end
