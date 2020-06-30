bbf1c7906f06511912057c81003cf75fe08302ed
Patrice Lebel <patleb@users.noreply.github.com>
2020-06-30 15:07:13 -0400

    make sure that new pg data dir is empty

--- mix_core/config/provision/recipes/db/postgres__POSTGRES__/data_directory.sh
+++ mix_core/config/provision/recipes/db/postgres__POSTGRES__/data_directory.sh
@@ -11,6 +11,7 @@ if fdisk -l | grep -Fq $__DATA_PARTITION__; then
   echo "data_directory = '$NEW_PG_DATA_DIR'" >> $PG_CONFIG_FILE
   echo $NEW_PG_DATA_DIR > $(sun.metadata_path 'pg_data_dir')
 
+  rm -rf $NEW_PG_DATA_DIR
   mkdir -p $NEW_PG_DATA_DIR
   chown -R postgres:postgres $NEW_PG_DATA_DIR
   rsync -ah $OLD_PG_DATA_DIR/ $NEW_PG_DATA_DIR
