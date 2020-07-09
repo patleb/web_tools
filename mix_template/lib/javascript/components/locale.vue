8e7111ce8e13261159d2ae22215ec98879f08780
Patrice Lebel <patleb@users.noreply.github.com>
2020-07-09 11:57:03 -0400

    make Vue locale component available under 3 types (button, dropdown, list)

--- mix_template/lib/javascript/components/locale.vue
+++ mix_template/lib/javascript/components/locale.vue
@@ -1,20 +1,33 @@
 <template>
-  <dropdown>
+  <a role="button" v-if="type === 'button'" @click="set_locale(next_locale)">
+    {{ pretty_locale(next_locale) }}
+  </a>
+  <dropdown v-else-if="type === 'dropdown'">
     <btn size="sm" class="dropdown-toggle">
       {{ $i18n.locale }} <i class="fa fa-angle-down"/>
     </btn>
     <template slot="dropdown">
-      <li v-for="locale in ['en', 'fr']" :class="{ active: locale === $i18n.locale }">
+      <li v-for="locale in available_locales" :class="{ active: locale === $i18n.locale }">
         <a role="button" @click="set_locale(locale)">
           {{ pretty_locale(locale) }}
         </a>
       </li>
     </template>
   </dropdown>
+  <ul v-else>
+    <li v-for="locale in available_locales" :class="{ active: locale === $i18n.locale }">
+      <a role="button" @click="set_locale(locale)">
+        {{ pretty_locale(locale) }}
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
   export default {
+    props: [
+      'type',
+    ],
     created: function () {
       this.$i18n.locale = this.$current_params.locale || this.$localStorage.get('locale', navigator.language.substring(0, 2))
     },
@@ -26,6 +39,14 @@
       pretty_locale: function (locale) {
         return this.$i18n.messages[locale]['lang']
       }
+    },
+    computed: {
+      next_locale: function () {
+        return _.find(this.available_locales, l => l !== this.$i18n.locale)
+      },
+      available_locales: function () {
+        return _.keys(this.$i18n.messages).sort()
+      },
     }
   }
 </script>
