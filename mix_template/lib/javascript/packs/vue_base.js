f6675a27c933edc2c56d0fe00e7d3aee90f57d09
Patrice Lebel <patleb@users.noreply.github.com>
2020-07-20 17:38:17 -0400

    pack renamed to vue_base.js and added compatibility imports for older browsers

--- mix_template/lib/javascript/packs/vue_base.js
+++ mix_template/lib/javascript/packs/vue_base.js
@@ -1,7 +1,22 @@
-// https://www.neontsunami.com/posts/import-whole-directory-in-webpacker
-// https://gist.github.com/rossta/5a9edcd7ba37416f8c6f7ed383200b0d
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
 const images = require.context('@/images', true)
 const image_path = (name) => images(name, true)
+
+// https://www.neontsunami.com/posts/import-whole-directory-in-webpacker
+// https://gist.github.com/rossta/5a9edcd7ba37416f8c6f7ed383200b0d
 const consume_js_attribute = (name) => {
   const js_attribute = document.getElementById(`js_${name}`)
   if (js_attribute) {
@@ -10,6 +25,8 @@ const consume_js_attribute = (name) => {
   }
 }
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import _ from 'lodash'
 import lru from 'tiny-lru/lib/tiny-lru'
 import axios from 'axios'
