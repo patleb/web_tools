4a36d5f13738c8c47cb31d853dfefab03c3b48a2
Patrice Lebel <patleb@users.noreply.github.com>
2020-07-27 10:50:09 -0400

    allow layout #extends to accept direct content

--- mix_template/lib/helpers/mix_template/layout_helper.rb
+++ mix_template/lib/helpers/mix_template/layout_helper.rb
@@ -1,7 +1,7 @@
 # Deprecation --> https://github.com/rails/rails/commit/c7820d8124c854760a4d288334f185de2fb99446
 module MixTemplate
   module LayoutHelper
-    def extends(layout, &block)
+    def extends(layout, *args, &block)
       # Make sure it's a string
       layout = layout.to_s
 
@@ -9,7 +9,7 @@ module MixTemplate
       layout = "layouts/#{layout}" unless layout.include?('/')
 
       # Capture the content to be placed inside the extended layout
-      @view_flow.get(:layout).replace capture(&block)
+      @view_flow.get(:layout).replace capture(*args, &block)
 
       render template: layout
     end
