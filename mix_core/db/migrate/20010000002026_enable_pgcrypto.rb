8a5892e4d67c8b15b38bc128f239886c9a054e84
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-03 14:02:50 -0400

    md5_hex sql function generalized

--- mix_core/db/migrate/20010000002026_enable_pgcrypto.rb
+++ mix_core/db/migrate/20010000002026_enable_pgcrypto.rb
@@ -4,20 +4,25 @@ class EnablePgcrypto < ActiveRecord::Migration[6.0]
 
     reversible do |change|
       change.up do
+        # TODO remove DROP line
         execute <<-SQL.strip_sql
-          CREATE OR REPLACE FUNCTION md5_hex(value ANYELEMENT, scope TEXT DEFAULT '') RETURNS TEXT AS $$
+          DROP FUNCTION IF EXISTS md5_hex(value ANYELEMENT, scope TEXT);
+
+          CREATE OR REPLACE FUNCTION md5_hex(VARIADIC args ANYARRAY) RETURNS TEXT AS $$
           BEGIN
-            RETURN scope || encode(digest(value::TEXT, 'md5'), 'hex');
+            RETURN encode(digest(array_to_string(args::TEXT[], '.', '?'), 'md5'), 'hex');
           END;
           $$ LANGUAGE plpgsql IMMUTABLE STRICT PARALLEL SAFE;
 
-          ALTER FUNCTION md5_hex(value ANYELEMENT, scope TEXT) SET search_path=public;
+          ALTER FUNCTION md5_hex(VARIADIC args ANYARRAY) SET search_path=public;
         SQL
       end
 
       change.down do
+        # TODO remove DROP line
         execute <<-SQL.strip_sql
           DROP FUNCTION IF EXISTS md5_hex(value ANYELEMENT, scope TEXT);
+          DROP FUNCTION IF EXISTS md5_hex(VARIADIC args ANYARRAY);
         SQL
       end
     end
