a092431c3a48d03bf9f5a3ec9e9a43a8fae05640
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-03 23:43:14 -0400

    correction

--- ext_ruby/lib/ext_ruby/secure_random.rb
+++ ext_ruby/lib/ext_ruby/secure_random.rb
@@ -1,5 +1,5 @@
 module SecureRandom
-  UUID = /\A[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}\z/.freeze
+  UUID = /\A[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}\z/i.freeze
 
   def self.uuid_v1_to_timestamp(uuid)
     Time.at((uuid_v1_to_100nsecs(uuid).to_d - 122_192_928_000_000_000) / 10_000_000)
