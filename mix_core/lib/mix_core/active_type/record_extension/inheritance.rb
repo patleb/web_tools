b3d1a2312f639168f0d3fcddb67d838842e77aaa
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-10 13:31:40 -0400

    TODO added

--- mix_core/lib/mix_core/active_type/record_extension/inheritance.rb
+++ mix_core/lib/mix_core/active_type/record_extension/inheritance.rb
@@ -1,5 +1,6 @@
 require 'active_type/record_extension/inheritance'
 
+# TODO https://github.com/makandra/active_type/blob/master/CHANGELOG.md --> 1.4.0, i18n key
 ActiveType::RecordExtension::Inheritance::ClassMethods.module_eval do
   alias_method :old_model_name, :model_name
   def model_name
