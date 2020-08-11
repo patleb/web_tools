118f8cc7fd9c99dee0c012cfd2167655c79efd39
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-11 16:28:23 -0400

    prependable + memoized_at tests added

---
+++ mix_core/test/libraries/active_support/core_ext/memoized_at_test.rb
@@ -0,0 +1,39 @@
+require './test/rails_helper'
+
+class SimpleClass
+  include MemoizedAt
+
+  def access_time(**options)
+    m_access(:access_time, **options) do
+      Time.current
+    end
+  end
+end
+
+class MemoizedAtTest < ActiveSupport::TestCase
+  subject{ SimpleClass.new }
+
+  describe '#m_access' do
+    it 'should memoize until threshold passed' do
+      previous = subject.access_time
+      assert_equal subject.access_time, previous
+      travel_to (MemoizedAt::ACCESS_THRESHOLD + 1).seconds.from_now do
+        current = subject.access_time
+        refute_equal current, previous
+        assert_equal subject.access_time, current
+      end
+    end
+
+    it 'should continue to memoized when touch is used' do
+      previous = subject.access_time
+      travel_to (MemoizedAt::ACCESS_THRESHOLD - 1).seconds.from_now do
+        current = subject.access_time(touch: true)
+        assert_equal current, previous
+      end
+      travel_to (MemoizedAt::ACCESS_THRESHOLD + 1).seconds.from_now do
+        current = subject.access_time
+        assert_equal current, previous
+      end
+    end
+  end
+end
---
+++ mix_core/test/libraries/active_support/prependable_test.rb
@@ -0,0 +1,145 @@
+require './test/rails_helper'
+
+class ParentClass
+  def self.overridden(*callers)
+    callers << 'Parent'
+  end
+
+  def overridden(*callers)
+    callers << 'Parent'
+  end
+
+  def redefined(*callers)
+    callers << 'Parent'
+  end
+end
+
+module ExtendModule
+  def overridden(*callers)
+    super(*callers << 'Extend')
+  end
+end
+
+module IncludeModule
+  extend ActiveSupport::Concern
+
+  included do
+    alias_method :not_included_method, :redefined
+    define_method :redefined do |*callers|
+      not_included_method(*callers << 'Included')
+    end
+  end
+
+  class_methods do
+    def overridden(*callers)
+      super(*callers << 'Include')
+    end
+  end
+
+  def overridden(*callers)
+    super(*callers << 'Include')
+  end
+end
+
+module PrependModule
+  extend ActiveSupport::Concern
+
+  prepended do
+    alias_method :not_prepended_method, :redefined
+    define_method :redefined do |*callers|
+      not_prepended_method(*callers << 'Prepended')
+    end
+  end
+
+  class_methods do
+    def overridden(*callers)
+      super(*callers << 'Prepend')
+    end
+  end
+
+  def overridden(*callers)
+    super(*callers << 'Prepend')
+  end
+end
+
+class PrependableTest < ActiveSupport::TestCase
+  let(:overridden_class){ BaseClass.overridden }
+  let(:overridden_instance){ BaseClass.new.overridden }
+  let(:redefined){ BaseClass.new.redefined }
+
+  before do
+    class Object::BaseClass < ParentClass
+      def self.overridden(*callers)
+        super(*callers << 'Base')
+      end
+
+      def overridden(*callers)
+        super(*callers << 'Base')
+      end
+
+      def redefined(*callers)
+        super(*callers << 'Base')
+      end
+    end
+  end
+
+  after do
+    Object.send :remove_const, :BaseClass
+  end
+
+  describe '.extend, .include, .prepend' do
+    before do
+      Object::BaseClass.class_eval do
+        extend ExtendModule
+        include IncludeModule
+        prepend PrependModule
+      end
+    end
+
+    it do
+      assert_equal ["Prepend", "Base", "Include", "Extend", "Parent"], overridden_class
+      assert_equal ["Prepend", "Base", "Include", "Parent"], overridden_instance
+    end
+  end
+
+  describe '.prepend, .include, .extend' do
+    before do
+      Object::BaseClass.class_eval do
+        include IncludeModule
+        extend ExtendModule
+        prepend PrependModule
+      end
+    end
+
+    it do
+      assert_equal ["Prepend", "Base", "Extend", "Include", "Parent"], overridden_class
+      assert_equal ["Prepend", "Base", "Include", "Parent"], overridden_instance
+    end
+  end
+
+  describe '.included, .prepended' do
+    before do
+      Object::BaseClass.class_eval do
+        include IncludeModule
+        prepend PrependModule
+      end
+    end
+
+    it do
+      assert_equal ["Prepended", "Included", "Base", "Parent"], redefined
+    end
+  end
+
+  describe '.prepended, .included' do
+    before do
+      Object::BaseClass.class_eval do
+        prepend PrependModule
+        include IncludeModule
+      end
+    end
+
+    it do
+      assert_equal ["Included", "Prepended", "Base", "Parent"], redefined
+    end
+  end
+end
