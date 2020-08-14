b9cabffb1d0f0fa34f55666deea0599fe35e8d28
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-14 17:28:49 -0400

    correction

--- ext_rake/lib/ext_rake/engine.rb
+++ ext_rake/lib/ext_rake/engine.rb
@@ -7,7 +7,7 @@ module ExtRake
   DONE =      '[DONE]'.freeze
   CANCEL =    '[CANCEL]'.freeze
 
-  class Railtie < Rails::Engine
+  class Engine < Rails::Engine
     require 'mix_rescue'
     require 'ext_rake/rake/dsl'
     require 'ext_rake/rake/task'
