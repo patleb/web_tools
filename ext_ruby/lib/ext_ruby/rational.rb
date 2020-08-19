951072ea89da45492907526e877913b5baadd930
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-19 01:24:26 -0400

    manage limits

--- ext_ruby/lib/ext_ruby/rational.rb
+++ ext_ruby/lib/ext_ruby/rational.rb
@@ -1,5 +1,17 @@
 class Rational
+  class LowerAndUpperNil < ArgumentError; end
+  class LowerGreaterOrEqualToUpper < ArgumentError; end
+
   def self.intermediate(lower, upper)
-    (lower + (upper - lower) / 2.0).rationalize(1e-12)
+    raise LowerAndUpperNil if lower.nil? && upper.nil?
+    raise LowerGreaterOrEqualToUpper if lower >= upper
+
+    if lower.nil? || lower == -Float::INFINITY
+      (upper.ceil - 1.0).to_r
+    elsif upper.nil? || upper == Float::INFINITY
+      (lower.floor + 1.0).to_r
+    else
+      (lower + (upper - lower) / 2.0).rationalize(1e-12)
+    end
   end
 end
