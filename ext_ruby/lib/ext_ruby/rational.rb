be7de977532bd797a289f1502df0a11de72cad6f
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-24 21:58:23 -0400

    Rational#intermediate implemented correctly

--- ext_ruby/lib/ext_ruby/rational.rb
+++ ext_ruby/lib/ext_ruby/rational.rb
@@ -1,17 +1,28 @@
+### References
+# https://github.com/begriffs/pg_rational/blob/master/pg_rational.c
 class Rational
-  class LowerAndUpperNil < ArgumentError; end
+  class LowerOrUpperNegative < ArgumentError; end
   class LowerGreaterOrEqualToUpper < ArgumentError; end
 
-  def self.intermediate(lower, upper)
-    raise LowerAndUpperNil if lower.nil? && upper.nil?
-    raise LowerGreaterOrEqualToUpper if lower >= upper
+  LO = Rational(0, 1).freeze
+  HI = Float::MAX.rationalize.freeze
 
-    if lower.nil? || lower == -Float::INFINITY
-      (upper.ceil - 1.0).to_r
-    elsif upper.nil? || upper == Float::INFINITY
-      (lower.floor + 1.0).to_r
-    else
-      (lower + (upper - lower) / 2.0).rationalize(1e-12)
+  def self.intermediate(lower, upper)
+    lower ||= LO
+    upper ||= HI
+    lower = lower.rationalize unless lower.is_a? Rational
+    upper = upper.rationalize unless upper.is_a? Rational
+    raise LowerOrUpperNegative if lower.negative? || upper.negative?
+    raise LowerGreaterOrEqualToUpper if lower >= upper
+    loop do
+      value = Rational(lower.numerator + upper.numerator, lower.denominator + upper.denominator)
+      if value <=> lower < 1
+        lower = value
+      elsif value <=> upper > -1
+        upper = value
+      else
+        return value
+      end
     end
   end
 end
