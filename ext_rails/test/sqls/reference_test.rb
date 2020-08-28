0a45c7d048eaf11ebbee8dac441d413627a53e0e
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-28 09:27:54 -0400

    spaced migration numbers

--- ext_rails/test/sqls/reference_test.rb
+++ ext_rails/test/sqls/reference_test.rb
@@ -4,9 +4,8 @@ class ReferenceTest < Sql::TestCase
   self.migrations = {
     '20010000002220_enable_pg_counter_cache' => ExtRails::Engine.root,
     '20010000002222_enable_pg_touch' => ExtRails::Engine.root,
-    '20180106225127_add_triggers_to_lib_pages' => MixPage::Engine.root,
-    '20180106225128_add_triggers_to_lib_page_sections' => MixPage::Engine.root,
-    '20180106225129_add_triggers_to_lib_page_fields' => MixPage::Engine.root,
+    '20180106225120_add_triggers_to_lib_pages' => MixPage::Engine.root,
+    '20180106225130_add_triggers_to_lib_page_fields' => MixPage::Engine.root,
   }
   self.sql_root = 'ext_rails/test/sqls'
 
