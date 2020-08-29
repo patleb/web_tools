187fff18a506ef7ae855386c02fe38e6bdc945a5
Patrice Lebel <patleb@users.noreply.github.com>
2020-08-28 22:09:49 -0400

    give access to :template_virtual_path in views

--- mix_template/lib/mix_template/action_controller/base/with_presenter.rb
+++ mix_template/lib/mix_template/action_controller/base/with_presenter.rb
@@ -3,7 +3,7 @@ module ActionController::Base::WithPresenter
 
   included do
     attr_accessor :template_virtual_path
-    helper_method :presenter_class
+    helper_method :template_virtual_path, :presenter_class
   end
 
   def presenter_class
