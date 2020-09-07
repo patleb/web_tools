88f167b6e62d8dabc3d8561a07730a4bee601a15
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-07 02:03:46 -0400

    unique option added to Rescue#enqueue

--- mix_rescue/app/models/rescue.rb
+++ mix_rescue/app/models/rescue.rb
@@ -12,13 +12,13 @@ class Rescue < LibRecord
 
   enum type: MixRescue.config.available_types
 
-  def self.enqueue(exception, message = nil)
+  def self.enqueue(exception, message = nil, unique: false)
     unless exception.class.respond_to? :rescue_class
       exception = RescueError.new(exception)
     end
     message ||= exception.message
     type = exception.class.rescue_class.to_s
-    id = Digest.md5_hex(type, exception.name, message.squish_numbers.squish!)
+    id = unique ? UUID.create.to_s.remove('-') : Digest.md5_hex(type, exception.name, message.squish_numbers.squish!)
     create! id: id, type: type, exception: exception.name, message: message.lines, data: exception.data
     NEW_ERROR
   rescue ActiveRecord::RecordNotUnique
