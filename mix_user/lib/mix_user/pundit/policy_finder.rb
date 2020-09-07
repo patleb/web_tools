44496dfbe403c1317b837c697c7ad75fc78ddb51
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-07 01:43:41 -0400

    fully support STI in Pundit

--- mix_user/lib/mix_user/pundit/policy_finder.rb
+++ mix_user/lib/mix_user/pundit/policy_finder.rb
@@ -4,13 +4,37 @@ module Pundit::PolicyFinder::WithNilableCache
   end
 
   def policy
-    object && "#{find(object)}#{Pundit::SUFFIX}".to_const || ::ApplicationPolicy
+    find_policy(object) || find_parent_policy(object) || ::ApplicationPolicy
   end
 
   private
 
+  def find_policy(object)
+    object && "#{find(object)}#{Pundit::SUFFIX}".to_const
+  end
+
+  def find_parent_policy(object)
+    object && sti_child?(object) && "#{find_parent(object)}#{Pundit::SUFFIX}".to_const
+  end
+
   def find(subject)
     subject.model_name
+  end
+
+  def sti_child?(subject)
+    if subject.is_a? Class
+      subject.sti? && !subject.base_class?
+    else
+      sti_child? subject.class
+    end
+  end
+
+  def find_parent(subject)
+    if subject.is_a? Class
+      subject.base_class.model_name
+    else
+      find_parent(subject.class)
+    end
   end
 end
 
