ea5c435587e4c58a5f11dc4548094d093c568c46
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-15 08:17:44 -0400

    should set @p only if undefined

--- mix_template/lib/mix_template/action_view/template_renderer/with_presenter.rb
+++ mix_template/lib/mix_template/action_view/template_renderer/with_presenter.rb
@@ -4,7 +4,7 @@ module ActionView::TemplateRenderer::WithPresenter
   def render_template(view, template, layout_name, locals)
     Current.view = view
     c = view.controller
-    if c.respond_to? :template_virtual_path
+    if c.respond_to?(:template_virtual_path) && !view.instance_variable_defined?(:@p)
       c.template_virtual_path ||= template.try(:virtual_path)
       view.instance_variable_set :@p, c.presenter_class&.new
     end
