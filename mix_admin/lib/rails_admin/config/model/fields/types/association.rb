5de15eaa96a1e244713f98beff9e8b8e81856b12
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-17 17:16:16 -0400

    correction

--- mix_admin/lib/rails_admin/config/model/fields/types/association.rb
+++ mix_admin/lib/rails_admin/config/model/fields/types/association.rb
@@ -28,7 +28,7 @@ class RailsAdmin::Config::Model::Fields::Association < RailsAdmin::Config::Model
       model = polymorphic? ? RailsAdmin.model(associated) : associated_model # perf optimization for non-polymorphic associations
       wording = model.with(object: associated).object_label
       wording = sanitize(wording) if sanitized?
-      path = object.discarded? && authorized_path_for(:trash, model)
+      path = associated.discarded? && authorized_path_for(:trash, model)
       path ||= authorized_path_for(:show, model, associated)
       path ||= authorized_path_for(:edit, model, associated)
       path ? a_('.pjax', wording, href: path) : ERB::Util.html_escape(wording)
