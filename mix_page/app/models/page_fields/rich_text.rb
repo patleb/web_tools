883f17801270492fcd94046e1f8d77e784ff9075
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-23 13:03:49 -0400

    make sure that empty rich text is an empty string

--- mix_page/app/models/page_fields/rich_text.rb
+++ mix_page/app/models/page_fields/rich_text.rb
@@ -1,4 +1,11 @@
 module PageFields
   class RichText < Text
+    I18n.available_locales.each do |locale|
+      before_validation do
+        if send("text_#{locale}_changed?") && send("text_#{locale}").to_s.gsub(/(<\/?p>|&nbsp;|<br>)/, '').blank?
+          send("text_#{locale}=", '')
+        end
+      end
+    end
   end
 end
