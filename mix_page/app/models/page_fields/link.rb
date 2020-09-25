03d81331a5137d68741db2dd388a5d124739406a
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-24 22:01:39 -0400

    validate :fieldable_type on update

--- mix_page/app/models/page_fields/link.rb
+++ mix_page/app/models/page_fields/link.rb
@@ -4,9 +4,12 @@ module PageFields
 
     json_translate text: [:string, default: ->(record) { record.fieldable&.title }]
 
-    validates :fieldable, presence: true, on: :update
-    I18n.available_locales.each do |locale|
-      validates "text_#{locale}", length: { maximum: 120 }
+    with_options on: :update do
+      validates :fieldable, presence: true
+      validates :fieldable_type, inclusion: { in: ['PageTemplate'] }
+      I18n.available_locales.each do |locale|
+        validates "text_#{locale}", length: { maximum: 120 }
+      end
     end
   end
 end
