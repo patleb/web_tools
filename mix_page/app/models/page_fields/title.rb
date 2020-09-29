33377dd2294d965d5e8f80e44bec08e80f52b878
Patrice Lebel <patleb@users.noreply.github.com>
2020-09-28 21:25:01 -0400

    better default

--- mix_page/app/models/page_fields/title.rb
+++ mix_page/app/models/page_fields/title.rb
@@ -2,7 +2,7 @@ module PageFields
   class Title < Text
     with_options on: :update, unless: :list_changed? do
       I18n.available_locales.each do |locale|
-        validates "text_#{locale}", length: { maximum: 180 }
+        validates "text_#{locale}", length: { maximum: 120 }
       end
     end
   end
