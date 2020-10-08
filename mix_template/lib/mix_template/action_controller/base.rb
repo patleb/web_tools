c20668f70c55b5240c569acb72cbfb1f226120eb
Patrice Lebel <patleb@users.noreply.github.com>
2020-10-07 20:10:22 -0400

    correction

--- mix_template/lib/mix_template/action_controller/base.rb
+++ mix_template/lib/mix_template/action_controller/base.rb
@@ -8,7 +8,11 @@ ActionController::Base.class_eval do
   prepend self::BeforeRenderInstance
 
   def app_root_path
-    @@_app_root_path ||= (main_app.try(:root_path) || '/')
+    if ActionController::Base.class_variable_defined? :@@_app_root_path
+      ActionController::Base.class_variable_get(:@@_app_root_path)
+    else
+      ActionController::Base.class_variable_set(:@@_app_root_path, main_app.try(:root_path) || '/')
+    end
   end
   helper_method :app_root_path
 end
