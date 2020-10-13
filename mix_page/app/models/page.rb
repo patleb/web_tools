c54e74edd9f6da302e7a9048c6a9802d6fc02b77
Patrice Lebel <patleb@users.noreply.github.com>
2020-10-12 20:48:05 -0400

    no need to eager_load

--- mix_page/app/models/page.rb
+++ mix_page/app/models/page.rb
@@ -4,7 +4,7 @@ class Page < LibRecord
 
   has_many :page_fields, -> { order(:position) }, dependent: :destroy
 
-  scope :with_content, -> { includes(page_fields: [:parent, :fieldable]) }
+  scope :with_content, -> { includes(page_fields: :fieldable) }
 
   enum type: {
     'PageLayout'   => 10,
