12661996258812880c08efcfee1481c382eeee90
Patrice Lebel <patleb@users.noreply.github.com>
2020-10-25 23:43:56 -0400

    cron manual testing note added

--- config/schedule.rb
+++ config/schedule.rb
@@ -15,6 +15,10 @@ ExtWhenever.setup(self)
 # every :minute do
 #   bash 'every_minute.sh'
 # end
+#
+# Testing
+# -------
+# sudo run-parts /var/spool/cron/crontabs --regex='^deployer$'
 
 case @environment
 when 'vagrant'
