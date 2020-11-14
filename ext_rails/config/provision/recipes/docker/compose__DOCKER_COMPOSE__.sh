959e182701da2077724571f2364a87300527a924
Patrice Lebel <patleb@users.noreply.github.com>
2020-11-14 16:14:16 -0500

    docker-compose default version updated

--- ext_rails/config/provision/recipes/docker/compose__DOCKER_COMPOSE__.sh
+++ ext_rails/config/provision/recipes/docker/compose__DOCKER_COMPOSE__.sh
@@ -1,4 +1,4 @@
-DOCKER_COMPOSE_VERSION=<%= sun.docker_compose || '1.23.2' %>
+DOCKER_COMPOSE_VERSION=<%= sun.docker_compose || '1.27.4' %>
 
 curl -L https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-`uname -s`-`uname -m` > $HOME/docker-compose
 chmod +x $HOME/docker-compose
