791543f02119f1c13bc36b13dfb493777201153b
Patrice Lebel <patleb@users.noreply.github.com>
2020-12-06 04:37:52 -0500

    use :string instead of :text

--- mix_rescue/db/migrate/20171225054502_create_lib_rescues.rb
+++ mix_rescue/db/migrate/20171225054502_create_lib_rescues.rb
@@ -3,7 +3,7 @@ class CreateLibRescues < ActiveRecord::Migration[6.0]
     create_table :lib_rescues, id: false do |t|
       t.primary_key :id, :text
       t.integer     :type,         null: false, default: 0
-      t.text        :exception,    null: false
+      t.string      :exception,    null: false
       t.text        :message,      null: false, array: true
       t.jsonb       :data,         null: false, default: {}
       t.bigint      :events_count, null: false, default: 1
