81638192568593511f0cc1505287509f059c24c7
Patrice Lebel <patleb@users.noreply.github.com>
2020-12-24 08:50:33 -0500

    correction

--- mix_global/db/migrate/20171213234602_lib_globals_refactor.rb
+++ mix_global/db/migrate/20171213234602_lib_globals_refactor.rb
@@ -1,4 +1,4 @@
-class CreateLibGlobals < ActiveRecord::Migration[6.0]
+class LibGlobalsRefactor < ActiveRecord::Migration[6.0]
   def change
     remove_column :lib_globals, :text
     add_column    :lib_globals, :string, :string
