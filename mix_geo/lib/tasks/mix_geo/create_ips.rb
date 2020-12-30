5b7e03c6431ab6048bc492653a3e5b38a6d78978
Patrice Lebel <patleb@users.noreply.github.com>
2020-12-30 17:29:53 -0500

    clean-up

--- mix_geo/lib/tasks/mix_geo/create_ips.rb
+++ mix_geo/lib/tasks/mix_geo/create_ips.rb
@@ -6,13 +6,9 @@ module MixGeo
     GEO_TABLES           = ['lib_geo_ips', 'lib_geo_cities', 'lib_geo_states', 'lib_geo_countries']
     GEOLITE2_CSV         = 'geolite2-city-ipv4.csv'
     TMP_GEOLITE2_FOLDER  = 'tmp/geolite2'
-    TMP_GEOLITE2_CSV     = "#{TMP_GEOLITE2_FOLDER}/#{GEOLITE2_CSV}"
-    TMP_GEOLITE2_CSV_GZ  = "#{TMP_GEOLITE2_CSV}.gz"
     TMP_GEOLITE2_PREFIX  = "#{TMP_GEOLITE2_FOLDER}-ipv4-"
     GIT_GEOLITE2_CSV_GZ  = "https://raw.githubusercontent.com/sapics/ip-location-db/master/geolite2-city/#{GEOLITE2_CSV}.gz"
     GIT_GEOLITE2_VERSION = "https://raw.githubusercontent.com/sapics/ip-location-db/master/geolite2-city/package.json"
-    GEM_GEOLITE2_CSV     = Gem.root('ip_location_db').join('geolite2-city', GEOLITE2_CSV).to_s
-    GEM_GEOLITE2_CSV_GZ  = "#{GEM_GEOLITE2_CSV}.gz"
 
     def self.args
       {
@@ -39,21 +35,18 @@ module MixGeo
       if options.remote
         @version = Gem::Version.new(JSON.parse(open(GIT_GEOLITE2_VERSION))['version'])
         if @version > version_was
-          IO.copy_stream(open(GIT_GEOLITE2_CSV_GZ), TMP_GEOLITE2_CSV_GZ)
+          @csv_file  = "#{TMP_GEOLITE2_FOLDER}/#{GEOLITE2_CSV}"
+          IO.copy_stream(open(GIT_GEOLITE2_CSV_GZ), gzip_file)
           puts_info 'VERSION', "New GeoLite2 version [#{@version}] downloaded in #{TMP_GEOLITE2_FOLDER} folder"
-          @csv_file  = TMP_GEOLITE2_CSV
-          @gzip_file = TMP_GEOLITE2_CSV_GZ
         end
       elsif options.path.present?
         raise PathWithoutVersion unless options.version.present?
         @version = Gem::Version.new(options.version)
         @csv_file  = "#{options.path}/#{GEOLITE2_CSV}"
-        @gzip_file = "#{@csv_file}.gz"
-        raise PathInvalid unless File.exist? @gzip_file
+        raise PathInvalid unless File.exist? gzip_file
       else
         @version   = Gem.loaded_specs['ip_location_db'].version
-        @csv_file  = GEM_GEOLITE2_CSV
-        @gzip_file = GEM_GEOLITE2_CSV_GZ
+        @csv_file  = Gem.root('ip_location_db').join('geolite2-city', GEOLITE2_CSV).to_s
       end
 
       if @version > version_was
@@ -86,7 +79,7 @@ module MixGeo
     end
 
     def extract_csv_and_split
-      sh 'unpigz', '--keep', @gzip_file, verbose: false
+      sh 'unpigz', '--keep', gzip_file, verbose: false
       sh 'split', '-l', '100000', '-d', '--additional-suffix', '.csv', @csv_file, TMP_GEOLITE2_PREFIX, verbose: false
     end
 
@@ -149,6 +142,10 @@ module MixGeo
 
     def tmp_files_glob
       Dir.glob("#{TMP_GEOLITE2_PREFIX}*")
+    end
+
+    def gzip_file
+      @gzip_file ||= "#{@csv_file}.gz"
     end
   end
 end
