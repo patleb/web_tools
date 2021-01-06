7b590a2eb2abb75bf79b6926b08bb9eaca455cff
Patrice Lebel <patleb@users.noreply.github.com>
2021-01-05 21:49:50 -0500

    correction

--- mix_geo/app/models/geo_ip.rb
+++ mix_geo/app/models/geo_ip.rb
@@ -9,10 +9,11 @@ class GeoIp < LibRecord
 
   def self.select_by_ips(ips)
     connection.exec_query(sanitize_sql_array([<<-SQL.strip_sql, ips]))
-      SELECT #{table_name}.* FROM UNNEST(ARRAY[?]::INET[]) ips(ip)
+      SELECT #{table_name}.* FROM UNNEST(ARRAY[?]::INET[]) WITH ORDINALITY ips(ip, i)
         LEFT JOIN LATERAL (
           SELECT #{table_name}.* FROM #{table_name} WHERE id <= ip ORDER BY id DESC LIMIT 1
         ) #{table_name} ON TRUE
+      ORDER BY i
     SQL
   end
 end
