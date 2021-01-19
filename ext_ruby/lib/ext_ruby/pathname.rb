d43510f74319c1497cd2c0c03685e674347b1d1f
Patrice Lebel <patleb@users.noreply.github.com>
2021-01-19 14:18:16 -0500

    Pathname#children already doing this

--- ext_ruby/lib/ext_ruby/pathname.rb
+++ ext_ruby/lib/ext_ruby/pathname.rb
@@ -31,23 +31,4 @@ end
 
 class Pathname
   prepend self::WithoutRaise
-
-  SYSTEM_BASENAMES = %w(. ..).freeze
-
-  def normalized_entries(hidden: false)
-    root = to_s
-    list = entries.lazy
-    list =
-      if hidden
-        list.reject{ |file| SYSTEM_BASENAMES.include? file.to_s }
-      else
-        list.reject{ |file| file.to_s.start_with? '.' }
-      end
-    list = list.map{ |file| file.expand_path(root) }
-    if block_given?
-      yield list
-    else
-      list.force
-    end
-  end
 end
