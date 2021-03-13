a21161c667066907af24eb58dc1a8b3845edbe57
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-13 11:49:55 -0500

    make sure that nodejs can be updated

--- mix_server/config/provision/recipes/lang/nodejs/system__NODEJS__.sh
+++ mix_server/config/provision/recipes/lang/nodejs/system__NODEJS__.sh
@@ -1,5 +1,9 @@
 __NODEJS__=${__NODEJS__:-14}
 
+if [ -f /etc/apt/sources.list.d/nodesource.list ]; then
+  sun.remove 'nodejs'
+  rm -f /etc/apt/sources.list.d/nodesource.list
+fi
 curl -sL https://deb.nodesource.com/setup_$__NODEJS__.x | bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb [arch=$ARCH] https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
