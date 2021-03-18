51e3b82605cd0e830ad2f551106243d7979648b9
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-17 21:37:28 -0400

    include mix_log and mix_job by default

--- mix_server/lib/mix_server/capistrano.rb
+++ mix_server/lib/mix_server/capistrano.rb
@@ -2,6 +2,7 @@ require 'ext_capistrano/all'
 require 'ext_rails/capistrano'
 require 'ext_whenever/capistrano' if Gem.loaded_specs['ext_whenever']
 require 'mix_backup/capistrano' if Gem.loaded_specs['mix_backup']
+require 'mix_job/capistrano'
 require 'mix_setting/capistrano'
 require 'mix_server/sh'
 require 'mix_server/capistrano/helpers'
