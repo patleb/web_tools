f60ecf24afda69d3d1d202f28154044367c1f77c
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-18 00:38:43 -0400

    postgis correction

--- mix_geo/db/postgis/migrate/20010000000620_enable_postgis.rb
+++ mix_geo/db/postgis/migrate/20010000000620_enable_postgis.rb
@@ -2,5 +2,6 @@ class EnablePostgis < ActiveRecord::Migration[6.0]
   def change
     enable_extension 'postgis'
     enable_extension 'postgis_topology'
+    enable_extension 'postgis_raster'
   end
 end
--- mix_geo/db/postgis/migrate/20010000000622_add_proj_index_to_spatial_ref_sys.rb
+++
@@ -1,5 +0,0 @@
-class AddProjIndexToSpatialRefSys < ActiveRecord::Migration[6.0]
-  def change
-    add_index :spatial_ref_sys, [:srtext, :proj4text], unique: true
-  end
-end
