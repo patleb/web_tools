59cb5c7830cd700bc00ecd7771c0c33f2870ac95
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-27 00:11:19 -0400

    count_estimate sql function added

---
+++ ext_rails/db/migrate/20010000002420_enable_count_estimate.rb
@@ -0,0 +1,26 @@
+### References
+# https://wiki.postgresql.org/wiki/Count_estimate
+class EnableCountEstimate < ActiveRecord::Migration[6.0]
+  def up
+    execute <<-SQL.strip_sql
+      CREATE FUNCTION count_estimate(query text) RETURNS INTEGER AS $$
+      DECLARE
+        line RECORD;
+        size BIGINT;
+      BEGIN
+        FOR line IN EXECUTE 'EXPLAIN ' || query LOOP
+          size = substring(line."QUERY PLAN" FROM ' rows=([[:digit:]]+)');
+          EXIT WHEN size IS NOT NULL;
+        END LOOP;
+        RETURN size;
+      END
+      $$ LANGUAGE plpgsql;
+    SQL
+  end
+
+  def down
+    execute <<-SQL.strip_sql
+      DROP FUNCTION IF EXISTS count_estimate(query text);
+    SQL
+  end
+end
