084e37000116105e17f5544eac6760e61e1fc096
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-27 01:31:53 -0400

    correction and added table_estimate sql method

--- ext_rails/db/migrate/20010000002420_enable_count_estimate.rb
+++ ext_rails/db/migrate/20010000002420_enable_count_estimate.rb
@@ -3,7 +3,7 @@
 class EnableCountEstimate < ActiveRecord::Migration[6.0]
   def up
     execute <<-SQL.strip_sql
-      CREATE FUNCTION count_estimate(query text) RETURNS INTEGER AS $$
+      CREATE FUNCTION count_estimate(query text) RETURNS BIGINT AS $$
       DECLARE
         line RECORD;
         size BIGINT;
@@ -15,12 +15,22 @@ class EnableCountEstimate < ActiveRecord::Migration[6.0]
         RETURN size;
       END
       $$ LANGUAGE plpgsql;
+
+      CREATE FUNCTION table_estimate(name text) RETURNS BIGINT AS $$
+      DECLARE
+        size BIGINT;
+      BEGIN
+        SELECT reltuples::BIGINT INTO size FROM pg_class WHERE oid = name::regclass LIMIT 1;
+        RETURN size;
+      END
+      $$ LANGUAGE plpgsql;
     SQL
   end
 
   def down
     execute <<-SQL.strip_sql
       DROP FUNCTION IF EXISTS count_estimate(query text);
+      DROP FUNCTION IF EXISTS table_estimate(name text);
     SQL
   end
 end
