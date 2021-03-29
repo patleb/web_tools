191d7283c59ecf308ad581c47bd590873cc177a7
Patrice Lebel <patleb@users.noreply.github.com>
2021-03-29 10:33:38 -0400

    still support postgis < 3.0

--- mix_geo/db/postgis/migrate/20010000000620_enable_postgis.rb
+++ mix_geo/db/postgis/migrate/20010000000620_enable_postgis.rb
@@ -2,6 +2,6 @@ class EnablePostgis < ActiveRecord::Migration[6.0]
   def change
     enable_extension 'postgis'
     enable_extension 'postgis_topology'
-    enable_extension 'postgis_raster'
+    enable_extension 'postgis_raster' unless select_value('SELECT postgis_version()').split.first.to_f < 3.0
   end
 end
