41d6729868d70e6b180b5cd389bd5de1ffe9ffc7
Patrice Lebel <patleb@users.noreply.github.com>
2021-04-02 10:07:39 -0400

    correction

--- mix_rescue/config/initializers/rack_attack.rb
+++ mix_rescue/config/initializers/rack_attack.rb
@@ -1,5 +1,5 @@
 Rack::Attack.throttle('rescues:javascripts:ip', limit: 5, period: 1.hour) do |req|
-  if req.post? && req.path == MixRescue.js_routes[:rescue]
+  if req.post? && req.path == MixRescue.routes[:rescue]
     req.ip
   end
 end
