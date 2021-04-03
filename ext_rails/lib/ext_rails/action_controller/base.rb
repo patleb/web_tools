104c5f7476d4cf7352df49f2e45683c59e95c3b9
Patrice Lebel <patleb@users.noreply.github.com>
2021-04-03 14:57:34 -0400

    no need

--- ext_rails/lib/ext_rails/action_controller/base.rb
+++ ext_rails/lib/ext_rails/action_controller/base.rb
@@ -5,9 +5,4 @@ ActionController::Base.class_eval do
   include ActiveSupport::LazyLoadHooks::Autorun
   include ActionController::Redirecting::WithQueryParams
   include self::WithContext
-
-  def params!
-    @_params_bang ||= params.to_unsafe_h.with_keyword_access
-  end
-  helper_method :params!
 end
