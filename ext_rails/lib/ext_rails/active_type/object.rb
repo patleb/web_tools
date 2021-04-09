2d34ccaae6afe61acf3bdae573f224dfdafb1974
Patrice Lebel <patleb@users.noreply.github.com>
2021-04-09 08:08:00 -0400

    attr_enum added to ActiveType::Object

--- ext_rails/lib/ext_rails/active_type/object.rb
+++ ext_rails/lib/ext_rails/active_type/object.rb
@@ -8,5 +8,15 @@ ActiveType::Object.class_eval do
       type = :array if options.delete(:array)
       attribute(name, type, options)
     end
+
+    def attr_enum(default: nil, **definition)
+      enum(definition)
+      name, _values = definition.first
+      default = default.nil? ? nil : default.to_s
+      attribute name, default: proc{ default }
+      define_method "#{name}_for_database" do
+        self.class.send(name.to_s.pluralize)[default]
+      end
+    end
   end
 end
--- mix_user/app/models/user/null.rb
+++ mix_user/app/models/user/null.rb
@@ -1,19 +1,14 @@
 class User::Null < ActiveType::NullObject
-  enum role: MixUser.config.available_roles
+  attr_enum role: MixUser.config.available_roles, default: :null
 
   MixUser.config.json_attributes.each do |name, type|
     attribute name, type
   end
   attribute :discarded, :boolean, default: proc{ false }
-  attribute :role, default: proc{ 'null' }
 
   alias_attribute :user_id, :id
 
   def has?(_record)
     false
-  end
-
-  def role_for_database
-    self.class.roles[:null]
   end
 end
