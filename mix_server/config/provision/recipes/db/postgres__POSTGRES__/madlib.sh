ae5491d09b0cb61c082fc02cb2a77c7fe92b64dc
Patrice Lebel <patleb@users.noreply.github.com>
2021-04-19 23:04:22 -0400

    clean-up

--- mix_server/config/provision/recipes/db/postgres__POSTGRES__/madlib.sh
+++ mix_server/config/provision/recipes/db/postgres__POSTGRES__/madlib.sh
@@ -1,8 +1,4 @@
 # TODO install from binaries instead, too long to build 13 minutes 14 seconds
 # https://gist.github.com/tqyq/955fcf2e505894d59db6b0e771cfe3e8
 # https://dist.apache.org/repos/dist/release/madlib/1.17.0/apache-madlib-1.17.0-bin-Linux.deb
-sun.install "pgxnclient"
-sun.install "$PG_PACKAGES"
-sun.lock "$PG_PACKAGES"
-
 pgxn install madlib
---
+++ mix_server/config/provision/recipes/db/postgres__POSTGRES__/pgxnclient.sh
@@ -0,0 +1,1 @@
+sun.install "pgxnclient"
