6c2f19b37beaa2d53a7b70c3be7c8402b3d8c8d1
Patrice Lebel <patleb@users.noreply.github.com>
2021-04-19 23:11:02 -0400

    better postgres logs

--- mix_server/config/provision/recipes/db/postgres__POSTGRES__/logrotate.sh
+++ mix_server/config/provision/recipes/db/postgres__POSTGRES__/logrotate.sh
@@ -1,10 +1,14 @@
 ### References
 # https://hunleyd.github.io/posts/PostgreSQL-logging-strftime-and-you
 cat >> "$(sun.pg_config_file)" << EOF
+logging_collector = on
+log_directory = '/var/log/postgresql/postgresql-$__POSTGRES__-main'
 log_filename = 'postgresql-%W.log'
 log_truncate_on_rotation = on
 log_rotation_size = 0
 log_rotation_age = 7d
+log_checkpoints = on
+log_lock_waits = on
 EOF
 
 sun.pg_restart_force
