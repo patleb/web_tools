9650c9acee968592ae582986e098a8f8578c790c
Patrice Lebel <patleb@users.noreply.github.com>
2021-05-11 17:14:11 -0400

    use rbenv sudo for cron jobs

--- ext_whenever/lib/ext_whenever/setup.rb
+++ ext_whenever/lib/ext_whenever/setup.rb
@@ -7,8 +7,9 @@ deployer = Dir.pwd.match(/home\/(\w+)\//)[1]
 rbenv_ruby = %{#{Sh.rbenv_export(deployer)}; #{Sh.rbenv_init}}
 context = %{#{rbenv_ruby}; export RAKE_OUTPUT=true; cd :path && :environment_variable=:environment :application_variable=:application}
 flock = %{flock -n #{Whenever.path}/tmp/locks/:task.lock}
-rake = File.file?("#{Whenever.path}/bin/rake") ? 'bin/rake' : ':bundle_command rake'
+rake = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rake"
+runner = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rails runner"
 
 job_type :rake,   "#{context} #{flock} nice -n 19 #{rake} :task --silent :output"
 job_type :bash,   "#{context} #{flock} bash -e -u bin/:task :output"
-job_type :runner, "#{context} #{flock} :bundle_command :runner_command ':task' :output"
+job_type :runner, "#{context} #{flock} #{runner} ':task' :output"
