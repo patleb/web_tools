e946cf831517a233d726ad8070f3dc667458e911
Patrice Lebel <patleb@users.noreply.github.com>
2021-05-27 21:40:34 -0400

    Enumerable#(count|tally)_by methods added

--- ext_ruby/lib/ext_ruby/enumerable.rb
+++ ext_ruby/lib/ext_ruby/enumerable.rb
@@ -4,4 +4,17 @@ module Enumerable
   def stable_sort_by
     sort_by.with_index{ |obj, i| [yield(obj), i] }
   end
+
+  def count_by(sort = nil, &block)
+    result = group_by(&block).transform_values!(&:count)
+    case sort
+    when :desc then result.sort_by(&:last).reverse.to_h
+    when :asc  then result.sort_by(&:last).to_h
+    else result
+    end
+  end
+
+  def tally_by(&block)
+    map(&block).tally
+  end
 end
