410a5072172bf44cd51bb6bbaf130990dfd62948
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-02 02:19:00 -0400

    increase system file limits

--- mix_server/config/provision/recipes/bootstrap/nofile.sh
+++ mix_server/config/provision/recipes/bootstrap/nofile.sh
@@ -13,5 +13,14 @@ echo "*               soft    nproc           65536" >> /etc/security/limits.con
 echo "*               hard    nproc           65536" >> /etc/security/limits.conf
 echo "*               soft    nofile          65536" >> /etc/security/limits.conf
 echo "*               hard    nofile          65536" >> /etc/security/limits.conf
-echo "fs.file-max = 524288" >> /etc/sysctl.conf
+
+#/proc/sys/fs/file-max = 524288
+echo "fs.file-max = 2432476" >> /etc/sysctl.conf
+#/proc/sys/fs/inotify/max_user_watches = 8192
+echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
+#/proc/sys/fs/inotify/max_user_instances = 128
+echo "fs.inotify.max_user_instances = 256" >> /etc/sysctl.conf
+#/proc/sys/fs/inotify/max_queued_events = 16384
+echo "fs.inotify.max_queued_events = 32768" >> /etc/sysctl.conf
+
 sysctl -p
