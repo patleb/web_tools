738ea921742a3ca37c7305d6ba196255339d71b0
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-02 02:19:35 -0400

    don't replace original authorized_keys file

--- mix_server/config/provision/recipes/bootstrap/ssh.sh
+++ mix_server/config/provision/recipes/bootstrap/ssh.sh
@@ -2,7 +2,7 @@ sun.backup_compare '/etc/ssh/sshd_config'
 
 mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
-echo "$__OWNER_PUBLIC_KEY__" > $HOME/.ssh/authorized_keys
+<%= Sh.concat('$HOME/.ssh/authorized_keys', '$__OWNER_PUBLIC_KEY__', escape: false, unique: true) %>
 chmod 600 $HOME/.ssh/authorized_keys
 echo -e "$__OWNER_PRIVATE_KEY__" > $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
