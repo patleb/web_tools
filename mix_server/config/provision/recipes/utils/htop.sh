284fc8f9ec98218d0657723353281db471925995
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-05 15:31:19 -0400

    corrections

--- mix_server/config/provision/recipes/utils/htop.sh
+++ mix_server/config/provision/recipes/utils/htop.sh
@@ -1,11 +1,12 @@
 DEPLOYER_PATH=/home/$__DEPLOYER_NAME__
-CONFIG_PATH="$DEPLOYER_PATH/.config/htop/htoprc"
 
 sun.install "htop"
+bash --rcfile ~/.bashrc -ci 'htop > /dev/null 2>&1'
 sudo su - $__DEPLOYER_NAME__ << 'EOF'
   bash --rcfile ~/.bashrc -ci 'htop > /dev/null 2>&1'
 EOF
 
-sun.backup_defaults $CONFIG_PATH
-<%= Sh.sub! '$CONFIG_PATH', 'hide_userland_threads=0', 'hide_userland_threads=1' %>
-<%= Sh.sub!('$CONFIG_PATH', 'color_scheme=0', 'color_scheme=6') %>
+sun.backup_defaults "$HOME/.config/htop/htoprc"
+sun.backup_defaults "$DEPLOYER_PATH/.config/htop/htoprc"
+<%= Sh.sub! '$HOME/.config/htop/htoprc', 'hide_userland_threads=0', 'hide_userland_threads=1' %>
+<%= Sh.sub! '$DEPLOYER_PATH/.config/htop/htoprc', 'hide_userland_threads=0', 'hide_userland_threads=1' %>
--- mix_server/lib/tasks/mix_server/osquery.rake
+++ mix_server/lib/tasks/mix_server/osquery.rake
@@ -4,6 +4,6 @@ namespace :osquery do
     conf = compile 'config/deploy/templates/osquery/osquery.conf'
     flags = compile 'config/deploy/templates/osquery/osquery.flags'
     sh "sudo mv #{conf} /etc/osquery/osquery.conf"
-    sh "sudo mv #{flags} /etc/osquery/osquery.flags.default"
+    sh "sudo mv #{flags} /etc/osquery/osquery.flags"
   end
 end
