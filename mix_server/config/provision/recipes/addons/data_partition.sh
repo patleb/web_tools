c7066145dd2c68955c7e8763f784d6a7477e257b
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-27 12:55:06 -0400

    correction

--- mix_server/config/provision/recipes/addons/data_partition.sh
+++ mix_server/config/provision/recipes/addons/data_partition.sh
@@ -1,6 +1,5 @@
 # TODO https://ubuntu.com/tutorials/setup-zfs-storage-pool#1-overview
 __DATA_PARTITION__=${__DATA_PARTITION__:-/dev/sdb}
-__DATA_ORDER__=${__DATA_ORDER__:-3}
 if fdisk -l | grep -Fq $__DATA_PARTITION__; then
   if df -h | grep -Fq $__DATA_PARTITION__; then
     : # do nothing --> already mounted
@@ -9,6 +8,6 @@ if fdisk -l | grep -Fq $__DATA_PARTITION__; then
     rm -rf $__DATA_DIRECTORY__
     mkdir -p $__DATA_DIRECTORY__
     mount $__DATA_PARTITION__ $__DATA_DIRECTORY__
-    echo "$__DATA_PARTITION__    $__DATA_DIRECTORY__    auto    defaults,nofail    0    $__DATA_ORDER__" >> /etc/fstab
+    echo "$__DATA_PARTITION__    $__DATA_DIRECTORY__    auto    defaults,nofail    0    2" >> /etc/fstab
   fi
 fi
