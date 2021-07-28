de19af98c9635e0113254c0c94d48adeb299747b
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-27 22:26:51 -0400

    correction

--- ext_whenever/lib/ext_whenever/setup.rb
+++ ext_whenever/lib/ext_whenever/setup.rb
@@ -6,10 +6,11 @@ Setting.load(env: @environment, app: @application)
 deployer = Dir.pwd.match(/home\/(\w+)\//)[1]
 rbenv_ruby = "#{Sh.rbenv_export(deployer)}; #{Sh.rbenv_init}"
 context = "#{rbenv_ruby}; export RAKE_OUTPUT=true; export RAILS_ENV=#{@environment}; export RAILS_APP=#{@application};"
+rbenv_sudo = "/home/#{deployer}/.rbenv/bin/rbenv sudo RAKE_OUTPUT=true :environment_variable=:environment :application_variable=:application"
 path = "cd :path &&"
 flock = "flock -n #{Whenever.path}/tmp/locks/:task.lock"
-rake = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rake"
-runner = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rails runner"
+rake = "#{rbenv_sudo} bin/rake"
+runner = "#{rbenv_sudo} bin/rails runner"
 
 job_type :rake,   "#{context} #{path} #{flock} nice -n 19 #{rake} :task --silent :output"
 job_type :rake!,  "#{context} #{path} #{flock} #{rake} :task --silent :output"
