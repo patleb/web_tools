0c8d00d98f8ed8f6dd495cd6f68f03e6d6891d37
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-28 10:26:58 -0400

    keep tmp/geolite2 around

--- mix_server/lib/mix_server/capistrano.rb
+++ mix_server/lib/mix_server/capistrano.rb
@@ -1,7 +1,8 @@
 require 'ext_capistrano/all'
 require 'ext_rails/capistrano'
-require 'ext_whenever/capistrano' if Gem.loaded_specs['ext_whenever']
-require 'mix_job/capistrano'
+require 'ext_whenever/capistrano'
+require 'mix_geo/capistrano' if Gem.loaded_specs['mix_geo']
+require 'mix_job/capistrano' if Gem.loaded_specs['mix_job']
 require 'mix_setting/capistrano'
 require 'mix_server/sh'
 require 'mix_server/capistrano/helpers'
