9fc201e951ef190ff6fafabd74338837ef2e6a04
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-27 23:29:59 -0400

    cleaner

--- ext_whenever/lib/ext_whenever/setup.rb
+++ ext_whenever/lib/ext_whenever/setup.rb
@@ -3,17 +3,17 @@ set :output, "#{Whenever.path}/log/cron.log"
 
 Setting.load(env: @environment, app: @application)
 
-deployer = Dir.pwd.match(/home\/(\w+)\//)[1]
+deployer   = Dir.pwd.match(/home\/(\w+)\//)[1]
 rbenv_ruby = "#{Sh.rbenv_export(deployer)}; #{Sh.rbenv_init}"
-context = "#{rbenv_ruby}; export RAKE_OUTPUT=true; export RAILS_ENV=#{@environment}; export RAILS_APP=#{@application};"
+context    = "#{rbenv_ruby}; export RAKE_OUTPUT=true; export RAILS_ENV=#{@environment}; export RAILS_APP=#{@application};"
 rbenv_sudo = "/home/#{deployer}/.rbenv/bin/rbenv sudo RAKE_OUTPUT=true :environment_variable=:environment :application_variable=:application"
-path = "cd :path &&"
-flock = "flock -n #{Whenever.path}/tmp/locks/:task.lock"
-rake = "#{rbenv_sudo} bin/rake"
-runner = "#{rbenv_sudo} bin/rails runner"
+path       = "cd :path &&"
+flock      = "flock -n #{Whenever.path}/tmp/locks/:task.lock"
+rake       = "#{rbenv_sudo} bin/rake"
+runner     = "#{rbenv_sudo} bin/rails runner"
 
-job_type :rake,   "#{context} #{path} #{flock} nice -n 19 #{rake} :task --silent :output"
-job_type :rake!,  "#{context} #{path} #{flock} #{rake} :task --silent :output"
-job_type :runner, "#{context} #{path} #{flock} nice -n 19 #{runner} ':task' :output"
+job_type :rake,    "#{context} #{path} #{flock} nice -n 19 #{rake} :task --silent :output"
+job_type :rake!,   "#{context} #{path} #{flock} #{rake} :task --silent :output"
+job_type :runner,  "#{context} #{path} #{flock} nice -n 19 #{runner} ':task' :output"
 job_type :runner!, "#{context} #{path} #{flock} #{runner} ':task' :output"
-job_type :bash,   "#{context} #{path} #{flock} bash -e -u bin/:task :output"
+job_type :bash,    "#{context} #{path} #{flock} bash -e -u bin/:task :output"
