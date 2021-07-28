f3a69d78aeda5d1af74df8fa4b631e9502242486
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-27 22:13:24 -0400

    correction

--- ext_whenever/lib/ext_whenever/setup.rb
+++ ext_whenever/lib/ext_whenever/setup.rb
@@ -4,14 +4,15 @@ set :output, "#{Whenever.path}/log/cron.log"
 Setting.load(env: @environment, app: @application)
 
 deployer = Dir.pwd.match(/home\/(\w+)\//)[1]
-rbenv_ruby = %{#{Sh.rbenv_export(deployer)}; #{Sh.rbenv_init}}
-context = %{#{rbenv_ruby}; export RAKE_OUTPUT=true; cd :path && :environment_variable=:environment :application_variable=:application}
-flock = %{flock -n #{Whenever.path}/tmp/locks/:task.lock}
+rbenv_ruby = "#{Sh.rbenv_export(deployer)}; #{Sh.rbenv_init}"
+context = "#{rbenv_ruby}; export RAKE_OUTPUT=true; export RAILS_ENV=#{@environment}; export RAILS_APP=#{@application}"
+path = "cd :path &&"
+flock = "flock -n #{Whenever.path}/tmp/locks/:task.lock"
 rake = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rake"
 runner = "/home/#{deployer}/.rbenv/bin/rbenv sudo bin/rails runner"
 
-job_type :rake,   "#{context} #{flock} nice -n 19 #{rake} :task --silent :output"
-job_type :rake!,  "#{context} #{flock} #{rake} :task --silent :output"
-job_type :runner, "#{context} #{flock} nice -n 19 #{runner} ':task' :output"
-job_type :runner!, "#{context} #{flock} #{runner} ':task' :output"
-job_type :bash,   "#{context} #{flock} bash -e -u bin/:task :output"
+job_type :rake,   "#{context} #{path} #{flock} nice -n 19 #{rake} :task --silent :output"
+job_type :rake!,  "#{context} #{path} #{flock} #{rake} :task --silent :output"
+job_type :runner, "#{context} #{path} #{flock} nice -n 19 #{runner} ':task' :output"
+job_type :runner!, "#{context} #{path} #{flock} #{runner} ':task' :output"
+job_type :bash,   "#{context} #{path} #{flock} bash -e -u bin/:task :output"
