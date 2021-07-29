12d5321072bca97bb320dbb24612f5791a16d455
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-29 08:23:51 -0400

    correction (host snapshot)

--- mix_check/app/models/checks/linux/host.rb
+++ mix_check/app/models/checks/linux/host.rb
@@ -39,8 +39,9 @@ module Checks
       def self.capture
         last_updated_at = LogLines::Host.last_messages(text_tiny: host.private_ip).pick(:updated_at)
         if last_updated_at.nil? || last_updated_at < (Setting[:check_host_interval] - 30.seconds).ago
-          Global.write! snapshot_key, host.build_snapshot
+          snapshot = host.build_snapshot
           Log.host(current)
+          Global.write! snapshot_key, snapshot
           reset
         end
       end
