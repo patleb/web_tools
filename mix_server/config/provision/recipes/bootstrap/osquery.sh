842628493c3b5af0c44d717527090db4879e65cb
Patrice Lebel <patleb@users.noreply.github.com>
2021-07-30 21:13:37 -0400

    corrections

--- mix_server/config/provision/recipes/bootstrap/osquery.sh
+++ mix_server/config/provision/recipes/bootstrap/osquery.sh
@@ -1,8 +1,9 @@
 export OSQUERY_KEY=1484120AC4E9F8A1A577AEEE97A80C63C9D8B80B
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys $OSQUERY_KEY
 add-apt-repository 'deb [arch=amd64] https://pkg.osquery.io/deb deb main'
-apt-get update
-apt-get install osquery
+sun.update
+
+sun.install "osquery"
 
 sun.backup_compare "/usr/share/osquery/osquery.example.conf"
 sun.backup_compare "/usr/share/osquery/packs/hardware-monitoring.conf"
--- mix_server/config/provision/recipes/lang/crystal/system__CRYSTAL__.sh
+++ mix_server/config/provision/recipes/lang/crystal/system__CRYSTAL__.sh
@@ -1,5 +1,5 @@
 curl -sL "https://keybase.io/crystal/pgp_keys.asc" | apt-key add -
 echo "deb [arch=$ARCH] https://dist.crystal-lang.org/apt crystal main" | tee /etc/apt/sources.list.d/crystal.list
-sudo apt-get update
+sun.update
 
 sun.install "crystal"
