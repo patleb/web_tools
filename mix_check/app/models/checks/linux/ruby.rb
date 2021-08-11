7ad8a605c3697ee91e1b33d25b6e0c6c2a7fd59f
Patrice Lebel <patleb@users.noreply.github.com>
2021-08-11 13:22:37 -0400

    don't check ruby pids if app in maintenance or disabled

--- mix_check/app/models/checks/linux/ruby.rb
+++ mix_check/app/models/checks/linux/ruby.rb
@@ -6,6 +6,9 @@ module Checks
       end
 
       def pids_issue?
+        return unless File.exist? "/etc/nginx/sites-enabled/#{MixServer.config.deploy_dir}"
+        return unless (nginx_conf = Pathname.new("/etc/nginx/sites-available/#{MixServer.config.deploy_dir}")).exist?
+        return unless nginx_conf.readlines.none?{ |line| line.match? /^\s*return 503;/ }
         return unless self.class.host.uptime > Setting[:check_interval]
         return unless (Time.current - Server.current.created_at) > Setting[:check_interval]
         minimum = MixServer.config.minimum_workers + Setting[:check_from_cron].to_i
