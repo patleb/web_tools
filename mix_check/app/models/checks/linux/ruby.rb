3c7fe97269d48367f11bb7eede06424d2c442e66
Patrice Lebel <patleb@users.noreply.github.com>
2021-08-11 09:34:11 -0400

    ruby pids check correction

--- mix_check/app/models/checks/linux/ruby.rb
+++ mix_check/app/models/checks/linux/ruby.rb
@@ -6,8 +6,10 @@ module Checks
       end
 
       def pids_issue?
+        return unless self.class.host.uptime > Setting[:check_interval]
+        return unless (Time.current - Server.current.created_at) > Setting[:check_interval]
         minimum = MixServer.config.minimum_workers + Setting[:check_from_cron].to_i
-        pids < minimum && self.class.host.uptime > (Setting[:check_interval] - 30.seconds)
+        pids < minimum
       end
     end
   end
