3dee47d3a91bf0aee45aa63898e65393d810ba6d
Patrice Lebel <patleb@users.noreply.github.com>
2021-08-16 02:24:53 -0400

    Host rollups correction

--- mix_check/app/models/log_lines/host.rb
+++ mix_check/app/models/log_lines/host.rb
@@ -45,15 +45,12 @@ module LogLines
 
     def self.rollups
       %i(week day).each_with_object({}) do |period, result|
-        groups = group_by_period(period).calculate(LogRollups::Host::OPERATIONS)
-        result[[period, :period]] = groups.transform_values! do |group|
-          group.map! do |rows|
-            next rows unless rows.is_a? Array
-            rows.map!.with_index do |value, i|
-              next value.ceil(3) if rollups_keys[i] == :load_avg
-              next value.ceil(2) if rollups_type(i) == :float
-              value
-            end
+        group = group_by_period(period).calculate(LogRollups::Host::OPERATIONS)
+        result[[period, :period]] = group.transform_values! do |row|
+          row.map!.with_index do |value, i|
+            next value.ceil(3) if rollups_keys[i] == :load_avg
+            next value.ceil(2) if rollups_type(i) == :float
+            value
           end
         end
       end
