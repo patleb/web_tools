45619e1c0950f3e0a044c4295f4d240de4742786
Patrice Lebel <patleb@users.noreply.github.com>
2021-08-19 22:23:07 -0400

    correction for unlogged tables

--- mix_log/db/migrate/20201218123509_create_lib_log_lines.rb
+++ mix_log/db/migrate/20201218123509_create_lib_log_lines.rb
@@ -7,10 +7,15 @@ class CreateLibLogLines < ActiveRecord::Migration[6.0]
 
     change_table :lib_log_lines do |t|
       t.integer    :type,        null: false
-      t.belongs_to :log,         null: false,  index: false, foreign_key: { to_table: :lib_logs }
-      t.belongs_to :log_message, index: false, foreign_key: { to_table: :lib_log_messages }
+      t.belongs_to :log,         null: false,  index: false, foreign_key: false
+      t.belongs_to :log_message, index: false, foreign_key: false
       t.integer    :pid
       t.jsonb      :json_data,   null: false, default: {}
+    end
+
+    unless ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.create_unlogged_tables
+      add_foreign_key :lib_log_lines, :lib_logs, column: :log_id
+      add_foreign_key :lib_log_lines, :lib_log_messages, column: :log_message_id
     end
 
     add_index :lib_log_lines, [:created_at, :type, :log_id, :log_message_id, :pid, :json_data], using: :gin,
