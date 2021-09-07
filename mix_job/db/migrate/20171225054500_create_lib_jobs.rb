1ee1955d4dab9a24f22d22c2fc53105a7b053b72
Patrice Lebel <patleb@users.noreply.github.com>
2021-09-07 01:44:26 -0400

    typo

--- mix_job/db/migrate/20171225054500_create_lib_jobs.rb
+++ mix_job/db/migrate/20171225054500_create_lib_jobs.rb
@@ -6,7 +6,7 @@ class CreateLibJobs < ActiveRecord::Migration[6.0]
       t.integer     :priority,     null: false, default: 0
       t.timestamp   :scheduled_at, null: false
       t.jsonb       :json_data,    null: false, default: {}
-      t.timestamp   :created_at,  null: false
+      t.timestamp   :created_at,   null: false
     end
 
     add_index :lib_jobs, [:queue_name, :priority, :scheduled_at], order: { priority: :desc }
