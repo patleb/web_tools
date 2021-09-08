aaaf3f1519a967c4d105f83e3836d212ba628c80
Patrice Lebel <patleb@users.noreply.github.com>
2021-09-08 01:25:50 -0400

    correction

--- mix_server/config/provision/recipes/utils/htop.sh
+++ mix_server/config/provision/recipes/utils/htop.sh
@@ -10,3 +10,4 @@ sun.backup_defaults "$HOME/.config/htop/htoprc"
 sun.backup_defaults "$DEPLOYER_PATH/.config/htop/htoprc"
 <%= Sh.sub! '$HOME/.config/htop/htoprc', 'hide_userland_threads=0', 'hide_userland_threads=1' %>
 <%= Sh.sub! '$DEPLOYER_PATH/.config/htop/htoprc', 'hide_userland_threads=0', 'hide_userland_threads=1' %>
+chown -R $__OWNER_NAME__:$__OWNER_NAME__ "$HOME/.config"
