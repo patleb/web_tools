e9b75f02382c5a316207efe65a754461a547abd9
Patrice Lebel <patleb@users.noreply.github.com>
2021-09-28 05:44:16 -0400

    clean-up

--- mix_check/lib/mix_check/configuration.rb
+++ mix_check/lib/mix_check/configuration.rb
@@ -1,14 +1,6 @@
 module MixCheck
   has_config do
-    attr_writer :available_types
     attr_writer :available_workers
-
-    def available_types
-      @available_types ||= {
-        'Checks::Postgres::Database' => 1,
-        'Checks::Linux::Host' => 2,
-      }
-    end
 
     def available_workers
       @available_workers ||= ['ruby', 'postgres']
