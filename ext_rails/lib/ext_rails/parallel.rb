e11c47c823158a48adb884c1ab4d61f10205af28
Patrice Lebel <patleb@users.noreply.github.com>
2021-09-29 16:23:55 -0400

    correction

--- ext_rails/lib/ext_rails/parallel.rb
+++ ext_rails/lib/ext_rails/parallel.rb
@@ -6,10 +6,7 @@ module Parallel::WithActiveRecord
       options = options.dup
       return super if (ar_bases = Array.wrap(options.delete(:ar_base))).empty?
       begin
-        super(source, options) do |*args|
-          @ar_reconnected ||= ar_bases.map{ |ar_base| ar_base.connection.reconnect! } || true
-          block.call(*args)
-        end
+        super
       ensure
         ar_bases.map{ |ar_base| ar_base.connection.reconnect! }
       end
