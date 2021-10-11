d563431745cbf635686ba3679b8532c3436cd48e
Patrice Lebel <patleb@users.noreply.github.com>
2021-10-11 13:02:01 -0400

    mix_setting.cap moved to mix_server + clean-up

--- mix_server/lib/mix_server/capistrano.rb
+++ mix_server/lib/mix_server/capistrano.rb
@@ -4,7 +4,6 @@ require 'ext_whenever/capistrano'
 require 'mix_geo/capistrano' if Gem.loaded_specs['mix_geo']
 require 'mix_job/capistrano' if Gem.loaded_specs['mix_job']
 require 'mix_file/capistrano' if Gem.loaded_specs['mix_file']
-require 'mix_setting/capistrano'
 require 'mix_server/sh'
 require 'mix_server/capistrano/helpers'
 include MixServer::Helpers
--- mix_setting/lib/mix_setting/capistrano.rb
+++
@@ -1,1 +0,0 @@
-load 'tasks/mix_setting.cap'
