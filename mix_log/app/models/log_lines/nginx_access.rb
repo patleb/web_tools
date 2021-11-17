326902d50cd3a7bb7d917b292fa4f87c20ad0427
Patrice Lebel <patleb@users.noreply.github.com>
2021-11-17 07:27:27 -0500

    faster check

--- mix_log/app/models/log_lines/nginx_access.rb
+++ mix_log/app/models/log_lines/nginx_access.rb
@@ -220,7 +220,7 @@ module LogLines
         gzip: gzip == '-' ? nil : gzip.to_f,
       }
       global_log = log.path&.end_with?('/access.log')
-      if global_log || status == 404 || path.end_with?(*MixLog.config.filter_endings) || MixLog.config.filter_ips.include?(ip)
+      if global_log || status == 404 || MixLog.config.filter_ips.include?(ip) || path.end_with?(*MixLog.config.filter_endings)
         method, path, params = nil, '*', nil
       end
       if global_log
