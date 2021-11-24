cf493f9b1bcc93c52621bb11bc93a56bd67422fe
Patrice Lebel <patleb@users.noreply.github.com>
2021-11-24 09:50:44 -0500

    main mailer correction

--- mix_email/app/jobs/main_mailer_job.rb
+++ mix_email/app/jobs/main_mailer_job.rb
@@ -1,5 +1,2 @@
 class MainMailerJob < ActionMailer::MailDeliveryJob
-  def perform(mailer, mail_method, delivery_method, options = {})
-    super(mailer, mail_method, delivery_method, **options)
-  end
 end
