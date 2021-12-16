92c6b88dc555cc5810f7f938caff3467fa699197
Patrice Lebel <patleb@users.noreply.github.com>
2021-12-16 01:20:23 -0500

    do not log nginx forbidden errors

--- mix_log/lib/mix_log/configuration.rb
+++ mix_log/lib/mix_log/configuration.rb
@@ -102,6 +102,7 @@ module MixLog
           'SSL routines:tls_early_post_process_client_hello:unsupported protocol',
           'SSL routines:tls_parse_ctos_key_share:bad key share',
           'SSL routines:tls_choose_sigalg:internal error',
+          'access forbidden by rule',
         ],
         info: [
           '[passenger_native_support.so] trying to compile for the current user',
