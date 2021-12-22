65686476b68b337c666662f019990ff314636be3
Patrice Lebel <patleb@users.noreply.github.com>
2021-12-22 15:19:10 -0500

    for some reason, Current.user could be nil (investigate)

--- mix_page/app/controllers/pages_controller.rb
+++ mix_page/app/controllers/pages_controller.rb
@@ -122,7 +122,7 @@ class PagesController < MixPage.config.parent_controller.constantize
   def stale_state?
     return true unless pjax?
     return true if Rails.env.dev_or_test?
-    return true if Current.user.role_admin?
+    return true if Current.user&.role_admin?
     stale? @state, etag: MixServer.current_version, template: @state.template
   end
 
