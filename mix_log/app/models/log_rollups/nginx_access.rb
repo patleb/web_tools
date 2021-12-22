f137c33ad1775a7326d0f4d66e62e725a332e000
Patrice Lebel <patleb@users.noreply.github.com>
2021-12-21 20:21:00 -0500

    useful LogRollups::NginxAccess requests

--- mix_log/app/models/log_rollups/nginx_access.rb
+++ mix_log/app/models/log_rollups/nginx_access.rb
@@ -33,4 +33,43 @@ class LogRollups::NginxAccess < LogRollup
     browser: 60,
     platform: 70,
   }
+
+  def self.requests_begin_at
+    where(period: 1.day).order(:period_at).pick(:period_at)&.utc
+  end
+
+  def self.requests_end_at
+    where(period: 1.day).order(period_at: :desc).pick(:period_at)&.utc
+  end
+
+  def self.total_requests
+    where(period: 1.week).period.order(period_at: :desc).sum(:requests).to_i
+  end
+
+  def self.total_bytes_out
+    where(period: 1.week).period.sum(:bytes_out).to_s(:human_size)
+  end
+
+  def self.requests_by(period_or_group)
+    case period_or_group
+    when :month
+      period.where(period: 1.day).group_by_period(:month, column: :period_at, reverse: true).sum(:requests).map do |(date, sum)|
+        [date.strftime('%Y-%m-%d'), sum.to_i]
+      end
+    when :week, :day
+      period.where(period: 1.send(period_or_group)).order(period_at: :desc).map do |row|
+        [row.period_at.strftime('%Y-%m-%d'), row.requests]
+      end
+    else
+      send(period_or_group).top_group_calculate(:group_value, :sum, column: :requests).map do |(group, sum)|
+        [group, sum.to_i]
+      end
+    end
+  end
+
+  def self.users_by(period = :week)
+    joins(:log).period.where(period: 1.send(period), log: { path: MixLog.config.passenger_log_path(:access) }).order(period_at: :desc).map do |row|
+      [row.period_at.strftime('%Y-%m-%d'), row.users]
+    end
+  end
 end
