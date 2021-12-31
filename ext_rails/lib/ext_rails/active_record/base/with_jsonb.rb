e7e97ed5f5ee2a1c05385d1ed738299aef635b5f
Patrice Lebel <patleb@users.noreply.github.com>
2021-12-31 10:19:35 -0500

    prevent exception when migrations aren't applied

--- ext_rails/lib/ext_rails/active_record/base/with_jsonb.rb
+++ ext_rails/lib/ext_rails/active_record/base/with_jsonb.rb
@@ -1,9 +1,17 @@
 module ActiveRecord::Base::WithJsonb
   extend ActiveSupport::Concern
 
+  class UndefinedTable < ActiveRecord::StatementInvalid
+    def self.===(exception)
+      exception.message.match? /PG::UndefinedTable/
+    end
+  end
+
   class_methods do
     def serialize(attr_name, class_name_or_coder = Object, **options)
       return if columns_hash[attr_name.to_s].sql_type == 'jsonb'
+      super
+    rescue UndefinedTable
       super
     end
   end
