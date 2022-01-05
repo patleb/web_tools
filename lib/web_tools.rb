cf7d57acf0cc2616c5bc7ec4e0e2ea88de186c76
Patrice Lebel <patleb@users.noreply.github.com>
2022-01-05 06:05:54 -0500

    use root gem if there is no subgems

--- lib/web_tools.rb
+++ lib/web_tools.rb
@@ -4,13 +4,13 @@ module WebTools
   def self.private_gems
     @private_gems ||= (!File.exists?('Gemfile.private') ? [] : File.readlines('Gemfile.private')
       .select_map{ |line| line.match(/(?:^| +)gem +["']([^"']+)["']/)&.captures&.first })
-      .flat_map{ |name| tools Gem.root(name) }
+      .flat_map{ |name| subgems Gem.root(name) }
       .map{ |d| [d, Gem.root(d)] }
       .to_h.with_indifferent_access
   end
 
   def self.gems
-    @gems ||= tools(root)
+    @gems ||= subgems(root)
       .map{ |d| [d, Gem.root(d)] }
       .to_h.with_indifferent_access
   end
@@ -19,8 +19,11 @@ module WebTools
     @root ||= Pathname.new(File.dirname(__dir__))
   end
 
-  def self.tools(root)
-    root.children.select{ |d| d.directory? && d.children.any?{ |f| f.to_s.end_with? '.gemspec' } }.map{ |d| d.basename.to_s }
+  def self.subgems(root)
+    list = root.children.select do |d|
+      d.directory? && d.children.any?{ |f| f.to_s.end_with? '.gemspec' }
+    end
+    list.any? ? list.map{ |d| d.basename.to_s } : root.basename.to_s
   end
-  private_class_method :tools
+  private_class_method :subgems
 end
