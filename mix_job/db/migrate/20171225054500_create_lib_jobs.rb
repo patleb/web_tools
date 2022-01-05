6ed22d36ccd6e9fbc32c7ec23fcce9af3bffd8de
Patrice Lebel <patleb@users.noreply.github.com>
2022-01-05 08:39:31 -0500

    don't use uuid for Job primary key

--- mix_job/db/migrate/20171225054500_create_lib_jobs.rb
+++ mix_job/db/migrate/20171225054500_create_lib_jobs.rb
@@ -1,7 +1,6 @@
 class CreateLibJobs < ActiveRecord::Migration[6.0]
   def change
-    create_table :lib_jobs, id: false do |t|
-      t.primary_key :id, :uuid,    null: false, default: 'uuid_generate_v1mc()'
+    create_table :lib_jobs do |t|
       t.integer     :queue_name,   null: false, default: 0
       t.integer     :priority,     null: false, default: 0
       t.timestamp   :scheduled_at, null: false
