2f996b40e79c2a6de8f9f03acc630d58d1d2ee2b
Patrice Lebel <patleb@users.noreply.github.com>
2022-03-10 21:57:21 -0500

    vue removed from ext_webpacker

--- ext_webpacker/lib/javascript/webpack/environment.js
+++ ext_webpacker/lib/javascript/webpack/environment.js
@@ -2,10 +2,8 @@ const path = require('path')
 const require_module = (name) => require(path.resolve('node_modules', name))
 
 const { environment, config } = require_module('@rails/webpacker')
-const { VueLoaderPlugin } = require_module('vue-loader')
 const webpack = require_module('webpack')
 const erb = require('./loaders/erb')
-const vue = require('./loaders/vue')
 const coffee =  require('./loaders/coffee')
 
 const source_path = path.resolve(config.source_path)
@@ -20,16 +18,11 @@ case 'production':
   break
 }
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
   $:       'jquery',
   jQuery:  'jquery',
-  Vue:     ['vue/dist/vue.runtime.esm.js',   'default'],
-  Vuex:    ['vuex/dist/vuex.esm.js',         'default'],
-  VueI18n: ['vue-i18n/dist/vue-i18n.esm.js', 'default'],
 }))
 environment.loaders.prepend('erb', erb)
-environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('coffee', coffee)
 environment.loaders.get('sass').use.splice(-1, 0, { loader: 'resolve-url-loader' })
 environment.config.merge({ resolve: { alias: { '@': source_path, '@@': source_gems_path } } })
