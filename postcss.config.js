0c24b9477c2fe8c372af33aa78bb8c92e1837024
Patrice Lebel <patleb@users.noreply.github.com>
2022-03-22 08:12:41 -0400

    postcss-import and postcss-nested removed (already using sass)

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,6 @@ module.exports = {
     require('tailwindcss')('./tmp/tailwind.config.js'),
     require('tailwindcss/nesting'),
     require('autoprefixer'),
-    require('postcss-import'),
-    require('postcss-nested'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
