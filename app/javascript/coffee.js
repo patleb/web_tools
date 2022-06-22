dde44014496987c9ad4f85eeeb095cfc189e2453
Patrice Lebel <patleb@users.noreply.github.com>
2022-06-22 16:05:15 -0400

    coffee.js manifest updated

--- app/javascript/coffee.js
+++ app/javascript/coffee.js
@@ -1,2 +1,8 @@
 import '@@vendor/rails-ujs/all'
 import '@@vendor/turbolinks/all'
+import '@@lib/ext_coffee/core_ext/all'
+import '@@lib/ext_coffee/js/all'
+import '@@lib/ext_coffee/sm/all'
+import '@@lib/ext_coffee/concepts/js/all'
+
+Js.Concepts.initialize({ modules: ['Js'] })
