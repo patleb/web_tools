d116a8a71f52accd21f0e42eee62cf25eef8628a
Patrice Lebel <patleb@users.noreply.github.com>
2022-06-22 16:00:57 -0400

    jest_setup.js updated

--- ext_webpacker/lib/javascript/jest/jest_setup.js
+++ ext_webpacker/lib/javascript/jest/jest_setup.js
@@ -20,6 +20,6 @@ global.jest_console = global.console
 global.console = require('console')
 
 Object.assign(process.env, {
-  // LOGGER_DEBUG: true,
-  // LOGGER_TRACE: true,
+  LOGGER_DEBUG: false,
+  LOGGER_TRACE: false,
 })
