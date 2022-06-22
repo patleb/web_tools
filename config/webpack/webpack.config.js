87f3558a510a1049baaad3e049aab4d6fea349b2
Patrice Lebel <patleb@users.noreply.github.com>
2022-06-22 16:02:21 -0400

    application webpack.config.js updated

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,18 +1,18 @@
-const { config } = require('shakapacker')
+const { config, merge } = require('shakapacker')
 const { resolve } = require('path')
 const webpackConfigPath = resolve(config.source_path, 'lib/ext_webpacker/webpack/webpack.config')
 const webpackConfig = require(webpackConfigPath)
 
-// const webpack = require('webpack')
-// const { safeLoad } = require('js-yaml')
+const webpack = require('webpack')
+// const { load } = require('js-yaml')
 // const { readFileSync } = require('fs')
-// const settings = Object.keys(safeLoad(readFileSync(resolve('config/settings.yml')), 'utf8')[process.env.NODE_ENV])
+// const settings = Object.keys(load(readFileSync(resolve('config/settings.yml')), 'utf8')[process.env.NODE_ENV])
 
-// let environment = { plugins: [new webpack.EnvironmentPlugin(Object.assign(process.env, {
-//   YML_VALUE: settings.name,
-//   LOGGER_DEBUG: true
-//   LOGGER_TRACE: true
-// }))] }
-// module.exports = merge(webpackConfig, environment)
+let environment = { plugins: [new webpack.EnvironmentPlugin(Object.assign(process.env, {
+  // YML_VALUE: settings.name,
+  // LOGGER_DEBUG: true,
+  // LOGGER_TRACE: true,
+}))] }
+module.exports = merge(webpackConfig, environment)
 
 module.exports = webpackConfig
