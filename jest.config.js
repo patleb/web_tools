34f813c2f8321180f94dea7877ab8870897e441e
Patrice Lebel <patleb@users.noreply.github.com>
2022-06-21 21:09:02 -0400

    prevent bad breakpoints when debugging ext_coffee

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,13 @@ const jestConfigPath = resolve(config.source_path, 'lib/ext_webpacker/jest/jest.
 const jestConfig = require(jestConfigPath)
 
 jestConfig.roots.push('ext_coffee/test/javascript')
-jestConfig.moduleNameMapper['^@@test/ext_coffee/fixtures/(.+)'] = '<rootDir>/ext_coffee/test/fixtures/$1'
-jestConfig.moduleNameMapper['^@@test/ext_coffee/(.+)'] = '<rootDir>/ext_coffee/test/javascript/$1'
+jestConfig.moduleNameMapper = {
+  '^@@lib/ext_coffee/(.+)': '<rootDir>/ext_coffee/lib/javascript/$1',
+  '^@@lib/ext_webpacker/(.+)': '<rootDir>/ext_webpacker/lib/javascript/$1',
+  '^@@test/ext_coffee/fixtures/(.+)': '<rootDir>/ext_coffee/test/fixtures/$1',
+  '^@@test/ext_coffee/(.+)': '<rootDir>/ext_coffee/test/javascript/$1',
+  '^@@vendor/rails-ujs/(.+)': '<rootDir>/ext_coffee/vendor/javascript/rails-ujs/$1',
+  '^@@vendor/turbolinks/(.+)': '<rootDir>/ext_coffee/vendor/javascript/turbolinks/$1',
+}
 
 module.exports = jestConfig
