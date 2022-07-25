d0c04ff3913cabb3f71a9a00ae138cbc5d8dc0c4
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-25 19:22:30 -0400

    test correction

--- sunzistrano/test/libraries/sunzistrano/cli_test.rb
+++ sunzistrano/test/libraries/sunzistrano/cli_test.rb
@@ -3,6 +3,7 @@ require 'sunzistrano'
 
 class Sunzistrano::CliTest < Minitest::Spec
   it 'should build path relative to the owner provisioned dir' do
+    Setting.reload(env: 'test')
     path = Sunzistrano.owner_path :manifest_dir, 'version'
     assert_equal '/home/ubuntu/system-test-web_tools/sun_manifest/version', path
   end
