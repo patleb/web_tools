b3c87b07d6e46ce9597178d41ee099c82b295f83
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-25 10:30:25 -0400

    simple smoke test added

--- sunzistrano/test/libraries/sunzistrano/cli_test.rb
+++ sunzistrano/test/libraries/sunzistrano/cli_test.rb
@@ -1,13 +1,15 @@
 require './test/spec_helper'
 require 'sunzistrano'
 
-ROOT = Sunzistrano.root.join('test/fixtures/files')
-
 class Sunzistrano::CliTest < Minitest::Spec
-  let(:cli){ Sunzistrano::Cli.new }
-
   it 'should build path relative to the owner provisioned dir' do
     path = Sunzistrano.owner_path :manifest_dir, 'version'
     assert_equal '/home/ubuntu/system-test-web_tools/sun_manifest/version', path
+  end
+
+  it 'should compile' do
+    Sunzistrano::Cli.start(['compile', 'test:app', '--role=web'])
+    FileUtils.rm_rf "#{Sunzistrano::BASH_DIR}/web-test-app"
+    Setting.rollback!
   end
 end
