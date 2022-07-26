d716b0341048193a01f890b8b76c619662b5607c
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-26 05:22:01 -0400

    tests corrected

--- sunzistrano/test/libraries/sunzistrano/cli_test.rb
+++ sunzistrano/test/libraries/sunzistrano/cli_test.rb
@@ -5,12 +5,12 @@ class Sunzistrano::CliTest < Minitest::Spec
   it 'should build path relative to the owner provisioned dir' do
     Setting.reload(env: 'test')
     path = Sunzistrano.owner_path :manifest_dir, 'version'
-    assert_equal '/home/ubuntu/system-test-web_tools/sun_manifest/version', path
+    assert_equal '/home/ubuntu/test-web_tools/sun_manifest/version', path
   end
 
   it 'should compile' do
     Sunzistrano::Cli.start(['compile', 'test:app', '--role=web'])
-    FileUtils.rm_rf "#{Sunzistrano::BASH_DIR}/web-test-app"
+    FileUtils.rm_rf "#{Sunzistrano::BASH_DIR}/test-app"
     Setting.rollback!
   end
 end
