65b52749594b9aa0b303c196b496a60596f322be
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-26 19:26:16 -0400

    slightly better alignment

--- mix_server/lib/mix_server/sh/dns.rb
+++ mix_server/lib/mix_server/sh/dns.rb
@@ -14,12 +14,12 @@ module Sh::Dns
 
   def append_host(id, address, name, **options)
     <<~SH
-      #{"if [[ #{options[:if]} ]]; then" if options[:if] }
+   #{"if [[ #{options[:if]} ]]; then" if options[:if] }
         sudo sed -rzi -- "s/# #{id}-start.*# #{id}-end\\n//g" /etc/hosts
         echo '# #{id}-start' | sudo tee -a /etc/hosts
         echo "#{address}  #{name}" | sudo tee -a /etc/hosts
         echo '# #{id}-end' | sudo tee -a /etc/hosts
-      #{"fi" if options[:if] }
+   #{"fi" if options[:if] }
     SH
   end
 end
