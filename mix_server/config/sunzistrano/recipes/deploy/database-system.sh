f54248a63da703d002269e1895b538f1c727e09e
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-26 18:45:00 -0400

    - deploy/database-system recipe added; - git_config_url option added;

---
+++ mix_server/config/sunzistrano/recipes/deploy/database-system.sh
@@ -0,0 +1,28 @@
+if [[ "${db_host}" == '127.0.0.1' || "${db_host}" == 'localhost' ]]; then
+  if ! sun.psql "SELECT 1 FROM pg_roles WHERE rolname='${db_username}'" | grep -q 1; then
+    desc 'Create database user'
+    if sun.psql "CREATE USER ${db_username} WITH PASSWORD '${db_password}'"; then
+      echo "User ${db_username} created successfully"
+      desc 'Set as superuser'
+      if sun.psql "ALTER USER ${db_username} WITH SUPERUSER"; then
+        echo "User ${db_username} as superuser"
+      else
+        echo.red "Failed to set ${db_username} as superuser"
+        exit 1
+      fi
+    else
+      echo.red "Failed to create user ${db_username}"
+      exit 1
+    fi
+  fi
+
+  if ! sun.psql "SELECT 1 FROM pg_database WHERE datname='${db_database}'" | grep -q 1; then
+    desc 'Create database'
+    if sun.psql "CREATE DATABASE ${db_database} OWNER ${db_username}"; then
+      echo "Database ${db_database} created successfully"
+    else
+      echo.red "Failed to create database ${db_database}"
+      exit 1
+    fi
+  fi
+fi
--- sunzistrano/config/sunzistrano/recipes/deploy/start.sh
+++ sunzistrano/config/sunzistrano/recipes/deploy/start.sh
@@ -1,6 +1,10 @@
+git_config_url=${git_config_url:-false}
+
 desc 'Check that the repository is reachable'
-git config --global url."https://github.com/".insteadOf git@github.com:
-git config --global url."https://".insteadOf git://
+if [[ "${git_config_url}" == true ]]; then
+  git config --global url."https://github.com/".insteadOf git@github.com:
+  git config --global url."https://".insteadOf git://
+fi
 git ls-remote ${repo_url} HEAD
 
 desc 'Check shared and release directories exist'
