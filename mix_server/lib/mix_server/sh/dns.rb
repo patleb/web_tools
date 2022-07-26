374ed0266920d1a1041e251de373432ee196d2a3
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-26 19:12:21 -0400

    sudo added to Sh.append_host

--- mix_server/lib/mix_server/sh/dns.rb
+++ mix_server/lib/mix_server/sh/dns.rb
@@ -15,10 +15,10 @@ module Sh::Dns
   def append_host(id, address, name, **options)
     <<~SH
       #{"if [[ #{options[:if]} ]]; then" if options[:if] }
-        sed -rzi -- "s/# #{id}-start.*# #{id}-end\\n//g" /etc/hosts
-        echo '# #{id}-start' | tee -a /etc/hosts
-        echo "#{address}  #{name}" | tee -a /etc/hosts
-        echo '# #{id}-end' | tee -a /etc/hosts
+        sudo sed -rzi -- "s/# #{id}-start.*# #{id}-end\\n//g" /etc/hosts
+        echo '# #{id}-start' | sudo tee -a /etc/hosts
+        echo "#{address}  #{name}" | sudo tee -a /etc/hosts
+        echo '# #{id}-end' | sudo tee -a /etc/hosts
       #{"fi" if options[:if] }
     SH
   end
