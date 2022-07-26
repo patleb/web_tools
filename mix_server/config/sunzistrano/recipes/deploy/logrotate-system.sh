ac565bd196a3833f23fde279ab4b899512c08f0d
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-26 10:17:13 -0400

    use --system switch as a recipe filter (corrections)

---
+++ mix_server/config/sunzistrano/recipes/deploy/logrotate-system.sh
@@ -0,0 +1,2 @@
+desc "Add /etc/logrotate.d/${env}-${app}"
+sun.compile "/etc/logrotate.d/${env}-${app}" 0644 root:root
--- mix_server/config/sunzistrano/recipes/deploy/logrotate-{system}.sh
+++
@@ -1,4 +0,0 @@
-if [[ "${system}" == true ]]; then
-  desc "Add /etc/logrotate.d/${env}-${app}"
-  sun.compile "/etc/logrotate.d/${env}-${app}" 0644 root:root
-fi
