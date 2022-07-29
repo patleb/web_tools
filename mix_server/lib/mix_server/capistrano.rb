5775254408b7fdccb505987dca322383f2339595
Patrice Lebel <patleb@users.noreply.github.com>
2022-07-29 08:46:13 -0400

    removed old capistrano helpers

--- mix_server/lib/mix_server/capistrano.rb
+++ mix_server/lib/mix_server/capistrano.rb
@@ -3,7 +3,5 @@ require 'ext_rails/capistrano'
 require 'mix_geo/capistrano' if Gem.loaded_specs['mix_geo']
 require 'mix_file/capistrano' if Gem.loaded_specs['mix_file']
 require 'mix_server/sh'
-require 'mix_server/capistrano/helpers'
-include MixServer::Helpers
 
 load 'tasks/mix_server.cap'
