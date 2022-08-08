0bf2c57ccedcd8ba0aef59a79b0d5e7b8ed6a19b
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-08 09:27:05 -0400

    make sure release path is always the most recent

--- sunzistrano/config/sunzistrano/recipes/deploy/finish.sh
+++ sunzistrano/config/sunzistrano/recipes/deploy/finish.sh
@@ -1,4 +1,5 @@
 desc 'Clean up old releases'
+touch $release_path
 kept_releases=$(cat $revision_log | grep deployed | awk '{ print $2; }' | sed "s|^|${releases_path}/|")
 releases=$(ls -dt ${releases_path}/*)
 sun.remove_missing_files "$releases" "$kept_releases $release_path"
