f086448b03256bac2e23c4da8aa2fc31f0a2faaf
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-09 03:08:39 -0400

    --kill option added sun rake

--- mix_task/lib/mix_task/sunzistrano/cli/with_task.rb
+++ mix_task/lib/mix_task/sunzistrano/cli/with_task.rb
@@ -1,7 +1,7 @@
 module Sunzistrano
   Cli.class_eval do
-    desc 'rake [stage] [task] [--sudo] [--nohup] [--verbose]', 'Execute a rake task'
-    method_options sudo: false, nohup: false, verbose: false
+    desc 'rake [stage] [task] [--sudo] [--nohup] [--verbose] [--kill]', 'Execute a rake task'
+    method_options sudo: false, nohup: false, verbose: false, kill: false
     def rake(stage, task)
       do_rake(stage, task)
     end
@@ -16,8 +16,11 @@ module Sunzistrano
       def rake_remote_cmd
         environment = ["RAKE_OUTPUT=#{sun.verbose.to_b}", "RAILS_ENV=#{sun.env}", "RAILS_APP=#{sun.app}"]
         rbenv_ruby = "#{Sh.rbenv_export}; #{Sh.rbenv_init};"
-        rbenv_sudo = "rbenv sudo #{environment.join(' ')}" if sun.sudo
-        context = environment.map{ |value| "export #{value};" }.join(' ') unless sun.sudo
+        if sun.sudo
+          rbenv_sudo = "rbenv sudo #{environment.join(' ')}"
+        else
+          context = environment.map{ |value| "export #{value};" }.join(' ')
+        end
         path = "cd #{sun.deploy_path :current};"
         command = %{bin/rake '#{sun.task.escape_single_quotes.escape_spaces}'}
         if sun.nohup
@@ -25,6 +28,12 @@ module Sunzistrano
           command = "#{command} >> log/#{filename}.log 2>&1 & sleep 1 && echo $! > tmp/pids/#{filename}.pid"
           <<-SH.squish
             #{rbenv_ruby} #{context} #{path} nohup #{rbenv_sudo} #{command}
+          SH
+        elsif sun.kill
+          filename = nohup_basename(command)
+          pid = "#{sun.deploy_path :current}/tmp/pids/#{filename}.pid"
+          <<-SH.squish
+            sudo pkill '-P' "$(cat #{pid})" && rm -f #{pid} || rm -f #{pid}
           SH
         else
           <<-SH.squish
