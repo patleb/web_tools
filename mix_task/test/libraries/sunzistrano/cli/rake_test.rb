09264ede1aca2ce102b5d9565e2ffe0a4861d9a8
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-12 04:55:53 -0400

    tests corrected

--- mix_task/test/libraries/sunzistrano/cli/rake_test.rb
+++ mix_task/test/libraries/sunzistrano/cli/rake_test.rb
@@ -2,7 +2,7 @@ require './sunzistrano/test/spec_helper'
 
 class Sunzistrano::Cli::RakeTest < Minitest::Spec
   it 'should insert task string with escaped single quotes' do
-    assert_output(%r{bin/rake first_task second_task\[1, "ab c"\] ENV_VAR=\\'hello world\\'}) do
+    assert_output(%r{bin/rake first_task second_task\[1, "ab c"\] ENV_VAR='\\''hello world'\\''}) do
       Sunzistrano::Cli.start(['rake', 'test', %{first_task second_task[1, "ab c"] ENV_VAR='hello world'}])
     end
     Setting.rollback!
