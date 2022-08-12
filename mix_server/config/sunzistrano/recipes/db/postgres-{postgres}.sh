451c84073a69a80ceef1233e6773e3927e63d2f4
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-12 04:30:13 -0400

    rollback to uppercase recipe variables

--- mix_server/config/sunzistrano/recipes/db/postgres-{postgres}.sh
+++ mix_server/config/sunzistrano/recipes/db/postgres-{postgres}.sh
@@ -5,8 +5,8 @@
 # https://www.modio.se/scaling-past-the-single-machine.html
 # https://askubuntu.com/questions/732431/how-to-uninstall-specific-versions-of-postgres
 locale=${locale:-en_US}
-config=$(pg.default_config_file)
-pg_hba=$(pg.default_hba_file)
+PG_CONFIG_FILE=$(pg.default_config_file)
+PG_HBA_FILE=$(pg.default_hba_file)
 
 sh -c "echo 'deb [arch=$ARCH] http://apt.postgresql.org/pub/repos/apt/ $UBUNTU_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
@@ -14,21 +14,21 @@ echo "deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $UBUNTU_CODENAME
 wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | apt-key add -
 sun.update
 
-postgresql_version="$(sun.installed_version postgresql-${postgres})"
-packages="postgresql-${postgres} postgresql-server-dev-${postgres} postgresql-common libpq-dev"
-sun.install "$packages"
-sun.lock "$packages"
+PG_VERSION="$(sun.installed_version postgresql-${postgres})"
+PG_PACKAGES="postgresql-${postgres} postgresql-server-dev-${postgres} postgresql-common libpq-dev"
+sun.install "$PG_PACKAGES"
+sun.lock "$PG_PACKAGES"
 sun.install "timescaledb-tools"
 
 sudo su - postgres << EOF
   pg_dropcluster --stop "${postgres}" main
   pg_createcluster --port="543$((postgres % 10))" --locale "${locale}.UTF-8" --start "${postgres}" main <%= '-- --data-checksums' unless sun.pg_checksums == false %>
 EOF
-sun.backup_compare "$config"
-sun.backup_compare "$pg_hba"
+sun.backup_compare "$PG_CONFIG_FILE"
+sun.backup_compare "$PG_HBA_FILE"
 sun.backup_copy "/etc/logrotate.d/postgresql-common" 0440 root:root
 
-echo "$postgresql_version" >> $(sun.manifest_path 'postgresql')
+echo "$PG_VERSION" >> $(sun.manifest_path 'postgresql')
 echo "$(pg.default_data_dir)" > $(sun.metadata_path 'pg_data_dir')
 
 systemctl restart postgresql
