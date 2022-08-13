983e6a6680438dcfa0adca8808983d5b0e8a0167
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-13 16:20:04 -0400

    use webpacker rake tasks directly for deploy/assets recipe

--- mix_server/config/sunzistrano/recipes/deploy/assets.sh
+++ mix_server/config/sunzistrano/recipes/deploy/assets.sh
@@ -2,10 +2,13 @@ keep_assets=${keep_assets:-10}
 
 cd ${release_path}
 
-desc 'Compile assets'
-bin/rake assets:precompile
+desc 'Install all JavaScript dependencies as specified via Yarn'
+bin/rake webpacker:yarn_install
 
-desc 'Cleanup expired assets'
-bin/rake assets:clean[${keep_assets}]
+desc "Compile JavaScript packs using webpack for ${stage} with digests"
+bin/rake webpacker:compile
+
+desc 'Remove old compiled webpacks'
+bin/rake webpacker:clean[${keep_assets}]
 
 cd.back
