03d380bd14d78856d84a0eebaf4e28d548af0a79
Patrice Lebel <patleb@users.noreply.github.com>
2022-08-30 00:07:44 -0400

    added whenever actions to nginx maintenance lifecycle

--- ext_whenever/config/sunzistrano/helpers/whenever_helper.sh
+++ ext_whenever/config/sunzistrano/helpers/whenever_helper.sh
@@ -3,9 +3,11 @@ whenever.update() { # PUBLIC
   bin/whenever --update-crontab ${stage} --set "environment=${env}&application=${app}"
   cd.back
 }
+nginx_maintenance_disable_after+=('whenever.update')
 
 whenever.clear() { # PUBLIC
   cd ${release_path}
   bin/whenever --clear-crontab ${stage}
   cd.back
 }
+nginx_maintenance_enable_before+=('whenever.clear')
