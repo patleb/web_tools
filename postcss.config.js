f58b2b293fb6f0619d6f21414b9952a8b41d2590
Patrice Lebel <patleb@users.noreply.github.com>
2023-03-10 02:02:00 -0500

    tailwindcss/nesting correction

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./tmp/tailwind.config.js'),
     require('tailwindcss/nesting'),
+    require('tailwindcss')('./tmp/tailwind.config.js'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
