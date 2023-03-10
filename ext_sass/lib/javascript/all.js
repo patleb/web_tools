c4e2b22b5643ddce7e38bc9b4d76f854d399bb7e
Patrice Lebel <patleb@users.noreply.github.com>
2023-03-10 05:16:23 -0500

    generalize client js usage introspection

--- ext_sass/lib/javascript/all.js
+++ ext_sass/lib/javascript/all.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.documentElement.classList.remove('no-js')
+  document.cookie = '_js=yes; path=/' + ((window.location.protocol === 'https:') ? '; secure' : '')
 })
