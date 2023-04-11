b8736a4235c06f7bdf8749ee7f52426a039d0bd9
Patrice Lebel <patleb@users.noreply.github.com>
2023-04-11 15:15:51 -0400

    no-transition class added

--- ext_sass/lib/javascript/all.js
+++ ext_sass/lib/javascript/all.js
@@ -2,3 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   document.documentElement.classList.remove('no-js')
   document.cookie = '_js=yes; path=/' + ((window.location.protocol === 'https:') ? '; secure' : '')
 })
+
+if (window.Turbolinks) {
+  Rails.document_on('turbolinks:before-render', function () {
+    clearTimeout(window.no_transition_timeout)
+  })
+
+  Rails.document_on('turbolinks:load', function () {
+    window.no_transition_timeout = setTimeout(function () {
+      document.body.classList.remove('no-transition')
+    }, 250)
+  })
+}
