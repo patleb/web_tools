45b82c24402579614ded94681acff74b060abf0b
Patrice Lebel <patleb@users.noreply.github.com>
2024-06-10 08:42:27 -0400

    ActionController::Delegator and ActionView::Delegator added

---
+++ ext_rails/lib/ext_rails/action_controller/delegator.rb
@@ -0,0 +1,69 @@
+class ActionController::Delegator
+  extend ActiveSupport::DescendantsTracker
+
+  class << self
+    delegate :memoize, to: 'Current.controller'
+    delegate :t, to: I18n
+  end
+  delegate :memoize, to: 'Current.controller'
+  delegate :t, to: I18n
+
+  def self.method_missing(name, *args, **options, &block)
+    if Current.controller.respond_to? name, true
+      Current.controller.__send__(name, *args, **options, &block)
+    else
+      raise NoMethodError.new("No method '#{name}' for class #{self} or Current.controller", name)
+    end
+  end
+
+  def self.respond_to_missing?(name, _include_private = false)
+    Current.controller.respond_to?(name, true)
+  end
+
+  def method_missing(name, *args, **options, &block)
+    if @_locals.has_key? name
+      self.class.define_method(name) do
+        @_locals[name]
+      end
+      send(name)
+    elsif Current.controller.respond_to? name, true
+      Current.controller.__send__(name, *args, **options, &block)
+    else
+      raise NoMethodError.new("No method '#{name}' for #{self.class} or :locals or Current.controller", name)
+    end
+  end
+
+  def respond_to_missing?(name, _include_private = false)
+    @_locals.has_key?(name) || Current.controller.respond_to?(name, true)
+  end
+
+  def self.[](*ivars)
+    base_class =
+      case ivars.first
+      when Symbol, String, nil
+        self
+      else
+        ivars.shift
+      end
+
+    Class.new(base_class) do
+      define_singleton_method :default_ivars do
+        super().merge(ivars)
+      end
+    end
+  end
+
+  def self.default_ivars
+    Set.new
+  end
+
+  def initialize(**locals)
+    self.class.default_ivars.each do |name|
+      ivar(name, Current.controller.ivar(name))
+    end
+    @_locals = locals
+    after_initialize
+  end
+
+  def after_initialize; end
+end
---
+++ ext_rails/lib/ext_rails/action_view/delegator.rb
@@ -0,0 +1,42 @@
+class ActionView::Delegator < ActionController::Delegator
+  def self.method_missing(name, *args, **options, &block)
+    if Current.view.respond_to? name
+      Current.view.public_send(name, *args, **options, &block)
+    elsif Current.controller.respond_to? name, true
+      Current.controller.__send__(name, *args, **options, &block)
+    else
+      raise NoMethodError.new("No method '#{name}' for class #{self} or Current.view or Current.controller", name)
+    end
+  end
+
+  def self.respond_to_missing?(name, include_private = false)
+    Current.view.respond_to?(name, include_private) || super
+  end
+
+  def method_missing(name, *args, **options, &block)
+    if @_locals.has_key? name
+      self.class.define_method(name) do
+        @_locals[name]
+      end
+      send(name)
+    elsif Current.view.respond_to? name
+      Current.view.public_send(name, *args, **options, &block)
+    elsif Current.controller.respond_to? name, true
+      Current.controller.__send__(name, *args, **options, &block)
+    else
+      raise NoMethodError.new("No method '#{name}' for #{self.class} or :locals or Current.view or Current.controller", name)
+    end
+  end
+
+  def respond_to_missing?(name, include_private = false)
+    @_locals.has_key?(name) || Current.view.respond_to?(name, include_private) || Current.controller.respond_to?(name, true)
+  end
+
+  def initialize(**locals)
+    self.class.default_ivars.each do |name|
+      ivar(name, Current.view.ivar(name))
+    end
+    @_locals = locals
+    after_initialize
+  end
+end
