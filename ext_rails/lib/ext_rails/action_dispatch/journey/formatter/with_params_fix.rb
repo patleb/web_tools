c1f60bc65af4ff286355f8149fe9e4ec0469f18f
Patrice Lebel <patleb@users.noreply.github.com>
2024-06-10 08:41:34 -0400

    removed deprecated fixes

--- ext_rails/lib/ext_rails/action_dispatch/journey/formatter/with_params_fix.rb
+++
@@ -1,7 +0,0 @@
-module ActionDispatch::Journey::Formatter::WithParamsFix
-  def generate(name, options, path_parameters, *rest)
-    super(name, options.symbolize_keys.with_keyword_access, path_parameters.symbolize_keys.with_keyword_access, *rest)
-  end
-end
-
-ActionDispatch::Journey::Formatter.prepend ActionDispatch::Journey::Formatter::WithParamsFix
