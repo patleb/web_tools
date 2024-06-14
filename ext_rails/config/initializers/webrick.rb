1b0cc22c223deea0a96af3eb42af2602107f756b
Patrice Lebel <patleb@users.noreply.github.com>
2024-06-14 04:41:27 -0400

    Rackup::Handler::WEBrick.shutdown update has the correction

--- ext_rails/config/initializers/webrick.rb
+++ ext_rails/config/initializers/webrick.rb
@@ -1,17 +1,9 @@
 if defined?(WEBrick) && Rails.env.local?
   require 'webrick/httpserver'
-  require 'rack/handler/webrick'
 
   WEBrick::HTTPServer.class_eval do
     def access_log(config, req, res)
       # so assets don't log
-    end
-  end
-
-  Rack::Handler::WEBrick.class_eval do
-    def self.shutdown
-      @server&.shutdown
-      @server = nil
     end
   end
 end
