66f56c64c445d250df366edcbf2620f8c32590a9
Patrice Lebel <patleb@users.noreply.github.com>
2024-06-28 05:16:49 -0400

    correction

--- ext_rails/lib/ext_rails/action_controller/base/with_memoization.rb
+++ ext_rails/lib/ext_rails/action_controller/base/with_memoization.rb
@@ -1,7 +1,7 @@
 module ActionController::Base::WithMemoization
   # NOTE referential equality (#equal?) is used instead of value equality (#eql? or #==)
   def memoize(context, method_name, *args)
-    values = ((@_current ||= {})[context.object_id] ||= {})[method_name] ||= {}
+    values = ((@_memoize ||= {})[context.object_id] ||= {})[method_name] ||= {}
     args.map!(&:object_id)
     if values.has_key? args
       values[args]
