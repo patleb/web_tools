e71973fafbeaaf7f67e80c4477f8938802a247cb
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-01 15:46:47 -0400

    update

--- mix_job/app/models/job.rb
+++ mix_job/app/models/job.rb
@@ -3,7 +3,6 @@ class Job < LibRecord
 
   alias_attribute :provider_job_id, :id
   alias_attribute :enqueued_at, :created_at
-  alias_attribute :time_zone, :timezone
 
   json_attribute MixJob.config.json_attributes
 
