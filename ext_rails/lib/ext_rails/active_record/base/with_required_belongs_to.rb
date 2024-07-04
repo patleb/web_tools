37d5ec6fdcb70da8a304f017f9e97393a06e3037
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-03 21:44:01 -0400

    MonkeyPatch check added

--- ext_rails/lib/ext_rails/active_record/base/with_required_belongs_to.rb
+++ ext_rails/lib/ext_rails/active_record/base/with_required_belongs_to.rb
@@ -1,3 +1,5 @@
+MonkeyPatch.add{['activerecord', 'lib/active_record/associations/builder/belongs_to.rb', '7bd76bcf3d7a4927db3fd82e20eacd4a2f60c122a41683a717ac9ff25b0b3924']}
+
 module ActiveRecord::Base::WithRequiredBelongsTo
   extend ActiveSupport::Concern
 
