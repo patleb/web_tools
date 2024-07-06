6e79ec1351c450d9edfa7b2ff2591fea8886b828
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-05 21:22:29 -0400

    monkey patch check added

--- ext_rails/lib/ext_rails/active_record/connection_adapters/postgresql_adapter/with_type_map.rb
+++ ext_rails/lib/ext_rails/active_record/connection_adapters/postgresql_adapter/with_type_map.rb
@@ -1,3 +1,5 @@
+MonkeyPatch.add{['activerecord', 'lib/active_record/connection_adapters/postgresql_adapter.rb', 'd8c851a3c08445e649e35752a08eb0e86549deaaabdf8214a7d50c64a18fabf5']}
+
 module ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::WithTypeMap
   extend ActiveSupport::Concern
 
