456862f15f2e3e14afea338a1ee515d268a9a766
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-08 17:20:13 -0400

    String::Reference.clear! monkey patch check added

--- ext_rails/lib/ext_rails/active_support/dependencies/with_cache.rb
+++ ext_rails/lib/ext_rails/active_support/dependencies/with_cache.rb
@@ -1,10 +1,12 @@
+MonkeyPatch.add{['activesupport', 'lib/active_support/dependencies.rb', '2b11940364ba3f0b10c9b4338a1fac14e2dcbb29c5b1b13c92e29f2ec460f67d']}
+
 # NOTE https://github.com/rails/rails/commit/3c90308b175de54cd120f65b113ee774aec203b2
 module ActiveSupport::Dependencies::WithCache
   extend ActiveSupport::Concern
 
   class_methods do
     def clear
-      ActiveSupport::Dependencies.unload_interlock do
+      unload_interlock do
         String::Reference.clear!
       end
       super
