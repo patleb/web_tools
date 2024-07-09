775a2f683e6a23b4e6ec18f8b8f51053ee48f09a
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-08 20:04:30 -0400

    require_or_load has been removed in Rails

--- ext_rails/lib/ext_rails/setup.rb
+++ ext_rails/lib/ext_rails/setup.rb
@@ -3,6 +3,5 @@
 # i18n is slow to load --> would be the next best thing to replace (routes aren't that bad ~100ms)
 # bin/rake is significantly faster
 
-require 'ext_rails/active_support/dependencies/with_profile'
 require 'ext_rails/monkey_patch'
 require 'ext_rails/rake/backtrace'
