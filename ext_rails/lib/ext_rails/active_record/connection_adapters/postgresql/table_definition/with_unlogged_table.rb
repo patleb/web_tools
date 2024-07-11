52f72b5f2d11d99b970e2290c241d41ec90b6dbb
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-11 18:21:21 -0400

    monkey patch check added

--- ext_rails/lib/ext_rails/active_record/connection_adapters/postgresql/table_definition/with_unlogged_table.rb
+++ ext_rails/lib/ext_rails/active_record/connection_adapters/postgresql/table_definition/with_unlogged_table.rb
@@ -1,3 +1,5 @@
+MonkeyPatch.add{['activerecord', 'lib/active_record/connection_adapters/postgresql/schema_definitions.rb', '67345bc2286f297de37cf20a5164ce5bda8295f74fb2a12dbdc59e0b229e62bf']}
+
 module ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition::WithUnloggedTable
   def initialize(*, **options)
     super
