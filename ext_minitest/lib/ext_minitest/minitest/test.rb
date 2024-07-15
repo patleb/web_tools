8a5b3bb9cfdc0eab3ab77dc9a0efa5f62343b9f6
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-15 02:42:38 -0400

    clearer

--- ext_minitest/lib/ext_minitest/minitest/test.rb
+++ ext_minitest/lib/ext_minitest/minitest/test.rb
@@ -1,11 +1,14 @@
 Minitest::Test.class_eval do
+  DEFAULT_TIMEOUT = 1
+
   class << self
     alias_method :order_dependent!, :i_suck_and_my_tests_are_order_dependent!
   end
 
   alias_method :run_without_timeout, :run
   def run(&block)
-    if (seconds = defined?(run_timeout) ? run_timeout : 1) == false
+    seconds = defined?(run_timeout) ? run_timeout : DEFAULT_TIMEOUT
+    if seconds == false
       run_without_timeout(&block)
     else
       Timeout.timeout(seconds){ run_without_timeout(&block) }
