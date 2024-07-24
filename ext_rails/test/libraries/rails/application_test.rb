ce7eb7002d8f83b01c5d5d74787b3351c9b529f5
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-24 18:11:39 -0400

    tests added for Rails.(app|stage|viable_names)

---
+++ ext_rails/test/libraries/rails/application_test.rb
@@ -0,0 +1,20 @@
+require './test/test_helper'
+
+class Rails::ApplicationTest < ActiveSupport::TestCase
+  test '.app, .stage' do
+    assert_equal 'web_tools', Rails.app
+    assert Rails.app.is_a?(ActiveSupport::StringInquirer)
+    assert Rails.app.base?
+    assert_equal 'test-web_tools', Rails.stage
+  end
+
+  test '.viable_names' do
+    models = Rails.viable_names('models')
+    full_size = models.size
+    assert 10 < full_size
+    models = Rails.viable_names('models', ['Test::RelatedRecord'], ['/test/record.rb'])
+    assert models.exclude? 'Test::RelatedRecord'
+    assert models.exclude? 'Test::Record'
+    assert_equal full_size - 2, models.size
+  end
+end
