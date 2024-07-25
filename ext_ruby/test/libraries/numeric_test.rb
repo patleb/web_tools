4366cbdc4be6b5c22447faf11cb0ae813c68579f
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-25 15:39:26 -0400

    Numeric simplified/improved with tests

--- ext_ruby/test/libraries/numeric_test.rb
+++ ext_ruby/test/libraries/numeric_test.rb
@@ -1,7 +1,7 @@
 require './test/spec_helper'
 
 class NumericTest < Minitest::TestCase
-  it 'should simplify numbers with at least 5x 9s or 0s after the leading numbers' do
+  test '#simplify numbers with at least 5x 9s or 0s after the leading numbers' do
     assert_equal 0.19999, 0.19999.simplify
     assert_equal 0.2, 0.199999.simplify
     assert_equal 0.100009, 0.100009.simplify
@@ -13,5 +13,14 @@ class NumericTest < Minitest::TestCase
     assert_equal 0.0, 0.0.simplify
     assert_equal 9_999, 9_999.simplify
     assert_equal 100_000, 99_999.simplify
+  end
+
+  test '#to_hours, #to_days' do
+    assert_equal [0, 1, 0], 1.minute.to_hours
+    assert_equal [0, 1, 5], (1.minute + 5.seconds).to_hours
+    assert_equal [1, 1, 5], (1.hour + 1.minute + 5.seconds).to_hours
+    assert_equal [0, 1, 1, 5], (1.hour + 1.minute + 5.seconds).to_days
+    assert_equal [1, 0, 0, 0], 1.day.to_days
+    assert_equal [1, 1, 1, 5], (1.day + 1.hour + 1.minute + 5.seconds).to_days
   end
 end
