64ff139edbdcbed437b0fc521f73d827173e00da
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-25 16:43:34 -0400

    rollback memoized_at constants

--- ext_ruby/lib/ext_ruby/memoized_at.rb
+++ ext_ruby/lib/ext_ruby/memoized_at.rb
@@ -1,25 +1,34 @@
 module MemoizedAt
-  def m_access(name, timeout: ExtRuby.config.memoized_at_timeout, force: false)
+  def m_access(name, *constants, timeout: ExtRuby.config.memoized_at_timeout, force: false)
     @m_access_at ||= Concurrent::Hash.new
     @m_access_cache ||= Concurrent::Hash.new
-    if force || !@m_access_cache.has_key?(name) || (Time.current - @m_access_at[name]) > timeout
-      value = @m_access_cache[name] = block_given? ? yield : send(name)
-      @m_access_at[name] = Time.current
+    access_key = m_access_key(name, constants)
+    if force || !@m_access_cache.has_key?(access_key) || (Time.now - @m_access_at[access_key]) > timeout
+      value = @m_access_cache[access_key] = block_given? ? yield : send(name, *constants)
+      @m_access_at[access_key] = Time.now
     else
-      value = @m_access_cache[name]
+      value = @m_access_cache[access_key]
     end
     value
   end
 
-  def m_clear(name = nil)
+  def m_clear(name = nil, *constants)
     if name.nil?
       @m_access_at = Concurrent::Hash.new
       @m_access_cache = Concurrent::Hash.new
     else
       @m_access_at ||= Concurrent::Hash.new
       @m_access_cache ||= Concurrent::Hash.new
-      @m_access_at.delete(name)
-      @m_access_cache.delete(name)
+      access_key = m_access_key(name, constants)
+      @m_access_at.delete(access_key)
+      @m_access_cache.delete(access_key)
     end
+  end
+
+  private
+
+  def m_access_key(name, constants)
+    constants = constants.compact
+    constants.any? ? "#{name}:#{constants.map(&:to_s).join(':')}" : name.to_s
   end
 end
