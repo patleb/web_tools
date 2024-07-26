1879376e97cfbc93159ff19325c853c7137bfefa
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-25 20:46:36 -0400

    clean-up

--- ext_ruby/lib/ext_ruby/timeout.rb
+++ ext_ruby/lib/ext_ruby/timeout.rb
@@ -1,8 +1,9 @@
 # https://github.com/ruby-debug/ruby-debug-ide/issues/80
 if ENV['DEBUGGER_HOST']
   require 'timeout'
+
   module Timeout
-    def timeout(sec, klass = nil, message = nil)
+    def timeout(sec, *)
       yield(sec)
     end
     module_function :timeout
