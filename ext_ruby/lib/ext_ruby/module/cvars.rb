4b749482278ffaca2b709ff182f71cbe96bf3613
Patrice Lebel <patleb@users.noreply.github.com>
2024-07-31 23:24:34 -0400

    added missing cvar methods

--- ext_ruby/lib/ext_ruby/module/cvars.rb
+++ ext_ruby/lib/ext_ruby/module/cvars.rb
@@ -1,9 +1,14 @@
 class Module
   alias_method :cvars, :class_variables
+  alias_method :cvar_defined?, :class_variable_defined?
+
+  def remove_cvar(name)
+    remove_class_variable(name) if cvar_defined? name
+  end
 
   def cvar(name, *value)
     if block_given?
-      class_variable_defined?(name) ? class_variable_get(name) : class_variable_set(name, yield)
+      cvar_defined?(name) ? class_variable_get(name) : class_variable_set(name, yield)
     elsif value.empty?
       class_variable_get(name)
     else
