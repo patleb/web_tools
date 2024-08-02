6bc919ebb6004bfb8b9d9091363aaec3d8b5cff2
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-02 10:36:55 -0400

    added javascript_pack_tag! for test purposes

--- ext_webpacker/lib/ext_webpacker/railtie.rb
+++ ext_webpacker/lib/ext_webpacker/railtie.rb
@@ -9,5 +9,9 @@ module ExtWebpacker
     initializer 'ext_webpacker.bootstrap', before: 'webpacker.proxy' do
       require 'ext_webpacker/webpacker'
     end
+
+    ActiveSupport.on_load(:action_view) do
+      require 'ext_webpacker/webpacker/helper'
+    end
   end
 end
---
+++ ext_webpacker/lib/ext_webpacker/webpacker/helper.rb
@@ -0,0 +1,11 @@
+module Webpacker::Helper
+  def javascript_pack_tag!(...)
+    return if Rails.env.test?
+    javascript_pack_tag(...)
+  end
+
+  def stylesheet_pack_tag!(...)
+    return if Rails.env.test?
+    stylesheet_pack_tag(...)
+  end
+end
