c532717c2ddefd13e7ccfaebc58bf60df9e149f8
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-14 15:25:27 -0400

    correctly ignore autoload for rice and javascript dirs

--- ext_rice/lib/ext_rice/railtie.rb
+++ ext_rice/lib/ext_rice/railtie.rb
@@ -4,6 +4,10 @@ module ExtRice
       load 'tasks/ext_rice.rake'
     end
 
+    config.before_initialize do |app|
+      ActiveSupport::Dependencies.autoload_paths.delete("#{app.root}/app/rice")
+    end
+
     initializer 'ext_rice.require_ext' do
       Rice.require_ext
     end
--- ext_shakapacker/lib/ext_shakapacker/railtie.rb
+++ ext_shakapacker/lib/ext_shakapacker/railtie.rb
@@ -6,6 +6,10 @@ module ExtShakapacker
       load 'tasks/ext_shakapacker.rake'
     end
 
+    config.before_initialize do |app|
+      ActiveSupport::Dependencies.autoload_paths.delete("#{app.root}/app/javascript")
+    end
+
     initializer 'ext_shakapacker.bootstrap', before: 'shakapacker.proxy' do
       require 'ext_shakapacker/shakapacker'
     end
