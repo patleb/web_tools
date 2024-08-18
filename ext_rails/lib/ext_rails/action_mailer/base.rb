648519d5b4cbae12af323d683932c2c8048f9855
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-18 14:45:24 -0400

    added i18n_subject helper to mailer

--- ext_rails/lib/ext_rails/action_mailer/base.rb
+++ ext_rails/lib/ext_rails/action_mailer/base.rb
@@ -1,1 +1,10 @@
-ActionMailer::Base.include ActiveSupport::LazyLoadHooks::Autorun
+ActionMailer::Base.class_eval do
+  include ActiveSupport::LazyLoadHooks::Autorun
+
+  private
+
+  def i18n_subject(interpolations = {})
+    mailer_scope = self.class.mailer_name.tr("/", ".")
+    I18n.t(:subject, **interpolations.merge(scope: [mailer_scope, action_name]))
+  end
+end
