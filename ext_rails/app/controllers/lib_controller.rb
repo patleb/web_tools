50a432af9d9361c13623fce16f1efda2c23b1973
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-18 06:09:49 -0400

    #root_path, #back_path and #redirect_back helpers added to LibController

--- ext_rails/app/controllers/lib_controller.rb
+++ ext_rails/app/controllers/lib_controller.rb
@@ -11,14 +11,38 @@ class LibController < ActionController::Base
   end
   helper_method :application_path
 
+  def root_path
+    application_path
+  end
+  helper_method :root_path
+
+  def back_path
+    back = _back
+    return root_path unless back && _url_host_allowed?(back)
+    back
+  end
+  helper_method :back_path
+
+  def redirect_back(fallback_location: root_path, **)
+    super(fallback_location: fallback_location, **)
+  end
+
+  def redirect_back_or_to(fallback_location = root_path, allow_other_host: _allow_other_host, **)
+    back = _back
+    if back && (allow_other_host || _url_host_allowed?(back))
+      redirect_to(back, allow_other_host: allow_other_host, **)
+    else
+      redirect_to(fallback_location, **)
+    end
+  end
+
   private
 
   def set_meta_values
-    (@meta ||= {}).merge!(
-      root: respond_to?(:root_path) ? root_path : application_path,
-      app: Rails.application.title,
-      title: Rails.application.title,
-      description: Rails.application.title,
-    )
+    (@meta ||= {}).merge!(root: root_path, app: (title = Rails.application.title), title: title, description: title)
+  end
+
+  def _back
+    params[:_back].presence || request.headers['X-Back'].presence || request.referer.presence
   end
 end
