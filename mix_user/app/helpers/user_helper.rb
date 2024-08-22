815d8814ac25b0a2a4d8cdf5032236570988668a
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-22 12:20:30 -0400

    clearer

--- mix_user/app/helpers/user_helper.rb
+++ mix_user/app/helpers/user_helper.rb
@@ -14,12 +14,13 @@ module UserHelper
   end
 
   def user_links
+    return unless controller.is_a? Users::BaseController
     i18n = i18n_for('link' => [:back, :sign_up, :log_in, :forgot_password, :missing_confirm])
     links = []
-    case controller_name
-    when 'users'
+    case controller
+    when UsersController
       links << a_(i18n[:log_in], href: MixUser::Routes.new_session_path)
-    when 'user_sessions'
+    when UserSessionsController
       if params[:edit].present?
         links << a_(i18n[:back], href: MixUser::Routes.new_session_path)
       elsif MixUser.config.registerable?
