a833a7a43e78dd563ceb90c0a424e672582e44cb
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-26 07:27:16 -0400

    update correction

--- mix_file/app/jobs/active_storage/optimize_job.rb
+++ mix_file/app/jobs/active_storage/optimize_job.rb
@@ -1,6 +1,6 @@
 class ActiveStorage::OptimizeJob < ActiveStorage::BaseJob
   discard_on ActiveRecord::RecordNotFound
-  retry_on ActiveStorage::IntegrityError, attempts: 10, wait: :exponentially_longer
+  retry_on ActiveStorage::IntegrityError, attempts: 10, wait: :polynomially_longer
 
   def perform(blob)
     blob.optimize
