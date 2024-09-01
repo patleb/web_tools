496cb4a422d922e18be6beccba5f526800d39e4f
Patrice Lebel <patleb@users.noreply.github.com>
2024-08-31 22:49:22 -0400

    LogLines::NginxError.parse test added

--- mix_server/test/models/log_lines/nginx_error_test.rb
+++ mix_server/test/models/log_lines/nginx_error_test.rb
@@ -1,19 +1,19 @@
-require './test/rails_helper'
-require Gem.root('mix_server').join('test/fixtures/files/log/nginx/web_tools.error.rb').to_s
+require './test/test_helper'
+require Gem.root('mix_server').join('test/fixtures/files/log/nginx/web_tools_test.error.rb').to_s
 
 module LogLines
   class NginxErrorTest < ActiveSupport::TestCase
     self.file_fixture_path = Gem.root('mix_server').join('test/fixtures/files').to_s
 
-    it 'should parse correctly each line' do
-      file = file_fixture('log/nginx/web_tools.error.log.0')
+    test '.parse' do
+      file = file_fixture('log/nginx/web_tools_test.error.log.0')
+      mtime = Time.utc(2022,7,27,10,9,54)
 
       log = Log.create! server: Server.current, path: file.to_s.delete_suffix('.0')
       previous = nil
       file.each_line.with_index do |line, i|
-        line = LogLines::NginxError.parse(log, line, mtime: file.mtime, previous: previous)
-        # puts line
-        # assert_equal ERROR_EXPECTATIONS[i], LogLines::NginxError.parse(log, line, mtime: file.mtime)
+        line = LogLines::NginxError.parse(log, line, mtime: mtime, previous: previous)
+        assert_equal ERROR_EXPECTATIONS[i], line
         previous = line
       end
     end
