5602932bf8e7e83149f0b658618af9211b614632
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-02 13:20:56 -0400

    Log.remove_ivar(:@db_log) before test

--- mix_server/lib/minitest/mix_server/active_support/test_case.rb
+++ mix_server/lib/minitest/mix_server/active_support/test_case.rb
@@ -2,6 +2,7 @@ ActiveSupport::TestCase.class_eval do
   alias_method :run_without_server_and_logs, :run
   def run(...)
     Server.current
+    Log.remove_ivar(:@db_log)
     result = run_without_server_and_logs(...)
     clear_logs unless use_transactional_tests
     result
