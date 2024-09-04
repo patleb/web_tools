a274ab602c9f4269ecdc04faafa23feb08e04360
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-04 11:59:49 -0400

    added value check support to MonkeyPatch

--- ext_rails/lib/ext_rails/monkey_patch.rb
+++ ext_rails/lib/ext_rails/monkey_patch.rb
@@ -1,21 +1,31 @@
 module MonkeyPatch
   def self.add
     return unless Rails.env.test?
-    gem_name, file_path, checksum = yield
-    ((@files ||= {})[gem_name] ||= {})[file_path] = checksum
+    path, value, expect = yield
+    if path.start_with? '/'
+      ((@values ||= {})[path] ||= {})[value] = expect
+    else
+      gem_name, file, checksum = path, value, expect
+      ((@files ||= {})[gem_name] ||= {})[file] = checksum
+    end
   end
 
   def self.verify_all!
     return unless @files.present?
     errors = @files.each_with_object({}) do |(gem_name, files), errors|
       root = Gem.root(gem_name)
-      files.each do |file_path, checksum_was|
-        path = root.join(file_path)
+      files.each do |file, checksum_was|
+        path = root.join(file)
         checksum = Digest::SHA256.hexdigest(path.read)
         errors[path] = checksum if checksum != checksum_was
       end
     end
+    errors = @values.each_with_object(errors) do |(path, values), errors|
+      values.each do |value, expect|
+        errors[path] = value if value != expect
+      end
+    end
     return if errors.empty?
-    raise "\n#{errors.map{ |(path, checksum)| "#{path}: #{checksum}" }.join("\n")}\ncount: #{errors.size}"
+    raise "\n#{errors.map{ |(path, value)| "#{path}: #{value}" }.join("\n")}\ncount: #{errors.size}"
   end
 end
