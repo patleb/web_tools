9949a4c5285674bad4b30fdbceb0d1678da16240
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-05 13:36:43 -0400

    added enqueued check to lib mailer test

--- ext_rails/test/mailers/lib_mailer_test.rb
+++ ext_rails/test/mailers/lib_mailer_test.rb
@@ -3,7 +3,7 @@ require './test/test_helper'
 class LibMailerTest < ActionMailer::TestCase
   self.use_transactional_tests = false
 
-  it 'should send healthcheck email' do
+  test '#healthcheck' do
     email = Setting.with(freeze:false) do |settings|
       settings[:mail_to] = 'example@test.com'
       assert_equal Setting[:mail_to], 'example@test.com'
@@ -16,5 +16,8 @@ class LibMailerTest < ActionMailer::TestCase
     assert_equal Setting[:mail_to], email.to
     assert_equal '[WebTools TEST] Healthcheck', email.subject
     assert_equal 'ok', email.text_part.body.to_s
+    assert_enqueued_email_with(LibMailer, :healthcheck, params: { a: 1, 'b' => 2 }) do
+      LibMailer.with(a: 1, 'b' => 2).healthcheck.deliver_later
+    end
   end
 end
