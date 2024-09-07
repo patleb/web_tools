063359bc01df04b0b621ba6cd9fd8e284f560c93
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-07 11:53:30 -0400

    support composite foreign key in belongs_to validates presence option

--- ext_rails/lib/ext_rails/active_record/base/with_required_belongs_to.rb
+++ ext_rails/lib/ext_rails/active_record/base/with_required_belongs_to.rb
@@ -7,7 +7,15 @@ module ActiveRecord::Base::WithRequiredBelongsTo
     def belongs_to(name, *, **options)
       unless options.has_key?(:optional) || options.has_key?(:required) || belongs_to_required_by_default
         options[:optional] = true
-        validates options[:foreign_key] || "#{name}_id", presence: true
+        Array.wrap(options[:foreign_key] || options[:query_constraints] || "#{name}_id").each do |foreign_key|
+          foreign_key = foreign_key.to_sym
+          next_foreign_key = false
+          reflect_on_all_associations.each do |association|
+            next unless association.belongs_to?
+            break (next_foreign_key = true) if association.foreign_key.to_sym == foreign_key
+          end
+          validates foreign_key, presence: true unless next_foreign_key
+        end
       end
       super
     end
