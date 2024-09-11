567245521d348c20b0f359e368e9c969c172c914
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-11 14:43:10 -0400

    use gen_random_uuid

--- mix_page/db/migrate/20020000002280_create_lib_pages.rb
+++ mix_page/db/migrate/20020000002280_create_lib_pages.rb
@@ -2,7 +2,7 @@ class CreateLibPages < ActiveRecord::Migration[7.1]
   def change
     create_table :lib_pages do |t|
       # TODO move uuid V5
-      t.uuid       :uuid,                 null: false, default: 'uuid_generate_v1mc()', index: { using: :hash }
+      t.uuid       :uuid,                 null: false, default: 'gen_random_uuid()', index: { using: :hash }
       t.integer    :type,                 null: false
       t.integer    :view,                 null: false, index: true
       t.belongs_to :page_layout,          foreign_key: { to_table: :lib_pages }
