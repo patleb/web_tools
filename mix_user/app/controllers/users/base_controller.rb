dc97e38ea3fe6c36033ed30fe690b2d10c224bb2
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-11 16:47:17 -0400

    reset the whole session on destroy

--- mix_user/app/controllers/users/base_controller.rb
+++ mix_user/app/controllers/users/base_controller.rb
@@ -12,16 +12,17 @@ module Users
       user.create_session! ip_address: request.remote_ip, user_agent: user_agent
       Current.user = user
       Current.role = user.role
-      clear_role
       session[:user_id] = user.id
+      session.delete(:role)
+      cookies.delete(:_role)
     end
 
     def destroy_session!(user)
       user.session&.destroy!
       Current.user = User::Null.new
       Current.role = :null
-      clear_role
-      session.delete(:user_id)
+      reset_session
+      cookies.clear
     end
 
     def send_email(name, user_scope = nil)
@@ -29,11 +30,6 @@ module Users
       UserMailer.with(user: user).public_send(name).deliver_later
     rescue ActiveRecord::RecordNotFound
       # do nothing
-    end
-
-    def clear_role
-      session.delete(:role)
-      cookies.delete(:_role)
     end
   end
 end
