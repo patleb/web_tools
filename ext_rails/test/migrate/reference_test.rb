adc0a6d30fb288e4814d8534f49b9d29a8e0e5f8
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-11 14:50:42 -0400

    correction

--- ext_rails/test/migrate/reference_test.rb
+++ ext_rails/test/migrate/reference_test.rb
@@ -4,9 +4,9 @@ class ReferenceTest < Sql::TestCase
   self.migrations = {
     '20010000002220_enable_pg_counter_cache' => ExtRails::Engine.root,
     '20010000002222_enable_pg_touch' => ExtRails::Engine.root,
-    '20180106225120_add_triggers_to_lib_pages' => MixPage::Engine.root,
-    '20180106225130_add_triggers_to_lib_page_fields' => MixPage::Engine.root,
-    '20210100000020_add_triggers_to_lib_searches' => MixSearch::Engine.root,
+    '20020000002284_add_triggers_to_lib_pages' => MixPage::Engine.root,
+    '20020000002286_add_triggers_to_lib_page_fields' => MixPage::Engine.root,
+    '20020000002444_add_triggers_to_lib_searches' => MixSearch::Engine.root,
   }
   self.sql_root = 'ext_rails/test/migrate'
 
