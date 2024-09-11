319aee61398078151c6f035f809af0a0a925c59e
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-11 15:00:10 -0400

    clearer

--- mix_admin/lib/mix_admin/engine.rb
+++ mix_admin/lib/mix_admin/engine.rb
@@ -1,6 +1,84 @@
 require 'mix_admin/configuration'
 require 'mix_admin/routes'
 
+module Admin
+  extend ActiveSupport::Autoload
+
+  autoload :Action
+  autoload :Configurable
+  autoload :Field
+  autoload :Group
+  autoload :Model
+  autoload :Section
+
+  module Actions
+    extend ActiveSupport::Autoload
+
+    autoload :Delete
+    autoload :Export
+    autoload :Index
+    autoload :New
+    autoload :Show
+    autoload :Trash
+  end
+
+  module Fields
+    extend ActiveSupport::Autoload
+
+    autoload :BelongsTo
+    autoload :BelongsToListParent
+    autoload :BelongsToPolymorphic
+    autoload :Boolean
+    autoload :BooleanArray
+    autoload :Code
+    autoload :Date
+    autoload :DateArray
+    autoload :DateTime
+    autoload :DateTimeArray
+    autoload :Decimal
+    autoload :DecimalArray
+    autoload :Discarded
+    autoload :Enum
+    autoload :EnumArray
+    autoload :EnumAttribute
+    autoload :EnumSti
+    autoload :ForeignKey
+    autoload :HasMany
+    autoload :HasOne
+    autoload :Hidden
+    autoload :Hstore
+    autoload :Integer
+    autoload :IntegerArray
+    autoload :Interval
+    autoload :IntervalArray
+    autoload :Json
+    autoload :JsonArray
+    autoload :Money
+    autoload :Password
+    autoload :Serialized
+    autoload :String
+    autoload :StringArray
+    autoload :Text
+    autoload :Time
+    autoload :TimeArray
+    autoload :Timestamp
+    autoload :TimestampArray
+    autoload :Uuid
+  end
+
+  module Sections
+    extend ActiveSupport::Autoload
+
+    autoload :Delete
+    autoload :Edit
+    autoload :Export
+    autoload :Index
+    autoload :Nested
+    autoload :New
+    autoload :Show
+  end
+end
+
 module MixAdmin
   class Engine < ::Rails::Engine
     require 'mix_global'
