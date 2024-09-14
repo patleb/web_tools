a487d03e7e6d0f3be048a740531f46932315bdfa
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-14 02:33:50 -0400

    added Hash#to_hwia shortcut

--- ext_ruby/lib/ext_ruby/hash_with_indifferent_access.rb
+++ ext_ruby/lib/ext_ruby/hash_with_indifferent_access.rb
@@ -1,3 +1,7 @@
+class Hash
+  alias_method :to_hwia, :with_indifferent_access
+end
+
 ActiveSupport::HashWithIndifferentAccess.class_eval do
   def self.convert_key(key)
     key.is_a?(String) && key.match?(Hash::KEYWORD) ? key.to_sym : key
