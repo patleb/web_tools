46a82a3e8488e8042a05ca2bf45e5af83da7a3e2
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-24 18:00:05 -0400

    added :end attribute to SocketEvent and Threat to find when it finishes

--- mix_server/app/models/monit/osquery/socket_event.rb
+++ mix_server/app/models/monit/osquery/socket_event.rb
@@ -11,18 +11,22 @@ module Monit
       attribute :remote_port, :integer
       attribute :connected, :boolean
       attribute :path
+      attribute :end, :boolean
 
       def self.list
-        (osquery['socket_events'] || []).flat_map do |rows|
-          rows[:new].map do |row|
-            pid, time, remote_ip, remote_port = row.values_at('pid', 'time', 'remote_address', 'remote_port')
-            {
-              id: [pid, time, remote_ip, remote_port].join(':'), pid: pid, time: Time.at(time).utc,
-              local_ip: row['local_address'], local_port: row['local_port'],
-              remote_ip: remote_ip, remote_port: remote_port,
-              connected: %w(connect bind).include?(row['action']),
-              path: row['cmdline'],
-            }
+        (osquery['socket_events'] || []).flat_map do |events|
+          events.select_map do |state, rows|
+            rows.map do |row|
+              pid, time, remote_ip, remote_port = row.values_at('pid', 'time', 'remote_address', 'remote_port')
+              {
+                id: [pid, time, remote_ip, remote_port].join(':'), pid: pid, time: Time.at(time).utc,
+                local_ip: row['local_address'], local_port: row['local_port'],
+                remote_ip: remote_ip, remote_port: remote_port,
+                connected: %w(connect bind).include?(row['action']),
+                path: row['cmdline'],
+                end: state == :old
+              }
+            end
           end
         end
       end
--- mix_server/app/models/monit/osquery/threat.rb
+++ mix_server/app/models/monit/osquery/threat.rb
@@ -3,12 +3,16 @@ module Monit
     class Threat < Base
       attribute :name
       attribute :time, :datetime
+      attribute :end, :boolean
 
       def self.list
-        (osquery.except(*LogLines::Osquery.monitors) || {}).select_map do |name, rows|
-          next if rows[:new].empty?
-          time = rows[:time]
-          { id: [name, time].join(':'), name: name, time: Time.at(time).utc }
+        (osquery.except(*LogLines::Osquery.monitors) || {}).flat_map do |name, threats|
+          threats.select_map do |state, rows|
+            rows.map do |row|
+              time = row[:time]
+              { id: [name, time].join(':'), name: name, time: Time.at(time).utc, end: state == :old }
+            end
+          end
         end
       end
     end
