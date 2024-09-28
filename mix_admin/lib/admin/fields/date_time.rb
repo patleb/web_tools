a3155515b01ac76c956830ee7eb159a532c17014
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-28 05:40:04 -0400

    simplify by using ::Time.zone.parse

--- mix_admin/lib/admin/fields/date_time.rb
+++ mix_admin/lib/admin/fields/date_time.rb
@@ -28,16 +28,7 @@ module Admin
       end
 
       def parse_value(value)
-        return if value.blank?
-        return value if %w(DateTime Date Time).include?(value.class.name)
-        return if (delocalized_value = delocalize(value)).blank?
-        begin
-          # Adjust with the correct timezone and daylight saving time
-          datetime_with_wrong_tz = ::Time.strptime(delocalized_value, strftime_format.gsub('%-d', '%d'))
-          ::Time.parse_utc(datetime_with_wrong_tz.strftime('%Y-%m-%d %H:%M:%S'))
-        rescue ArgumentError
-          nil
-        end
+        ::Time.zone.parse(value) if value.present?
       end
 
       def format_value(value)
@@ -75,70 +66,6 @@ module Admin
 
       def search_type
         :datetime
-      end
-
-      def delocalize(date_string, format = strftime_format)
-        return date_string if Current.locale == :en
-        format.to_s.scan(/%[AaBbp]/) do |match|
-          case match
-          when '%A'
-            english = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
-            day_names.each_with_index { |d, i| date_string = date_string.gsub(/#{d}/, english[i]) }
-          when '%a'
-            english = %w(Sun Mon Tue Wed Thu Fri Sat)
-            abbr_day_names.each_with_index { |d, i| date_string = date_string.gsub(/#{d}/, english[i]) }
-          when '%B'
-            english = %w(January February March April May June July August September October November December)
-            month_names.each_with_index { |m, i| date_string = date_string.gsub(/#{m}/, english[i]) }
-          when '%b'
-            english = %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
-            abbr_month_names.each_with_index { |m, i| date_string = date_string.gsub(/#{m}/, english[i]) }
-          when '%p'
-            date_string = date_string.gsub(/#{I18n.t('date.time.am', default: 'am')}/, 'am')
-            date_string = date_string.gsub(/#{I18n.t('date.time.pm', default: 'pm')}/, 'pm')
-          end
-        end
-        date_string
-      end
-
-      def abbr_day_names
-        (@abbr_day_names ||= {})[Current.locale] ||= begin
-          I18n.t('date.abbr_day_names', raise: true)
-        rescue I18n::ArgumentError
-          %w(Sun Mon Tue Wed Thu Fri Sat)
-        end
-      end
-
-      def abbr_month_names
-        (@abbr_month_names ||= {})[Current.locale] ||= begin
-          I18n.t('date.abbr_month_names', raise: true)[1..-1]
-        rescue I18n::ArgumentError
-          %w(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec)
-        end
-      end
-
-      def date_format
-        (@date_format ||= {})[Current.locale] ||= begin
-          I18n.t('date.formats.default', raise: true)
-        rescue
-          '%Y-%m-%d'
-        end
-      end
-
-      def day_names
-        (@day_names ||= {})[Current.locale] ||= begin
-          I18n.t('date.day_names', raise: true)
-        rescue I18n::ArgumentError
-          %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
-        end
-      end
-
-      def month_names
-        (@month_names ||= {})[Current.locale] ||= begin
-          I18n.t('date.month_names', raise: true)[1..-1]
-        rescue I18n::ArgumentError
-          %w(January February March April May June July August September October November December)
-        end
       end
     end
   end
