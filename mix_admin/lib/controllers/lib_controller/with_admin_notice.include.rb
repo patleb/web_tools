f8a50cb9436d9fcc29113412b874763876770130
Patrice Lebel <patleb@users.noreply.github.com>
2024-09-29 16:26:01 -0400

    cleaner flash message notices

--- mix_admin/lib/controllers/lib_controller/with_admin_notice.include.rb
+++ mix_admin/lib/controllers/lib_controller/with_admin_notice.include.rb
@@ -1,15 +1,11 @@
 module LibController::WithAdminNotice
   def success_notice(name, action = action_name)
-    t('admin.flash.successful', name: name, action: t("admin.actions.#{action}.done"))
+    t('admin.flash.successful', name: name, action: t("admin.actions.#{action}.done")) << '.'
   end
 
   def error_notice(objects, name, action = action_name)
-    notice = t('admin.flash.error', name: name, action: t("admin.actions.#{action}.done"))
-    Array.wrap(objects).each do |object|
-      unless object.errors.empty?
-        notice += ExtRails::ERROR_SEPARATOR + object.errors.full_messages.join(ExtRails::ERROR_SEPARATOR)
-      end
-    end
-    simple_format! notice
+    lines = [t('admin.flash.error', name: name, action: t("admin.actions.#{action}.done"))]
+    lines.concat Array.wrap(objects).flat_map{ |object| object.errors.full_messages }.compact_blank
+    helpers.simple_format! lines.join(".\n") << '.'
   end
 end
