d92a8f7de79190b4184b2d585d40972d177aa35b
Patrice Lebel <patleb@users.noreply.github.com>
2024-10-22 14:49:22 -0400

    note added for HashWithIndifferentAccess

--- ext_ruby/lib/ext_ruby/hash_with_indifferent_access.rb
+++ ext_ruby/lib/ext_ruby/hash_with_indifferent_access.rb
@@ -2,6 +2,11 @@ class Hash
   alias_method :to_hwia, :with_indifferent_access
 end
 
+# NOTE
+# (hash_with_indifferent_access[key] ||= {})
+# if not assigned already, it returns {} instead of the converted one
+# so, either use (hash_with_indifferent_access[key] ||= {}.with_indifferent_access)
+# or use (hash[key] ||= {}) and convert the root node after assignments
 ActiveSupport::HashWithIndifferentAccess.class_eval do
   def self.convert_key(key)
     key.is_a?(String) && key.match?(Hash::KEYWORD) ? key.to_sym : key
