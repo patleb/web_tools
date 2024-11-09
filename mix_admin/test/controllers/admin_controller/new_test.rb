1ad4770b19ca50d2108b145752aa4ccf5b956761
Patrice Lebel <patleb@users.noreply.github.com>
2024-11-09 02:04:45 -0500

    tests for new fields added

--- mix_admin/test/controllers/admin_controller/new_test.rb
+++ mix_admin/test/controllers/admin_controller/new_test.rb
@@ -13,19 +13,67 @@ class AdminController::NewTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_layout :collection, :new, path: '/new'
     assert_group 'Nouveau(elle) Record extension' do |group|
-      string_field = group.fields_hash[:string]
-      assert_nil group.fields_hash[:id]
-      assert_equal 'string_field string_type', string_field.css_class
-      assert_equal 'String', string_field.label
-      assert_equal '-', string_field.pretty_value
-      assert_equal "<input type='text' name='string' class='input input-bordered' required='required' id='string'></input>",
-        string_field.pretty_input.gsub("\"", "'")
+      assert_field(group.fields_hash[:string],
+        'string_field string_type',
+        /Obligatoire.+String\*.+Longueur maximale 50.+tooltip/,
+        '-',
+        '<input type="text" name="string" class="input input-bordered" required="required" maxlength="50" id="string"></input>',
+      )
+      assert_field(group.fields_hash[:text],
+        'text_field text_type',
+        '<label>Text</label>',
+        '-',
+        %{<textarea name="text" class="textarea textarea-bordered" id="text">\n</textarea>},
+      )
+      assert_field(group.fields_hash[:lock_version],
+        'lock_version_field hidden_type',
+        false,
+        0,
+        '<input type="hidden" name="lock_version" class="input input-bordered" value="0" id="lock_version" autocomplete="off"></input>',
+      )
+      assert_field(group.fields_hash[:password],
+        'password_field password_type',
+        '<label>Password</label>',
+        '-',
+        '<input type="password" name="password" class="input input-bordered" id="password"></input>',
+      )
+      assert_field(group.fields_hash[:integer],
+        'integer_field enum_type',
+        '<label>Integer</label>',
+        'Zero',
+        '<select name="integer" class="input input-bordered" value="0" id="integer">'\
+          '<option value=""> </option>'\
+          '<option value="0" selected="selected">Zero</option>'\
+          '<option value="1">One</option>'\
+          '<option value="2">Two</option>'\
+          '<option value="3">Three</option>'\
+          '<option value="4">Four</option>'\
+          '<option value="5">Five</option>'\
+        '</select>',
+      )
+      assert group.fields_hash.reject{ |_, f| f.readonly? }.slice(:id, :deleted_at, :nested_record_name, :related_records_id).empty?
     end
   end
 
   test 'POST :new' do
-    post "/model/#{model_name}/new", params: { model_name.underscore => { string: 'new-string' } }
+    post "/model/#{model_name}/new", params: { model_name.underscore => { string: 'new-string', integer: 1 } }
 
-    assert Test::Extensions::RecordExtension.find_by(string: 'new-string')
+    assert Test::Extensions::RecordExtension.find_by(string: 'new-string', integer: 'one')
+  end
+
+  private
+
+  def assert_field(field, css_class, pretty_label, pretty_value, pretty_input)
+    assert_equal css_class, field.css_class
+    case pretty_label
+    when Regexp
+      assert_match pretty_label, field.pretty_label
+    when Boolean
+      assert_equal pretty_label, field.label
+    else
+      assert_equal pretty_label, field.pretty_label
+    end
+    assert_equal pretty_value, field.pretty_value
+    assert_equal pretty_input, field.pretty_input
   end
 end
