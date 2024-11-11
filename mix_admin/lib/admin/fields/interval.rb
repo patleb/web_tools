e8b79cc80c896b68ab90402e356fdb61958b96f5
Patrice Lebel <patleb@users.noreply.github.com>
2024-11-11 09:18:23 -0500

    clean-up

--- mix_admin/lib/admin/fields/interval.rb
+++ mix_admin/lib/admin/fields/interval.rb
@@ -16,7 +16,7 @@ module Admin
       def format_value(value)
         return unless value
         unit, compact = pretty_format
-        value = (unit == :iso8601) ? value.iso8601 : value.to_s(unit, compact: compact == :compact)
+        value = (unit == :iso8601) ? value.iso8601 : value.to_s(unit, compact: compact)
         value.html_safe
       end
     end
