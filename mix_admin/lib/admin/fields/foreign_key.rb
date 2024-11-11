d84fa1a26426963a3c04cd4e597cbfabf78a4afe
Patrice Lebel <patleb@users.noreply.github.com>
2024-11-11 10:54:16 -0500

    ForeignKey field refactored

--- mix_admin/lib/admin/fields/foreign_key.rb
+++ mix_admin/lib/admin/fields/foreign_key.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 module Admin
   module Fields
-    class ForeignKey < Integer
+    class ForeignKey < Admin::Field
       def self.has?(section, property)
-        section.model.associations.any? do |association|
+        section.model.associations.find do |association|
           association.type == :belongs_to && association.foreign_key == property.name
         end
       end
+      class << self
+        alias_method :association_for, :has?
+      end
 
-      register_option :property_model_name, memoize: true do
-        model_name = column_name.to_s.delete_suffix('_id').camelize
-        if (namespace = model.model_name.deconstantize).present?
-          namespaced_name = "#{namespace}::#{model_name}"
-          model_name = namespaced_name if namespaced_name.to_const
-        end
-        model_name
+      register_option :array_separator do
+        '<br>'.html_safe
+      end
+
+      register_option :array_bullet do
+        '- '.html_safe
       end
 
       def format_value(value)
@@ -24,13 +28,21 @@ module Admin
         foreign_key_link(value)
       end
 
+      def input_type
+        :number
+      end
+
+      def search_type
+        :numeric
+      end
+
       def foreign_key_link(label)
         url = property_model.viewable_url(id: value)
         url ? a_('.link.text-primary', text: label, href: url) : label
       end
 
       def property_model
-        @property_model ||= property_model_name.to_const.admin_model
+        @property_model ||= self.class.association_for(section, property).klass.admin_model
       end
     end
   end
