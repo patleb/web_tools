588e74393d0a2b2f467ad97eaafb7ddc8d535279
Patrice Lebel <patleb@users.noreply.github.com>
2024-11-11 09:11:08 -0500

    correction

--- ext_rails/lib/ext_rails/active_support/duration.rb
+++ ext_rails/lib/ext_rails/active_support/duration.rb
@@ -6,7 +6,8 @@ module ActiveSupport
     def to_s(unit = nil, compact: false)
       return to_s_without_format if unit.nil?
       parts = @parts.slice(*self.class::PARTS[UNITS[unit] + 1..-1]).map{ |unit, val| i18n(unit, val, compact) }
-      [i18n(unit, public_send("in_#{unit}").floor, compact)].concat(parts).to_sentence(last_word_connector: ', ')
+      parts = [i18n(unit, public_send("in_#{unit}").floor, compact)].concat(parts)
+      parts.join(', ')
     end
 
     private
