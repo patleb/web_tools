179891a41a68c29e143cdadab142a930f0b1b04b
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-02 18:48:59 -0500

    show website link if page_template is nil

--- mix_page/app/presenters/admin/page_field_presenter.rb
+++ mix_page/app/presenters/admin/page_field_presenter.rb
@@ -3,7 +3,11 @@ module Admin
     record_label_method :field_label
 
     field :lock_version
-    nests :page_template, as: :view
+    nests :page_template do
+      field :view do
+        pretty_blank{ presenter.website_link }
+      end
+    end
     field :name
 
     group :audit do
@@ -12,6 +16,10 @@ module Admin
       nests :creator, as: :email
       field :updated_at
       field :created_at
+    end
+
+    def website_link
+      a_ '.link.text-primary', t('link.website'), href: pages_root_path
     end
   end
 end
