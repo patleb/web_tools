2547f0b53596961ef39e9668d938ff697ee95237
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-05 11:49:50 -0500

    moved html5sortable to ext_coffee

--- ext_coffee/lib/javascript/all.js
+++ ext_coffee/lib/javascript/all.js
@@ -4,18 +4,22 @@ var image_path = function(name) { return images(name, true) }
 // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
 import lru from 'tiny-lru/lib/tiny-lru'
 import Cookies from 'js-cookie'
+import { enableDragDropTouch } from 'drag-drop-touch'
+import sortable from 'html5sortable/dist/html5sortable.es'
 
 import '@@lib/ext_coffee/core_ext/all'
-import '@@vendor/turbolinks/all'
 import '@@lib/ext_coffee/rescue'
 import '@@lib/ext_coffee/index'
 import '@@lib/ext_coffee/concepts'
 import '@@lib/ext_coffee/state_machine'
 import '@@lib/ext_coffee/concepts/all'
 import '@@lib/ext_coffee/sm/all'
+import '@@vendor/turbolinks/all'
 
 document.addEventListener('DOMContentLoaded', function() {
   window.$image = image_path
   window.lru = lru
   window.Cookies = Cookies
+  window.sortable = sortable
+  enableDragDropTouch()
 })
