f682f6bae224f8303b594e42ef83cb69a18cff93
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-06 01:00:51 -0500

    correction

--- mix_admin/lib/admin/model/definable.rb
+++ mix_admin/lib/admin/model/definable.rb
@@ -195,8 +195,10 @@ module Admin::Model::Definable
   def create_config_instance(configs, configs_name, name, klass, klass_name)
     if klass.name != klass_name
       klass_name.clear_const
+      klass_name = klass_name.demodulize
       klass = Class.new(klass)
-      @section.class.const_set(klass_name.demodulize, klass)
+      @section.class.send(:remove_const, klass_name) if @section.class.const_defined? klass_name
+      @section.class.const_set(klass_name, klass)
     end
     config = klass.new(name: name, model: self, section: @section, section_was: @section)
     values = config.ivar(:@values)
