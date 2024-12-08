e56e6543dfe3eec444c4eac727549c0d5e3c03b9
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-07 19:50:32 -0500

    admin js tests updated

---
+++ mix_admin/test/javascript/concepts/admin/search_concept_test.js
@@ -0,0 +1,120 @@
+import concepts from './../spec_helper'
+
+describe('Js.Admin.SearchConcept', () => {
+  concepts.with_page('admin/search', false)
+
+  test('.js_query_datetime[type=date]', () => {
+    const search = dom.find('.js_search')
+    const input = dom.find('.js_query_datetime[type=date]')
+    const values = [
+      ['2023-01-01', '2023-01-01'],
+      ['=',          '=2023-01-01'],  // '=', '==', '!', '!=', '<', '<=', '>', '>='
+      [' ',          ' =2023-01-01'], // '', ' ', '}', '|'
+    ]
+    for (const [value_was, value] of values) {
+      input.value = '2023-01-01'
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      dom.fire('change', { target: input })
+    }
+  })
+
+  test('.js_query_datetime[type=time]', () => {
+    const search = dom.find('.js_search')
+    const input = dom.find('.js_query_datetime[type=time]')
+    const values = [
+      ['23:01:01',    '23:01:01'],
+      ['=',           '=23:01:01'],
+      [' ',           ' =23:01:01'],
+      ['2023-01-01T', '2023-01-01T23:01:01'],
+      ['2023-01-01',  '2023-01-01T23:01:01'],
+    ]
+    for (const [value_was, value] of values) {
+      input.value = '23:01:01'
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      dom.fire('change', { target: input })
+    }
+  })
+
+  test('.js_query_keyword', () => {
+    const search = dom.find('.js_search')
+    const input = dom.find('.js_query_keyword')
+    const values = [
+      ['<', '<'],
+      ['=', '=_true'],
+      [' ', ' =_true'],
+    ]
+    for (const [value_was, value] of values) {
+      input.set_value('_true')
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      dom.fire('change', { target: input })
+    }
+  })
+
+  test('.js_query_operator', () => {
+    const search = dom.find('.js_search')
+    const input = dom.find('.js_query_operator')
+    const values = [
+      ['=', '='],
+      [' ', ' ='],
+    ]
+    for (const [value_was, value] of values) {
+      input.set_value('=')
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      dom.fire('change', { target: input })
+    }
+  })
+
+  test('.js_query_or', () => {
+    const search = dom.find('.js_search')
+    const or = dom.find('.js_query_or')
+    const values = [
+      ['=',      '='],
+      [' ',      ' '],
+      ['{id}=1', '{id}=1|']
+    ]
+    for (const [value_was, value] of values) {
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      or.click()
+    }
+  })
+
+  test('.js_query_and', () => {
+    const search = dom.find('.js_search')
+    const and = dom.find('.js_query_and')
+    const values = [
+      ['=',      '='],
+      [' ',      ' '],
+      ['{id}=1', '{id}=1 {_}']
+    ]
+    for (const [value_was, value] of values) {
+      search.value = value_was
+      dom.on_event({ 'change': () => { assert.equal(value, search.value) }})
+      and.click()
+    }
+  })
+
+  test('.js_query_field', () => {
+    const search = dom.find('.js_search')
+    const field = dom.find('.js_query_field')
+    const values = [
+      ['{_}',    '{_}'],
+      ['{id}==', '{id}=='],
+      ['',       '{id}'],
+      [' ',      ' {id}'],
+      ['{',      '{id'],
+      ['{id}',   '{id|id}'],
+      ['{id|',   '{id|'],
+      ['{id}=1', '{id}=1 {id}'],
+    ]
+    for (const [value_was, value] of values) {
+      search.value = value_was
+      dom.on_event({ 'click': () => { assert.equal(value, search.value) }})
+      field.click()
+    }
+  })
+})
--- mix_admin/test/javascript/concepts/spec_helper.js
+++ mix_admin/test/javascript/concepts/spec_helper.js
@@ -2,5 +2,10 @@ import concepts from '@@test/ext_coffee/concepts/spec_helper'
 import '@@lib/mix_admin/all'
 
 Js.Concepts.add_concept('Js.AdminConcept')
+Js.Concepts.add_module('Js.Admin')
+
+beforeAll(() => {
+  fixture.set_root('mix_admin/test/fixtures/files/concepts')
+})
 
 module.exports = concepts
