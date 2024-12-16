75a0520b27720a6633a62cb49c040b1bb2631df8
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-16 10:00:19 -0500

    admin tests correction

--- mix_admin/test/javascript/concepts/admin/markdown_concept_test.js
+++ mix_admin/test/javascript/concepts/admin/markdown_concept_test.js
@@ -11,31 +11,31 @@ describe('Js.Admin.MarkdownConcept', () => {
   beforeEach(() => {
     concept = Js.Admin.MarkdownConcept
     textarea = dom.find('.js_markdown')
-    undo = dom.find('.js_markdown_toolbar .undo')
-    redo = dom.find('.js_markdown_toolbar .redo')
+    undo = dom.find('.js_markdown_toolbar .js_undo')
+    redo = dom.find('.js_markdown_toolbar .js_redo')
   })
 
   test('#push_history, #undo_history, #redo_history', () => {
     assert.equal('text', textarea.get_value())
-    assert.equal({ push: ['text'], redo: [], undo: [] }, concept.get_history(textarea))
+    assert.equal({ undo: [], push: ['text'], redo: [] }, concept.get_history(textarea))
 
     undo.click()
-    assert.equal({ push: ['text'], redo: [], undo: [] }, concept.get_history(textarea))
+    assert.equal({ undo: [], push: ['text'], redo: [] }, concept.get_history(textarea))
 
     redo.click()
-    assert.equal({ push: ['text'], undo: [], redo: [] }, concept.get_history(textarea))
+    assert.equal({ undo: [], push: ['text'], redo: [] }, concept.get_history(textarea))
 
     textarea.set_value('new text', { event: true })
-    assert.equal({ 'page_fields_html[text_fr]': { push: ['new text'], redo: [], undo: ['text', 'new text'] } }, concept.history)
+    assert.equal({ 'page_fields_html[text_fr]': { undo: ['text', 'new text'], push: ['new text'], redo: [] } }, concept.history)
 
     textarea.set_value('new text', { event: true })
-    assert.equal({ 'page_fields_html[text_fr]': { push: ['new text'], redo: [], undo: ['text', 'new text'] } }, concept.history)
+    assert.equal({ 'page_fields_html[text_fr]': { undo: ['text', 'new text'], push: ['new text'], redo: [] } }, concept.history)
 
     textarea.set_value('changed text', { event: true })
-    assert.equal({ 'page_fields_html[text_fr]': { push: ['changed text'], redo: [], undo: ['text', 'new text', 'changed text'] } }, concept.history)
+    assert.equal({ 'page_fields_html[text_fr]': { undo: ['text', 'new text', 'changed text'], push: ['changed text'], redo: [] } }, concept.history)
 
     undo.click()
-    assert.equal({ 'page_fields_html[text_fr]': { push: ['new text'], redo: ['changed text'], undo: ['text'] } }, concept.history)
+    assert.equal({ 'page_fields_html[text_fr]': { undo: ['text'], push: ['new text'], redo: ['changed text'] } }, concept.history)
 
     redo.click()
     assert.equal({ 'page_fields_html[text_fr]': { push: ['changed text'], redo: [], undo: ['text', 'new text'] } }, concept.history)
