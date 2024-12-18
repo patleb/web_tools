a50b6e4ab9ca667607a5ef44ebdf691913ea92bc
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-18 04:43:23 -0500

    MixFile test updated

--- mix_file/test/models/active_storage/blob_test.rb
+++ mix_file/test/models/active_storage/blob_test.rb
@@ -1,15 +1,17 @@
-require './test/rails_helper'
+require './test/test_helper'
 
 class ActiveStorage::BlobTest < ActiveStorage::TestCase
   self.file_fixture_path = Gem.root('mix_file').join('test/fixtures/files').to_s
 
+  let(:filename){ 'circle.png' }
   let(:key){ 'i1uq3fq2bepgpxutsyx64te1silp' }
 
-  it 'should optimize file size' do
-    file = file_fixture('circle.png')
-    blob = ActiveStorage::Blob.create_and_upload! io: file.open, key: key, filename: 'circle.png', service_name: 'test'
+  test '#find_or_create_by_uid!' do
+    file = file_fixture(filename)
+    data = file.open
+    blob = ActiveStorage::Blob.find_or_create_by_uid! filename, data, key: key, service_name: 'test'
     assert_equal 452, blob.byte_size
     blob.optimize
-    assert_equal 214, blob.byte_size
+    assert_equal 216, blob.byte_size
   end
 end
