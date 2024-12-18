6479da27dc3672549dc8072254b9f4ffe92c7c45
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-18 04:53:30 -0500

    - clean-up #has_(one|many)_attached scope override; - make sure that attachment_reflections works both for strings and symbols;

--- mix_file/lib/mix_file/active_record/base/with_file.rb
+++ mix_file/lib/mix_file/active_record/base/with_file.rb
@@ -1,45 +1,26 @@
+MonkeyPatch.add{['activestorage', 'lib/active_storage/reflection.rb', 'c22d50cfe01f808b6a8b5171daa161b1e4190a9b0277fec18a0e64eac383a4b2']}
+
 module ActiveRecord::Base::WithFile
   extend ActiveSupport::Concern
 
-  # TODO correct :through globally --> record_type is nil
-  class_methods do
-    def has_one_attached(name, allow_destroy: nil, **options)
-      super(name, **options)
+  prepended do
+    class_attribute :attachment_reflections, instance_writer: false, default: {}.with_indifferent_access
+  end
 
-      _has_attached_with_destroy(name, allow_destroy, **options) if allow_destroy
+  class_methods do
+    def has_one_attached(name, ...)
+      super
 
       define_method "#{name}_blob" do
         super().rewhere(ActiveStorage::Attachment.table_name => { record_type: ActiveStorage::Attachment.record_types[self.class.name] })
       end
     end
 
-    def has_many_attached(name, allow_destroy: nil, **options)
-      super(name, **options)
-
-      _has_attached_with_destroy(name, allow_destroy, **options) if allow_destroy
+    def has_many_attached(name, ...)
+      super
 
       define_method "#{name}_blobs" do
         super().rewhere(ActiveStorage::Attachment.table_name => { record_type: ActiveStorage::Attachment.record_types[self.class.name] })
-      end
-    end
-
-    private
-
-    def _has_attached_with_destroy(name, allow_destroy, dependent: nil, **)
-      case allow_destroy
-      when true
-        remove_name = :"remove_#{name}"
-      when String, Symbol
-        remove_name = :"#{allow_destroy}_#{name}"
-      else
-        raise ArgumentError
-      end
-      attr_accessor remove_name
-      case dependent
-      when :purge_later, :purge
-        after_save{ public_send(name).send(dependent) if public_send(remove_name).to_b }
-      else
-        before_save{ public_send(name).detach if public_send(remove_name).to_b }
       end
     end
   end
