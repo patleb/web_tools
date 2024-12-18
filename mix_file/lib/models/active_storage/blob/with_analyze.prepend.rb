0a535e2d9e19516ffade6a8376c78be2bf412fdb
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-18 06:59:05 -0500

    MonkeyPatch check added for blob/analyzable

--- mix_file/lib/models/active_storage/blob/with_analyze.prepend.rb
+++ mix_file/lib/models/active_storage/blob/with_analyze.prepend.rb
@@ -1,3 +1,5 @@
+MonkeyPatch.add{['activestorage', 'app/models/active_storage/blob/analyzable.rb', 'd96f3eac18b413d12c146584d40fae6b7a8c8a322d5e86e86bb914a64ecce2a5']}
+
 module ActiveStorage::Blob::WithAnalyze
   def analyze
     with_lock do
