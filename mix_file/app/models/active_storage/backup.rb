b0e17c3f85ca16ae5d639562849e3067cd53e976
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-18 04:38:08 -0500

    MixFile migrations clean-up

--- mix_file/app/models/active_storage/backup.rb
+++ mix_file/app/models/active_storage/backup.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class ActiveStorage::Backup < ActiveStorage::Record
-  self.table_name = 'active_storage_backups'
-
   belongs_to :blob, class_name: 'ActiveStorage::Blob'
 end
--- mix_file/db/migrate/20020000002040_create_active_storage_tables.rb
+++ mix_file/db/migrate/20020000002040_create_active_storage_tables.rb
@@ -8,7 +8,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.1]
       t.string   :service_name, null: false
       t.bigint   :byte_size,    null: false
       t.string   :checksum
-      t.string   :uid,          null: false, index: { using: :hash }
+      t.string   :uid,          index: { using: :hash }
       t.datetime :created_at,   null: false
     end
 
@@ -16,20 +16,17 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.1]
       t.integer    :name,        null: false
       t.bigint     :record_id,   null: false
       t.integer    :record_type, null: false
-      t.belongs_to :blob,        null: false
-
-      t.datetime :created_at,    null: false
+      t.belongs_to :blob,        null: false, foreign_key: { to_table: :active_storage_blobs }
+      t.datetime   :created_at,  null: false
 
       t.index [:record_type, :record_id, :name, :blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
-      t.foreign_key :active_storage_blobs, column: :blob_id
     end
 
     create_table :active_storage_variant_records do |t|
-      t.belongs_to :blob,         null: false, index: false
+      t.belongs_to :blob,         null: false, foreign_key: { to_table: :active_storage_blobs }, index: false
       t.string :variation_digest, null: false
 
       t.index [:blob_id, :variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
-      t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
 end
