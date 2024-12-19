79ea3e6cb8cb9d8b24303779ad42f0c256393adf
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-19 08:59:15 -0500

    #similar_to now returns an array

--- mix_geo/app/models/geo_state.rb
+++ mix_geo/app/models/geo_state.rb
@@ -4,7 +4,7 @@ class GeoState < LibMainRecord
   belongs_to :geo_country
 
   def self.find_by_similarity(country, *tokens)
-    where(country_code: country).similar_to(*tokens).take
+    where(country_code: country).similar_to(*tokens).first
   end
 
   def raw_search_words
