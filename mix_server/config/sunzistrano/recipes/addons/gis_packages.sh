faf1e837d0c281d28124eae047331bcadee19d44
Patrice Lebel <patleb@users.noreply.github.com>
2024-12-19 07:33:13 -0500

    clean-up

--- mix_server/config/sunzistrano/recipes/addons/gis_packages.sh
+++ mix_server/config/sunzistrano/recipes/addons/gis_packages.sh
@@ -1,10 +1,8 @@
 sun.install "gdal-bin"
 sun.install "libgdal-dev"
-# python3-pyproj --> too old 1.9.4
 # pyproj==3.0.1 --> Minimum supported PROJ version is 7.2.0, installed version is 4.9.3
 sudo -H pip3 install pyproj==2.6.1
 sudo -H pip3 install rasterio==1.1.8
-# TODO python3-matplotlib
 sudo -H pip3 install matplotlib==3.2.1
 sun.install "python3-tk"
 sun.install "python3-numpy"
