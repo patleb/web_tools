64f78c671304a866abe4bf168b343e4cb02ca87c
Patrice Lebel <patleb@users.noreply.github.com>
2025-01-04 09:04:12 -0500

    Setting.authorized_keys added

--- mix_server/config/setting.rb
+++ mix_server/config/setting.rb
@@ -2,4 +2,8 @@ Setting.class_eval do
   def self.ftp_host_path
     "/#{stage}"
   end
+
+  def self.authorized_keys
+    Set.new([self[:owner_public_key]]).merge(self[:authorized_keys]).compact_blank
+  end
 end
--- mix_server/lib/mix_server/sh/authorized_keys.rb
+++ mix_server/lib/mix_server/sh/authorized_keys.rb
@@ -1,8 +1,8 @@
 module Sh::AuthorizedKeys
   def build_authorized_keys
-    keys = [Setting[:owner_public_key]].concat(Setting[:authorized_keys]).reject(&:blank?)
-    raise ':owner_public_key or :authorized_keys must be present' unless keys.any?
-
+    if (keys = Setting.authorized_keys).empty?
+      raise ':owner_public_key or :authorized_keys must be present'
+    end
     "echo -e '#{keys.join("\\n")}' > /home/deployer/.ssh/authorized_keys"
   end
 end
