6a9e6115cf3da1b790211dffe152036c1a258a22
Patrice Lebel <patleb@users.noreply.github.com>
2025-01-05 10:04:26 -0500

    do not reuse schema dump for db:migrate

--- ext_rails/lib/ext_rails/active_record/tasks/database_tasks/with_single_env.rb
+++ ext_rails/lib/ext_rails/active_record/tasks/database_tasks/with_single_env.rb
@@ -4,9 +4,31 @@ module ActiveRecord::Tasks::DatabaseTasks::WithSingleEnv
   extend ActiveSupport::Concern
 
   class_methods do
+    private
+
     def each_current_configuration(...)
       ENV['SKIP_TEST_DATABASE'] = 'true'
       super
+    end
+
+    def initialize_database(db_config)
+      with_temporary_pool(db_config) do
+        begin
+          database_already_initialized = migration_connection_pool.schema_migration.table_exists?
+        rescue ActiveRecord::NoDatabaseError
+          create(db_config)
+          retry
+        end
+
+        # unless database_already_initialized
+        #   schema_dump_path = schema_dump_path(db_config)
+        #   if schema_dump_path && File.exist?(schema_dump_path)
+        #     load_schema(db_config, ActiveRecord.schema_format, nil)
+        #   end
+        # end
+
+        !database_already_initialized
+      end
     end
   end
 end
