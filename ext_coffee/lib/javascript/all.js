ee820ccd858a37754da284636065140aaa4e77a7
Patrice Lebel <patleb@users.noreply.github.com>
2025-01-31 00:57:03 -0500

    tiny-lru update

--- ext_coffee/lib/javascript/all.js
+++ ext_coffee/lib/javascript/all.js
@@ -2,7 +2,7 @@ var images = require.context('@@/images', true)
 var image_path = function(name) { return images(name, true) }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
-import lru from 'tiny-lru/lib/tiny-lru'
+import { lru } from 'tiny-lru'
 import Cookies from 'js-cookie'
 import { enableDragDropTouch } from 'drag-drop-touch'
 import sortable from 'html5sortable/dist/html5sortable.es'
