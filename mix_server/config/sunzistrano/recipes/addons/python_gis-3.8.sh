db97540da2c8bdc614a68a86b6d02226c23a653d
Patrice Lebel <patleb@users.noreply.github.com>
2025-01-31 13:15:33 -0500

    do not use 'addons/python_gis-3.8', it conflicts with system python

--- mix_server/config/sunzistrano/recipes/addons/python_gis-3.8.sh
+++ mix_server/config/sunzistrano/recipes/addons/python_gis-3.8.sh
@@ -22,3 +22,16 @@ sudo -H pip3.8 install --root-user-action ignore netcdf4==1.5.8
 
 python3.8 --version | tr '[:upper:]' '[:lower:]'
 pip3.8 --version
+
+: '
+# rollback
+yes | sudo -H pip3.8 uninstall --root-user-action ignore netcdf4==1.5.8
+yes | sudo -H pip3.8 uninstall --root-user-action ignore psycopg2==2.8.6
+yes | sudo -H pip3.8 uninstall --root-user-action ignore matplotlib==3.2.1
+yes | sudo -H pip3.8 uninstall --root-user-action ignore rasterio==1.1.8
+yes | sudo -H pip3.8 uninstall --root-user-action ignore pyproj==2.6.1
+sudo apt-get remove -y python3.8-tk python3.8-dev python3.8-distutils python3.8
+sudo apt autoremove -y
+sudo add-apt-repository -r ppa:deadsnakes/ppa
+sudo rm -f /usr/local/bin/pip*
+'
