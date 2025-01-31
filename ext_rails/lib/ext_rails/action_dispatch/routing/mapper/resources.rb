b7dcf2f5e4871a8a9bc989db5c7dee0938547e0b
Patrice Lebel <patleb@users.noreply.github.com>
2025-01-31 05:00:56 -0500

    prevent warnings

--- ext_rails/lib/ext_rails/action_dispatch/routing/mapper/resources.rb
+++ ext_rails/lib/ext_rails/action_dispatch/routing/mapper/resources.rb
@@ -6,15 +6,15 @@ module ActionDispatch::Routing::Mapper::Resources
     actions = Set.new(except ? available_actions - Array(except).map(&:to_sym) : available_actions)
 
     scope(path: path, controller: controller, **) do
-      get '/' => :index,            as: :"#{controller}_index"                 if actions.include? :index
-      get '/new' => :new,           as: :"#{controller}_new"                   if actions.include?(:new) && (new = true)
-      post '/new' => :create,       as:(:"#{controller}_new" unless new)       if actions.include? :create
+      get '/' => :index,            as: :"#{controller}_index"                      if actions.include? :index
+      get '/new' => :new,           as: :"#{controller}_new" and (new = true)       if actions.include? :new
+      post '/new' => :create,       as:(:"#{controller}_new" unless new)            if actions.include? :create
       scope path: '/:id' do
-        get '/' => :show,           as: :"#{controller}_show"                  if actions.include? :show
-        get '/edit' => :edit,       as: :"#{controller}_edit"                  if actions.include?(:edit) && (edit = true)
-        post '/edit' => :update,    as:(:"#{controller}_edit" unless edit)     if actions.include? :update
-        get '/delete' => :delete,   as: :"#{controller}_delete"                if actions.include?(:delete) && (delete = true)
-        post '/delete' => :destroy, as:(:"#{controller}_delete" unless delete) if actions.include? :destroy
+        get '/' => :show,           as: :"#{controller}_show"                       if actions.include? :show
+        get '/edit' => :edit,       as: :"#{controller}_edit" and (edit = true)     if actions.include? :edit
+        post '/edit' => :update,    as:(:"#{controller}_edit" unless edit)          if actions.include? :update
+        get '/delete' => :delete,   as: :"#{controller}_delete" and (delete = true) if actions.include? :delete
+        post '/delete' => :destroy, as:(:"#{controller}_delete" unless delete)      if actions.include? :destroy
       end
     end
   end
