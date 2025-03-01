993f98fa171cf2371c32367cf3a1429b49d865d1
Patrice Lebel <patleb@users.noreply.github.com>
2025-03-01 12:03:04 -0500

    minitest dir shouldn't autoload

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module WebTools
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(tasks web_tools))
+    config.autoload_lib(ignore: %w(minitest tasks web_tools))
 
     # Configuration for the application, engines, and railties goes here.
     #
