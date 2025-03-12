179b6a18cbfc205d66fe7a0d0ae79921d168654a
Patrice Lebel <patleb@users.noreply.github.com>
2025-03-12 00:46:48 -0400

    stacktrace added to RuntimeError

--- ext_rice/lib/rice/runtime_error.hpp
+++ ext_rice/lib/rice/runtime_error.hpp
@@ -1,25 +1,27 @@
-#define THROW_ERROR(msg) throw RuntimeError((msg), __FILE__, __LINE__)
+#define THROW_ERROR(msg) throw RuntimeError((msg), __FILE__, __LINE__, std::to_string(std::stacktrace::current()))
 
 class RuntimeError : public std::exception {
   public:
 
-  RuntimeError(const char * what_msg, const char * file, int line):
-    RuntimeError(std::string(what_msg), file, line) {
+  RuntimeError(const char * what_msg, const char * file, int line, const std::string & stacktrace):
+    RuntimeError(std::string(what_msg), file, line, stacktrace) {
   }
 
-  RuntimeError(const std::string & what_msg, const char * file, int line):
+  RuntimeError(const std::string & what_msg, const char * file, int line, const std::string & stacktrace):
     what_msg(what_msg),
     file(file),
-    line(line) {
+    line(line),
+    stacktrace(stacktrace) {
   }
 
   const char * what() const throw() {
-    return (file + ":" + std::to_string(line) + ": " + what_msg).c_str();
+    return (file + ":" + std::to_string(line) + ": " + what_msg + "\n" + stacktrace).c_str();
   }
 
   private:
 
   std::string what_msg;
+  std::string stacktrace;
   std::string file;
   int line;
 };
