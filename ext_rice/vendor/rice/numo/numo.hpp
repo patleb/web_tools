d0cea5f3276be64bdf68492ab0629894e7b7f1d7
Patrice Lebel <patleb@users.noreply.github.com>
2025-03-16 00:16:47 -0400

    clean-up

--- ext_rice/vendor/rice/numo/numo.hpp
+++ ext_rice/vendor/rice/numo/numo.hpp
@@ -11,223 +11,192 @@
 
 namespace numo {
 
-class NArray {
-public:
-  NArray(VALUE v) {
-    construct_value(this->dtype(), v);
-  }
+  class NArray {
+    public:
 
-  NArray(Rice::Object o) {
-    construct_value(this->dtype(), o.value());
-  }
-
-  VALUE value() const {
-    return this->_value;
-  }
-
-  size_t ndim() const {
-    return RNARRAY_NDIM(this->_value);
-  }
-
-  size_t* shape() const {
-    return RNARRAY_SHAPE(this->_value);
-  }
-
-  size_t size() const {
-    return RNARRAY_SIZE(this->_value);
-  }
-
-  bool is_contiguous() const {
-    return nary_check_contiguous(this->_value) == Qtrue;
-  }
-
-  operator Rice::Object() const {
-    return Rice::Object(this->_value);
-  }
-
-  const void* read_ptr() {
-    if (!is_contiguous()) {
-      this->_value = nary_dup(this->_value);
+    NArray(VALUE v) {
+      construct_value(this->dtype(), v);
     }
-    return nary_get_pointer_for_read(this->_value) + nary_get_offset(this->_value);
-  }
 
-  void* write_ptr() {
-    return nary_get_pointer_for_write(this->_value);
-  }
+    NArray(Rice::Object o) {
+      construct_value(this->dtype(), o.value());
+    }
 
-protected:
-  NArray() { }
+    VALUE value() const {
+      return this->_value;
+    }
 
-  void construct_value(VALUE dtype, VALUE v) {
-    this->_value = rb_funcall(dtype, rb_intern("cast"), 1, v);
-  }
+    size_t ndim() const {
+      return RNARRAY_NDIM(this->_value);
+    }
 
-  void construct_shape(VALUE dtype, std::initializer_list<size_t> shape) {
-    // rb_narray_new doesn't modify shape, but not marked as const
-    this->_value = rb_narray_new(dtype, shape.size(), const_cast<size_t*>(shape.begin()));
-  }
+    size_t* shape() const {
+      return RNARRAY_SHAPE(this->_value);
+    }
 
-  void construct_shape(VALUE dtype, std::vector<size_t> shape) {
-    this->_value = rb_narray_new(dtype, shape.size(), shape.data());
-  }
+    size_t size() const {
+      return RNARRAY_SIZE(this->_value);
+    }
 
-  VALUE _value;
+    bool is_contiguous() const {
+      return nary_check_contiguous(this->_value) == Qtrue;
+    }
 
-private:
-  VALUE dtype() {
-    return numo_cNArray;
-  }
-};
-<%- [
-  ['SFloat',  'float'],
-  ['DFloat',  'double'],
-  ['Int8',    'int8_t'],
-  ['Int16',   'int16_t'],
-  ['Int32',   'int32_t'],
-  ['Int64',   'int64_t'],
-  ['UInt8',   'uint8_t'],
-  ['UInt16',  'uint16_t'],
-  ['UInt32',  'uint32_t'],
-  ['UInt64',  'uint64_t'],
-  ['RObject', 'VALUE'],
-].each do |na_type, type| -%>
+    operator Rice::Object() const {
+      return Rice::Object(this->_value);
+    }
 
-class <%= na_type %>: public NArray {
-public:
-  <%= na_type %>(VALUE v) {
-    construct_value(this->dtype(), v);
-  }
+    const void* read_ptr() {
+      if (!is_contiguous()) {
+        this->_value = nary_dup(this->_value);
+      }
+      return nary_get_pointer_for_read(this->_value) + nary_get_offset(this->_value);
+    }
 
-  <%= na_type %>(Rice::Object o) {
-    construct_value(this->dtype(), o.value());
-  }
+    void* write_ptr() {
+      return nary_get_pointer_for_write(this->_value);
+    }
 
-  <%= na_type %>(std::initializer_list<size_t> shape) {
-    construct_shape(this->dtype(), shape);
-  }
+    protected:
 
-  <%= na_type %>(std::vector<size_t> shape) {
-    construct_shape(this->dtype(), shape);
-  }
+    NArray() { }
 
-  const <%= type %> * read_ptr() {
-    return reinterpret_cast< const <%= type %> * >(NArray::read_ptr());
-  }
+    void construct_value(VALUE dtype, VALUE v) {
+      this->_value = rb_funcall(dtype, rb_intern("cast"), 1, v);
+    }
 
-  <%= type %> * write_ptr() {
-    return reinterpret_cast< <%= type %> * >(NArray::write_ptr());
-  }
+    void construct_shape(VALUE dtype, std::initializer_list<size_t> shape) {
+      // rb_narray_new doesn't modify shape, but not marked as const
+      this->_value = rb_narray_new(dtype, shape.size(), const_cast<size_t*>(shape.begin()));
+    }
 
-private:
-  VALUE dtype() {
-    return numo_c<%= na_type %>;
-  }
-};
-<%- end -%>
-<%- ['SComplex', 'DComplex', 'Bit'].each do |na_type| -%>
+    void construct_shape(VALUE dtype, std::vector<size_t> shape) {
+      this->_value = rb_narray_new(dtype, shape.size(), shape.data());
+    }
 
-class <%= na_type %>: public NArray {
-public:
-  <%= na_type %>(VALUE v) {
-    construct_value(this->dtype(), v);
-  }
+    VALUE _value;
 
-  <%= na_type %>(Rice::Object o) {
-    construct_value(this->dtype(), o.value());
-  }
+    private:
 
-  <%= na_type %>(std::initializer_list<size_t> shape) {
-    construct_shape(this->dtype(), shape);
-  }
+    VALUE dtype() {
+      return numo_cNArray;
+    }
+  };
+  <%- [
+    ['SFloat',  'float'],
+    ['DFloat',  'double'],
+    ['Int8',    'int8_t'],
+    ['Int16',   'int16_t'],
+    ['Int32',   'int32_t'],
+    ['Int64',   'int64_t'],
+    ['UInt8',   'uint8_t'],
+    ['UInt16',  'uint16_t'],
+    ['UInt32',  'uint32_t'],
+    ['UInt64',  'uint64_t'],
+    ['RObject', 'VALUE'],
+  ].each do |na_type, type| -%>
 
-  <%= na_type %>(std::vector<size_t> shape) {
-    construct_shape(this->dtype(), shape);
-  }
+  class <%= na_type %>: public NArray {
+    public:
 
-private:
-  VALUE dtype() {
-    return numo_c<%= na_type %>;
-  }
-};
-<%- end -%>
+    <%= na_type %>(VALUE v) {
+      construct_value(this->dtype(), v);
+    }
+
+    <%= na_type %>(Rice::Object o) {
+      construct_value(this->dtype(), o.value());
+    }
+
+    <%= na_type %>(std::initializer_list<size_t> shape) {
+      construct_shape(this->dtype(), shape);
+    }
+
+    <%= na_type %>(std::vector<size_t> shape) {
+      construct_shape(this->dtype(), shape);
+    }
+
+    const <%= type %> * read_ptr() {
+      return reinterpret_cast< const <%= type %> * >(NArray::read_ptr());
+    }
+
+    <%= type %> * write_ptr() {
+      return reinterpret_cast< <%= type %> * >(NArray::write_ptr());
+    }
+
+    private:
+
+    VALUE dtype() {
+      return numo_c<%= na_type %>;
+    }
+  };
+  <%- end -%>
+  <%- ['SComplex', 'DComplex', 'Bit'].each do |na_type| -%>
+
+  class <%= na_type %>: public NArray {
+    public:
+
+    <%= na_type %>(VALUE v) {
+      construct_value(this->dtype(), v);
+    }
+
+    <%= na_type %>(Rice::Object o) {
+      construct_value(this->dtype(), o.value());
+    }
+
+    <%= na_type %>(std::initializer_list<size_t> shape) {
+      construct_shape(this->dtype(), shape);
+    }
+
+    <%= na_type %>(std::vector<size_t> shape) {
+      construct_shape(this->dtype(), shape);
+    }
+
+    private:
+
+    VALUE dtype() {
+      return numo_c<%= na_type %>;
+    }
+  };
+  <%- end -%>
 
 }
 
 namespace Rice::detail {
+  <%- %w(NArray SFloat DFloat Int8 Int16 Int32 Int64 UInt8 UInt16 UInt32 UInt64 RObject SComplex DComplex Bit).each do |na_type| -%>
 
-template<>
-struct Type<numo::NArray>
-{
-  static bool verify() { return true; }
-};
+  template<>
+  struct Type< numo::<%= na_type %> > {
+    static bool verify() { return true; }
+  };
 
-template<>
-class From_Ruby<numo::NArray>
-{
-public:
-  Convertible is_convertible(VALUE value) {
-    switch (rb_type(value))
-    {
-      case RUBY_T_DATA:
-        return Data_Type<numo::NArray>::is_descendant(value) ? Convertible::Exact : Convertible::None;
-      case RUBY_T_ARRAY:
-        return Convertible::Cast;
-      default:
-        return Convertible::None;
-    }
-  }
+  template<>
+  class From_Ruby< numo::<%= na_type %> > {
+    public:
 
-  numo::NArray convert(VALUE x) {
-    return numo::NArray(x);
-  }
-};
-
-template<>
-class To_Ruby<numo::NArray>
-{
-public:
-  VALUE convert(const numo::NArray& x) {
-    return x.value();
-  }
-};
-<%- %w(SFloat DFloat Int8 Int16 Int32 Int64 UInt8 UInt16 UInt32 UInt64 RObject SComplex DComplex Bit).each do |na_type| -%>
-
-template<>
-struct Type< numo::<%= na_type %> >
-{
-  static bool verify() { return true; }
-};
-
-template<>
-class From_Ruby< numo::<%= na_type %> >
-{
-public:
-  Convertible is_convertible(VALUE value) {
-    switch (rb_type(value))
-    {
+    Convertible is_convertible(VALUE value) {
+      switch (rb_type(value)) {
       case RUBY_T_DATA:
         return Data_Type< numo::<%= na_type %> >::is_descendant(value) ? Convertible::Exact : Convertible::None;
       case RUBY_T_ARRAY:
         return Convertible::Cast;
       default:
         return Convertible::None;
+      }
     }
-  }
 
-  numo::<%= na_type %> convert(VALUE x) {
-    return numo::<%= na_type %>(x);
-  }
-};
+    numo::<%= na_type %> convert(VALUE x) {
+      return numo::<%= na_type %>(x);
+    }
+  };
 
-template<>
-class To_Ruby<numo::<%= na_type %>>
-{
-public:
-  VALUE convert(const numo::<%= na_type %>& x) {
-    return x.value();
-  }
-};
-<%- end -%>
+  template<>
+  class To_Ruby<numo::<%= na_type %>> {
+    public:
+
+    VALUE convert(const numo::<%= na_type %>& x) {
+      return x.value();
+    }
+  };
+  <%- end -%>
 
 }
