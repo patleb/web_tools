a19206134e9f0b11c3f0ed707b22a1b9b2647f65
Patrice Lebel <patleb@users.noreply.github.com>
2025-03-28 17:21:57 -0400

    make sure current link is styled for either the sidebar or group tab

--- mix_page/app/presenters/page_fields/link_presenter.rb
+++ mix_page/app/presenters/page_fields/link_presenter.rb
@@ -1,15 +1,26 @@
 module PageFields
   class LinkPresenter < PageFieldPresenter
+    def item_options
+      options = super
+      options.union!({ class: ['bordered', 'tab-active'] }) if request.path.start_with? record_url
+      options
+    end
+
     def rendering(**options)
       div_(options) {[
         pretty_actions,
-        a_(href: record.to_url){ title },
+        a_(href: record_url){ title },
       ]}
     end
 
     def edit_url
       return @edit_url if defined? @edit_url
       @edit_url = record.fieldable.admin_presenter.allowed_url(:edit)
+    end
+
+    def record_url
+      return @record_url if defined? @record_url
+      @record_url = record.to_url
     end
   end
 end
