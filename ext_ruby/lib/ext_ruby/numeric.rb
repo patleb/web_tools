ee87d4fa509d2684a9f2ef49aef7f556d5cd381b
Patrice Lebel <patleb@users.noreply.github.com>
2025-03-28 17:42:55 -0400

    brought back byte scale converter

--- ext_ruby/lib/ext_ruby/numeric.rb
+++ ext_ruby/lib/ext_ruby/numeric.rb
@@ -53,6 +53,9 @@ class Numeric
   B_PER_KB = BigDecimal(1_024)
   B_PER_MB = BigDecimal(1_048_576)
   B_PER_GB = BigDecimal(1_073_741_824)
+  KB_PER_MB = BigDecimal(1_024)
+  KB_PER_GB = BigDecimal(1_048_576)
+  MB_PER_GB = BigDecimal(1_024)
 
   def simplify(n = 5)
     sign, significant_digits, base, exponent = to_d.split
@@ -87,5 +90,29 @@ class Numeric
 
   def gb_to_bytes
     (self * B_PER_GB).to_i
+  end
+
+  def bytes_to_kb(precision = 3)
+    precision ? (self / B_PER_KB).to_f.ceil(precision) : (self / B_PER_KB).ceil
+  end
+
+  def bytes_to_mb(precision = 3)
+    precision ? (self / B_PER_MB).to_f.ceil(precision) : (self / B_PER_MB).ceil
+  end
+
+  def bytes_to_gb(precision = 3)
+    precision ? (self / B_PER_GB).to_f.ceil(precision) : (self / B_PER_GB).ceil
+  end
+
+  def kb_to_mb(precision = 3)
+    precision ? (self / KB_PER_MB).to_f.ceil(precision) : (self / KB_PER_MB).ceil
+  end
+
+  def kb_to_gb(precision = 3)
+    precision ? (self / KB_PER_GB).to_f.ceil(precision) : (self / KB_PER_GB).ceil
+  end
+
+  def mb_to_gb(precision = 3)
+    precision ? (self / MB_PER_GB).to_f.ceil(precision) : (self / MB_PER_GB).ceil
   end
 end
