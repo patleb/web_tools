2b8758b6bca3b3d111ba0b595f238519680af899
Patrice Lebel <patleb@users.noreply.github.com>
2025-05-18 14:43:20 -0400

    log_vector added

--- ext_rice/lib/rice/logger.hpp
+++ ext_rice/lib/rice/logger.hpp
@@ -5,6 +5,7 @@ class Logger {
 
   static const std::string LEVELS[<%= ExtRice.config.log_levels.size %>];
   static size_t marker_i;
+  static bool new_run;
 
   explicit Logger() {
     file.open("<%= ExtRice.config.log_path %>", std::ofstream::out | std::ofstream::app);
@@ -18,6 +19,9 @@ class Logger {
   // Parameter Pack: https://www.scs.stanford.edu/~dm/blog/param-pack.html
   template < class... Args >
   void log(const Args & ...messages, const LEVEL level) {
+    <%- if Rails.env.local? -%>
+    if (new_run) { new_run = false; file << std::endl; }
+    <%- end -%>
     file << "[" << C::timestamp() << "][" << LEVELS[level] << "]: ";
     (file << ... << std::format("{}", messages));
     file << std::endl;
@@ -30,6 +34,7 @@ class Logger {
 
 const std::string Logger::LEVELS[] = { <%= ExtRice.config.log_levels.keys.map(&:upcase).map(&:quoted).join(', ') %> };
 size_t Logger::marker_i = 0;
+bool Logger::new_run = true;
 
 auto logger = Logger();
 
@@ -44,4 +49,19 @@ void log_<%= level %>(const Args & ...messages) {
 <%- end -%>
 void log_mark() {
   log_error("mark: ", Logger::marker_i++);
+}
+
+template < class T >
+void log_vector(const T vector[], size_t count, std::string_view prefix = "") {
+  std::stringstream message;
+  message << (prefix.size() ? prefix : "vector");
+  for (size_t i = 0; i < count; ++i) {
+    message << " (" << std::to_string(i) << ") " << std::to_string(vector[i]);
+  }
+  log_error(message.str());
+}
+
+template < class T >
+void log_vector(const std::vector< T > & vector, std::string_view prefix = "") {
+  log_vector< T >(vector.data(), vector.size(), prefix);
 }
