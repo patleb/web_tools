962a259791e2a2603fb11f2c12716f9957f19b1b
Patrice Lebel <patleb@users.noreply.github.com>
2025-06-06 12:08:00 -0400

    clean-up

--- ext_rice/lib/rice/tensor.hpp
+++ ext_rice/lib/rice/tensor.hpp
@@ -240,23 +240,20 @@ namespace Tensor {
 
   using NType = std::variant< <%= compile_vars[:numeric_types].keys.join(', ') %>, Vstring >;
 
-  auto build(Tensor::Type type, const Vsize_t & shape, const GType & fill_value = null) {
+  Tensor::NType build(Tensor::Type type, const Vsize_t & shape, const GType & fill_value = null) {
     switch (type) {
     <%- compile_vars[:numeric_types].each do |tensor_type, type| -%>
-    case Tensor::Type::<%= tensor_type %>: {
-      auto nodata = g_cast< <%= type %> >(fill_value);
-      return Tensor::NType(Tensor::<%= tensor_type %>(shape, nodata));
-    }
+    case Tensor::Type::<%= tensor_type %>: return Tensor::<%= tensor_type %>(shape, g_cast< <%= type %> >(fill_value));
     <%- end -%>
     default:
       throw RuntimeError("invalid Tensor::Type");
     }
   }
 
-  auto cast(Tensor::Base & tensor, Tensor::Type type) {
+  Tensor::NType cast(Tensor::Base & tensor, Tensor::Type type) {
     switch (type) {
     <%- compile_vars[:numeric_types].each_key do |tensor_type| -%>
-    case Tensor::Type::<%= tensor_type %>: return Tensor::NType(dynamic_cast< Tensor::<%= tensor_type %> & >(tensor));
+    case Tensor::Type::<%= tensor_type %>: return dynamic_cast< Tensor::<%= tensor_type %> & >(tensor);
     <%- end -%>
     default:
       throw RuntimeError("invalid Tensor::Type");
