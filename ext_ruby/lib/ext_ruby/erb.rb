25861b419dfdd1c06152ace56f487c3b7e142ed4
Patrice Lebel <patleb@users.noreply.github.com>
2025-06-08 13:52:37 -0400

    :lean mode option added to ERB.template

--- ext_ruby/lib/ext_ruby/erb.rb
+++ ext_ruby/lib/ext_ruby/erb.rb
@@ -1,6 +1,35 @@
 class ERB
-  def self.template(src, binding = nil, **)
-    erb = ERB.new(Pathname.new(src).read, **)
+  def self.template(src, binding = nil, lean: false, **)
+    path = Pathname.new(src)
+    if lean
+      variables = []
+      content = path.each_line.with_object(+'') do |line, content|
+        case line
+        when / do \|([^|]+)\|/
+          if $1.match? /^[\WA-Z_0-9]+$/
+            tokens = $1.split(',').map{ |token| token.gsub(/\W/, '') }
+            tokens.each do |token|
+              line.gsub! /(\W)#{token}(\W)/, "\\1#{token.downcase}\\2"
+            end
+            variables.push tokens
+          else
+            variables.push []
+          end
+        when / end /
+          variables.pop
+        else
+          variables.each do |tokens|
+            tokens.each do |token|
+              line.gsub! /(\W)#{token}(\W)/, "\\1<%= #{token.downcase} %>\\2"
+            end
+          end
+        end
+        content << line
+      end
+    else
+      content = path.read
+    end
+    erb = ERB.new(content, **)
     binding ? erb.result(binding) : erb.result
   end
 end
