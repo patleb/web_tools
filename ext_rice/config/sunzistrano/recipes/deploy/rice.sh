f9ff8159b0f4f30da6edf69eb1ce3d29f112687e
Patrice Lebel <patleb@users.noreply.github.com>
2025-07-31 00:26:39 -0400

    copy previous ext.so on deploy

--- ext_rice/config/sunzistrano/recipes/deploy/rice.sh
+++ ext_rice/config/sunzistrano/recipes/deploy/rice.sh
@@ -1,6 +1,10 @@
+TARGET=<%= Rice.target %>
+
 cd ${release_path}
 
 desc "Compile Rice C++ extension for ${stage}"
+cp "${current_path}/app/rice/${TARGET}.sha256" "app/rice/${TARGET}.sha256" 2> /dev/null || :
+cp "${current_path}/app/rice/${TARGET}.so"     "app/rice/${TARGET}.so"     2> /dev/null || :
 bin/rake rice:compile
 
 cd.back
