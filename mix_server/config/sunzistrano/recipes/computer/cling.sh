462a63a51c77ff3a6f5dfd438a67422ad4ad76e2
Patrice Lebel <patleb@users.noreply.github.com>
2025-08-07 11:06:49 -0400

    gdbinit config added

--- mix_server/config/sunzistrano/recipes/computer/cling.sh
+++ mix_server/config/sunzistrano/recipes/computer/cling.sh
@@ -2,3 +2,7 @@ sun.add_repo "ppa:ppa-verse/cling"
 sun.update
 
 sun.install "cling"
+
+echo 'set history save on' >> $HOME/.gdbinit
+echo 'set history size unlimited' >>  $HOME/.gdbinit
+echo 'set history remove-duplicates unlimited' >> $HOME/.gdbinit
