e4901b9fdb7705fc47cb1e14e7a24fc42b376967
Patrice Lebel <patleb@users.noreply.github.com>
2025-08-07 11:16:06 -0400

    don't use ccache on servers

--- ext_rice/config/sunzistrano/recipes/deploy/rice.sh
+++ ext_rice/config/sunzistrano/recipes/deploy/rice.sh
@@ -5,6 +5,6 @@ cd ${release_path}
 desc "Compile Rice C++ extension for ${stage}"
 cp "${current_path}/app/rice/${TARGET}.sha256" "app/rice/${TARGET}.sha256" 2> /dev/null || :
 cp "${current_path}/app/rice/${TARGET}.so"     "app/rice/${TARGET}.so"     2> /dev/null || :
-bin/rake rice:compile
+CCACHE=false bin/rake rice:compile
 
 cd.back
