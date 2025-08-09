5d504d6075b4c73034ee035baf2acf969889dedb
Patrice Lebel <patleb@users.noreply.github.com>
2025-08-09 13:51:22 -0400

    do not auto-install ext_shakapacker on dev server load

--- ext_shakapacker/lib/ext_shakapacker/railtie.rb
+++ ext_shakapacker/lib/ext_shakapacker/railtie.rb
@@ -10,10 +10,6 @@ module ExtShakapacker
       ActiveSupport::Dependencies.autoload_paths.delete("#{app.root}/app/javascript")
     end
 
-    initializer 'ext_shakapacker.bootstrap', before: 'shakapacker.proxy' do
-      require 'ext_shakapacker/shakapacker'
-    end
-
     ActiveSupport.on_load(:action_view) do
       require 'ext_shakapacker/shakapacker/helper'
     end
