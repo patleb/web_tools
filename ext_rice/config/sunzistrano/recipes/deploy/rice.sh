c5c13b67a907dda8f4fd34d24446a6ff311ccf63
Patrice Lebel <patleb@users.noreply.github.com>
2025-08-26 05:28:55 -0400

    use ccache by default

--- ext_rice/config/sunzistrano/recipes/deploy/rice.sh
+++ ext_rice/config/sunzistrano/recipes/deploy/rice.sh
@@ -1,8 +1,10 @@
+ccache=${ccache:-true}
+
 cd ${release_path}
 
 desc "Compile Rice C++ extension for ${stage}"
 cp "${current_path}/app/rice/${rice_target}.sha256" "app/rice/${rice_target}.sha256" 2> /dev/null || :
 cp "${current_path}/app/rice/${rice_target}.so"     "app/rice/${rice_target}.so"     2> /dev/null || :
-CCACHE=false rake rice:compile
+CCACHE="${ccache}" rake rice:compile
 
 cd.back
