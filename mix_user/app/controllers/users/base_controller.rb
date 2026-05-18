71f8f28641aad2538a201a0e98172cc35493bd82
Patrice Lebel <patleb@users.noreply.github.com>
2026-05-17 20:57:46 -0400

    prevent bots from using user routes

--- mix_user/app/controllers/users/base_controller.rb
+++ mix_user/app/controllers/users/base_controller.rb
@@ -1,10 +1,16 @@
 module Users
   class BaseController < LibController
+    before_action :browser_bot! unless Rails.env.local?
+
     no_authentication only: [:new, :create]
 
     layout 'users'
 
     private
+
+    def browser_bot!
+      head :forbidden if browser_bot?
+    end
 
     def send_email(name, user_scope = nil)
       user = @user || user_scope.find_by!(email: create_params[:email])
