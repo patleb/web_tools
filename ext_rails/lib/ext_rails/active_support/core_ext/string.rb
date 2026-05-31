e3d2afc5a4afc38ea75f749f2f7c2e74e8e199eb
Patrice Lebel <patleb@users.noreply.github.com>
2026-05-31 19:03:00 -0400

    make #strip_sql debug friendly by keeping lines

--- ext_rails/lib/ext_rails/active_support/core_ext/string.rb
+++ ext_rails/lib/ext_rails/active_support/core_ext/string.rb
@@ -11,8 +11,8 @@ class String
     Arel.sql(self)
   end
 
-  def strip_sql(**variables)
-    result = strip.gsub(/(--.*\n|\n)/, ' ').gsub(/\s{2,}/, ' ')
+  def strip_sql(squish: false, **variables)
+    result = squish ? strip.gsub(/(--.*\n|\n)/, ' ').gsub(/\s{2,}/, ' ') : strip.gsub(/--.*$/, '')
     unless variables.empty?
       result = variables.reduce(result) do |sql, (name, value)|
         sql.gsub(/\{\{\s*#{name}\s*\}\}/, value.to_s.strip_sql)
