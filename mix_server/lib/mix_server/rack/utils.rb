b09eda13123886533bf9ada3ad59701c59ae5fb6
Patrice Lebel <patleb@users.noreply.github.com>
2026-06-18 17:58:32 -0400

    Rack::Utils.log_context improvements

--- mix_server/lib/mix_server/rack/utils.rb
+++ mix_server/lib/mix_server/rack/utils.rb
@@ -8,8 +8,9 @@ module Rack::Utils
     context = REQUEST_CONTEXT.each_with_object(variant: request.variant[0]&.to_s) do |k, h|
       h[k] = request.public_send(k)&.to_s
     end
-    params = request.filtered_parameters.except(*IGNORED_PARAMS)
-    { request: context.compact, params: params }
+    params = request.filtered_parameters.except(*IGNORED_PARAMS, request.controller_class.try(:controller_path))
+    cookies = request.cookies.try(:select){ |k, _| k.start_with?('_') && !k.end_with?('_session') } || {},
+    { request: context.compact, params: params, cookies: cookies }
   rescue Exception
     {}
   end
