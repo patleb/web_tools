f611b9c9d42eb75dcd06a62b81c55df3ad3d1bf3
Patrice Lebel <patleb@users.noreply.github.com>
2026-06-27 19:32:15 -0400

    postgres client recipe correction

--- mix_server/config/sunzistrano/recipes/db/postgres_client.sh
+++ mix_server/config/sunzistrano/recipes/db/postgres_client.sh
@@ -1,5 +1,6 @@
 sun.install "postgresql-common"
 yes | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh $CODE
+sun.install "postgresql-common" # update to the most recent apt.postgresql.org.sh
 sun.backup_compare /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
 
 sun.install "postgresql-client"
